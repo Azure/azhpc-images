f99e80d280394bdb277b7f5448566b6a02e00d72
Jithin Jose <jijos@microsoft.com>
2020-06-26 14:19:56 -0700

    Get kernel source using kernel-devel package

--- centos/centos-7.x/centos-7.6-hpc/install_mellanoxofed.sh
+++ centos/centos-7.x/centos-7.6-hpc/install_mellanoxofed.sh
@@ -7,5 +7,5 @@ tar zxvf MLNX_OFED_LINUX-5.0-2.1.8.0-rhel7.6-x86_64.tgz
 
 KERNEL=( $(rpm -q kernel | sed 's/kernel\-//g') )
 KERNEL=${KERNEL[-1]}
-yum -y install http://olcentgbl.trafficmanager.net/centos/7.6.1810/updates/x86_64/kernel-devel-${KERNEL}.rpm
+yum install -y kernel-devel-${KERNEL}
 ./MLNX_OFED_LINUX-5.0-2.1.8.0-rhel7.6-x86_64/mlnxofedinstall --kernel $KERNEL --kernel-sources /usr/src/kernels/${KERNEL} --add-kernel-support --skip-repo

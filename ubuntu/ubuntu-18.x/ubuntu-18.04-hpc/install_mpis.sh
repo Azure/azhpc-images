cae835c37119387ebf8be9200df7121b47c2d25e
Souvik De <souvikde@microsoft.com>
2021-01-13 14:15:20 -0800

    Added IMPI 2021 for Ubuntu

--- ubuntu/ubuntu-18.x/ubuntu-18.04-hpc/install_mpis.sh
+++ ubuntu/ubuntu-18.x/ubuntu-18.04-hpc/install_mpis.sh
@@ -40,16 +40,21 @@ cd openmpi-${OMPI_VERSION}
 ./configure --prefix=${INSTALL_PREFIX}/openmpi-${OMPI_VERSION} --with-ucx=${UCX_PATH} --with-hcoll=${HCOLL_PATH} --enable-mpirun-prefix-by-default --with-platform=contrib/platform/mellanox/optimized && make -j$(nproc) && make install
 cd ..
 
-# Intel MPI 2019 (update 8)
-IMPI_2019_VERSION="2019.8.254"
-IMPI_2019_DOWNLOAD_URL=http://registrationcenter-download.intel.com/akdlm/irc_nas/tec/16814/l_mpi_${IMPI_2019_VERSION}.tgz
-$COMMON_DIR/download_and_verify.sh $IMPI_2019_DOWNLOAD_URL "fa163b4b79bd1b7509980c3e7ad81b354fc281a92f9cf2469bf4d323899567c0"
-tar -xvf l_mpi_${IMPI_2019_VERSION}.tgz
-cd l_mpi_${IMPI_2019_VERSION}
-sed -i -e 's/ACCEPT_EULA=decline/ACCEPT_EULA=accept/g' silent.cfg
-./install.sh --silent ./silent.cfg
-cd ..
+# # Intel MPI 2019 (update 8)
+# IMPI_2019_VERSION="2019.8.254"
+# IMPI_2019_DOWNLOAD_URL=http://registrationcenter-download.intel.com/akdlm/irc_nas/tec/16814/l_mpi_${IMPI_2019_VERSION}.tgz
+# $COMMON_DIR/download_and_verify.sh $IMPI_2019_DOWNLOAD_URL "fa163b4b79bd1b7509980c3e7ad81b354fc281a92f9cf2469bf4d323899567c0"
+# tar -xvf l_mpi_${IMPI_2019_VERSION}.tgz
+# cd l_mpi_${IMPI_2019_VERSION}
+# sed -i -e 's/ACCEPT_EULA=decline/ACCEPT_EULA=accept/g' silent.cfg
+# ./install.sh --silent ./silent.cfg
+# cd ..
 
+# Intel MPI 2021 (update 1) - oneAPI
+IMPI_2021_VERSION="2021.1.1"
+IMPI_2021_DOWNLOAD_URL=https://registrationcenter-download.intel.com/akdlm/irc_nas/17397/l_mpi_oneapi_p_2021.1.1.76_offline.sh
+$COMMON_DIR/download_and_verify.sh $IMPI_2021_DOWNLOAD_URL "8b7693a156c6fc6269637bef586a8fd3ea6610cac2aae4e7f48c1fbb601625fe"
+./l_mpi_oneapi_p_2021.1.1.76_offline.sh -s -a -s --eula accept
 
 # Module Files
 MODULE_FILES_DIRECTORY=/usr/share/modules/modulefiles/mpi
@@ -102,22 +107,37 @@ setenv          MPI_HOME        /opt/openmpi-${OMPI_VERSION}
 EOF
 
 # Intel 2019
-cat << EOF >> ${MODULE_FILES_DIRECTORY}/impi_${IMPI_2019_VERSION}
+# cat << EOF >> ${MODULE_FILES_DIRECTORY}/impi_${IMPI_2019_VERSION}
+# #%Module 1.0
+# #
+# #  Intel MPI ${IMPI_2019_VERSION}
+# #
+# conflict        mpi
+# module load /opt/intel/impi/${IMPI_2019_VERSION}/intel64/modulefiles/mpi
+# setenv          MPI_BIN         /opt/intel/impi/${IMPI_2019_VERSION}/intel64/bin
+# setenv          MPI_INCLUDE     /opt/intel/impi/${IMPI_2019_VERSION}/intel64/include
+# setenv          MPI_LIB         /opt/intel/impi/${IMPI_2019_VERSION}/intel64/lib
+# setenv          MPI_MAN         /opt/intel/impi/${IMPI_2019_VERSION}/man
+# setenv          MPI_HOME        /opt/intel/impi/${IMPI_2019_VERSION}/intel64
+# EOF
+
+cat << EOF >> /usr/share/Modules/modulefiles/mpi/impi_${IMPI_2021_VERSION}
 #%Module 1.0
 #
-#  Intel MPI ${IMPI_2019_VERSION}
+#  Intel MPI ${IMPI_2021_VERSION}
 #
 conflict        mpi
-module load /opt/intel/impi/${IMPI_2019_VERSION}/intel64/modulefiles/mpi
-setenv          MPI_BIN         /opt/intel/impi/${IMPI_2019_VERSION}/intel64/bin
-setenv          MPI_INCLUDE     /opt/intel/impi/${IMPI_2019_VERSION}/intel64/include
-setenv          MPI_LIB         /opt/intel/impi/${IMPI_2019_VERSION}/intel64/lib
-setenv          MPI_MAN         /opt/intel/impi/${IMPI_2019_VERSION}/man
-setenv          MPI_HOME        /opt/intel/impi/${IMPI_2019_VERSION}/intel64
+module load /opt/intel/oneapi/mpi/${IMPI_2021_VERSION}/modulefiles/mpi
+setenv          MPI_BIN         /opt/intel/oneapi/mpi/${IMPI_2021_VERSION}/bin
+setenv          MPI_INCLUDE     /opt/intel/oneapi/mpi/${IMPI_2021_VERSION}/include
+setenv          MPI_LIB         /opt/intel/oneapi/mpi/${IMPI_2021_VERSION}/lib
+setenv          MPI_MAN         /opt/intel/oneapi/mpi/${IMPI_2021_VERSION}/man
+setenv          MPI_HOME        /opt/intel/oneapi/mpi/${IMPI_2021_VERSION}
 EOF
 
 # Softlinks
 ln -s ${MODULE_FILES_DIRECTORY}/hpcx-${HPCX_VERSION} ${MODULE_FILES_DIRECTORY}/hpcx
 ln -s ${MODULE_FILES_DIRECTORY}/mvapich2-${MV2_VERSION} ${MODULE_FILES_DIRECTORY}/mvapich2
 ln -s ${MODULE_FILES_DIRECTORY}/openmpi-${OMPI_VERSION} ${MODULE_FILES_DIRECTORY}/openmpi
-ln -s ${MODULE_FILES_DIRECTORY}/impi_${IMPI_2019_VERSION} ${MODULE_FILES_DIRECTORY}/impi-2019
+# ln -s ${MODULE_FILES_DIRECTORY}/impi_${IMPI_2019_VERSION} ${MODULE_FILES_DIRECTORY}/impi-2019
+ln -s ${MODULE_FILES_DIRECTORY}/impi_${IMPI_2021_VERSION} ${MODULE_FILES_DIRECTORY}/impi-2021

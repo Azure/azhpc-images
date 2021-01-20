9f949280db115ea36d137d8588909db61d8d85d0
Souvik De <souvikde@microsoft.com>
2021-01-20 15:16:00 -0800

    Updated to MOFED 5.2-1.0.4.0, MVAPICH2 2.3.5 and IMPI 2021.1.1. Diabled CUDA check in test

--- centos/common/install_mpis.sh
+++ centos/common/install_mpis.sh
@@ -14,17 +14,17 @@ export LD_LIBRARY_PATH=/opt/${GCC_VERSION}/lib64:$LD_LIBRARY_PATH
 set CC=/opt/${GCC_VERSION}/bin/gcc
 set GCC=/opt/${GCC_VERSION}/bin/gcc
 
-# MVAPICH2 2.3.4
-MV2_VERSION="2.3.4"
+# MVAPICH2 2.3.5
+MV2_VERSION="2.3.5"
 MV2_DOWNLOAD_URL=http://mvapich.cse.ohio-state.edu/download/mvapich/mv2/mvapich2-${MV2_VERSION}.tar.gz
-$COMMON_DIR/download_and_verify.sh $MV2_DOWNLOAD_URL "7226a45c7c98333c8e5d2888119cce186199b430c13b7b1dca1769909e68ea7a"
+$COMMON_DIR/download_and_verify.sh $MV2_DOWNLOAD_URL "f9f467fec5fc981a89a7beee0374347b10c683023c76880f92a1a0ad4b961a8c"
 tar -xvf mvapich2-${MV2_VERSION}.tar.gz
 cd mvapich2-${MV2_VERSION}
 ./configure --prefix=${INSTALL_PREFIX}/mvapich2-${MV2_VERSION} --enable-g=none --enable-fast=yes && make -j$(nproc) && make install
 cd ..
 
 
-# OpenMPI 4.0.4
+# OpenMPI 4.0.5
 OMPI_VERSION="4.0.5"
 OMPI_DOWNLOAD_URL=https://download.open-mpi.org/release/open-mpi/v4.0/openmpi-${OMPI_VERSION}.tar.gz
 $COMMON_DIR/download_and_verify.sh $OMPI_DOWNLOAD_URL "572e777441fd47d7f06f1b8a166e7f44b8ea01b8b2e79d1e299d509725d1bd05"
@@ -33,15 +33,21 @@ cd openmpi-${OMPI_VERSION}
 ./configure --prefix=${INSTALL_PREFIX}/openmpi-${OMPI_VERSION} --with-ucx=${UCX_PATH} --with-hcoll=${HCOLL_PATH} --enable-mpirun-prefix-by-default --with-platform=contrib/platform/mellanox/optimized && make -j$(nproc) && make install
 cd ..
 
-# Intel MPI 2019 (update 8)
-IMPI_2019_VERSION="2019.8.254"
-IMPI_2019_DOWNLOAD_URL=http://registrationcenter-download.intel.com/akdlm/irc_nas/tec/16814/l_mpi_${IMPI_2019_VERSION}.tgz
-$COMMON_DIR/download_and_verify.sh $IMPI_2019_DOWNLOAD_URL "fa163b4b79bd1b7509980c3e7ad81b354fc281a92f9cf2469bf4d323899567c0"
-tar -xvf l_mpi_${IMPI_2019_VERSION}.tgz
-cd l_mpi_${IMPI_2019_VERSION}
-sed -i -e 's/ACCEPT_EULA=decline/ACCEPT_EULA=accept/g' silent.cfg
-./install.sh --silent ./silent.cfg
-cd ..
+# Intel MPI 2021 (update 1)
+IMPI_2021_VERSION="2021.1.1"
+IMPI_2021_DOWNLOAD_URL=https://registrationcenter-download.intel.com/akdlm/irc_nas/17397/l_mpi_oneapi_p_2021.1.1.76_offline.sh
+$COMMON_DIR/download_and_verify.sh $IMPI_2021_DOWNLOAD_URL "8b7693a156c6fc6269637bef586a8fd3ea6610cac2aae4e7f48c1fbb601625fe"
+sudo bash l_mpi_oneapi_p_2021.1.1.76_offline.sh -s -a -s --eula accept
+
+# # Intel MPI 2019 (update 8)
+# IMPI_2019_VERSION="2019.8.254"
+# IMPI_2019_DOWNLOAD_URL=http://registrationcenter-download.intel.com/akdlm/irc_nas/tec/16814/l_mpi_${IMPI_2019_VERSION}.tgz
+# $COMMON_DIR/download_and_verify.sh $IMPI_2019_DOWNLOAD_URL "fa163b4b79bd1b7509980c3e7ad81b354fc281a92f9cf2469bf4d323899567c0"
+# tar -xvf l_mpi_${IMPI_2019_VERSION}.tgz
+# cd l_mpi_${IMPI_2019_VERSION}
+# sed -i -e 's/ACCEPT_EULA=decline/ACCEPT_EULA=accept/g' silent.cfg
+# ./install.sh --silent ./silent.cfg
+# cd ..
 
 # Install MVAPICH2-X 2.3
 #MVAPICH2X_DOWNLOAD_URL=https://mvapich.cse.ohio-state.edu/download/mvapich/mv2x/2.3/mofed5.1/mvapich2-x-azure-xpmem-mofed5.1-gnu9.2.0-v2.3xmofed5-1.el7.x86_64.rpm
@@ -98,20 +104,35 @@ setenv          MPI_MAN         /opt/openmpi-${OMPI_VERSION}/share/man
 setenv          MPI_HOME        /opt/openmpi-${OMPI_VERSION}
 EOF
 
-#IntelMPI-v2019
-cat << EOF >> /usr/share/Modules/modulefiles/mpi/impi_${IMPI_2019_VERSION}
+#IntelMPI-v2021
+cat << EOF >> /usr/share/Modules/modulefiles/mpi/impi_${IMPI_2021_VERSION}
 #%Module 1.0
 #
-#  Intel MPI ${IMPI_2019_VERSION}
+#  Intel MPI ${IMPI_2021_VERSION}
 #
 conflict        mpi
-module load /opt/intel/impi/${IMPI_2019_VERSION}/intel64/modulefiles/mpi
-setenv          MPI_BIN         /opt/intel/impi/${IMPI_2019_VERSION}/intel64/bin
-setenv          MPI_INCLUDE     /opt/intel/impi/${IMPI_2019_VERSION}/intel64/include
-setenv          MPI_LIB         /opt/intel/impi/${IMPI_2019_VERSION}/intel64/lib
-setenv          MPI_MAN         /opt/intel/impi/${IMPI_2019_VERSION}/man
-setenv          MPI_HOME        /opt/intel/impi/${IMPI_2019_VERSION}/intel64
+module load /opt/intel/oneapi/mpi/${IMPI_2021_VERSION}/modulefiles/mpi
+setenv          MPI_BIN         /opt/intel/oneapi/mpi/${IMPI_2021_VERSION}/bin
+setenv          MPI_INCLUDE     /opt/intel/oneapi/mpi/${IMPI_2021_VERSION}/include
+setenv          MPI_LIB         /opt/intel/oneapi/mpi/${IMPI_2021_VERSION}/lib
+setenv          MPI_MAN         /opt/intel/oneapi/mpi/${IMPI_2021_VERSION}/man
+setenv          MPI_HOME        /opt/intel/oneapi/mpi/${IMPI_2021_VERSION}
 EOF
+
+#IntelMPI-v2019
+# cat << EOF >> /usr/share/Modules/modulefiles/mpi/impi_${IMPI_2019_VERSION}
+# #%Module 1.0
+# #
+# #  Intel MPI ${IMPI_2019_VERSION}
+# #
+# conflict        mpi
+# module load /opt/intel/impi/${IMPI_2019_VERSION}/intel64/modulefiles/mpi
+# setenv          MPI_BIN         /opt/intel/impi/${IMPI_2019_VERSION}/intel64/bin
+# setenv          MPI_INCLUDE     /opt/intel/impi/${IMPI_2019_VERSION}/intel64/include
+# setenv          MPI_LIB         /opt/intel/impi/${IMPI_2019_VERSION}/intel64/lib
+# setenv          MPI_MAN         /opt/intel/impi/${IMPI_2019_VERSION}/man
+# setenv          MPI_HOME        /opt/intel/impi/${IMPI_2019_VERSION}/intel64
+# EOF
 
 # MVAPICH2-X 2.3
 #cat << EOF >> /usr/share/Modules/modulefiles/mpi/mvapich2x-${MV2X_VERSION}
@@ -134,6 +155,7 @@ EOF
 # Create symlinks for modulefiles
 ln -s /usr/share/Modules/modulefiles/mpi/mvapich2-${MV2_VERSION} /usr/share/Modules/modulefiles/mpi/mvapich2
 ln -s /usr/share/Modules/modulefiles/mpi/openmpi-${OMPI_VERSION} /usr/share/Modules/modulefiles/mpi/openmpi
-ln -s /usr/share/Modules/modulefiles/mpi/impi_${IMPI_2019_VERSION} /usr/share/Modules/modulefiles/mpi/impi-2019
+ln -s /usr/share/Modules/modulefiles/mpi/impi_${IMPI_2021_VERSION} /usr/share/Modules/modulefiles/mpi/impi-2021
+# ln -s /usr/share/Modules/modulefiles/mpi/impi_${IMPI_2019_VERSION} /usr/share/Modules/modulefiles/mpi/impi-2019
 #ln -s /usr/share/Modules/modulefiles/mpi/mvapich2x-${MV2X_VERSION} /usr/share/Modules/modulefiles/mpi/mvapich2x
 

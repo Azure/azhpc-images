e1f5ebd018fb872f18a789f9c3c9f0855d3e787b
Souvik De <souvikde@microsoft.com>
2021-01-21 10:48:30 -0800

    Updated MKL to 2021.1.1

--- common/install_intel_libs.sh
+++ common/install_intel_libs.sh
@@ -9,6 +9,7 @@ set -ex
 # sed -i -e 's/ACCEPT_EULA=decline/ACCEPT_EULA=accept/g' silent.cfg
 # ./install.sh --silent ./silent.cfg
 
+
 MKL_DOWNLOAD_URL=https://registrationcenter-download.intel.com/akdlm/irc_nas/17402/l_onemkl_p_2021.1.1.52_offline.sh
 $COMMON_DIR/download_and_verify.sh $MKL_DOWNLOAD_URL "818b6bd9a6c116f4578cda3151da0612ec9c3ce8b2c8a64730d625ce5b13cc0c"
 sudo bash l_onemkl_p_2021.1.1.52_offline.sh -s -a -s --eula accept

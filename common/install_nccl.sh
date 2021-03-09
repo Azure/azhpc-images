d2059f83b887e06f99deb3694b0cb50503ba2c61
Souvik De <souvikde@microsoft.com>
2021-03-08 18:59:23 -0800

    Replaced HPCX explicit path with env var

--- common/install_nccl.sh
+++ common/install_nccl.sh
@@ -32,5 +32,5 @@ sudo mkdir -p /opt/microsoft
 cd /opt/microsoft
 sudo git clone https://github.com/NVIDIA/nccl-tests.git
 cd /opt/microsoft/nccl-tests
-make MPI=1 MPI_HOME=/opt/hpcx-v2.7.4-gcc-MLNX_OFED_LINUX-5.2-1.0.4.0-ubuntu18.04-x86_64/ompi CUDA_HOME=/usr/local/cuda
+make MPI=1 MPI_HOME=${HPCX_MPI_DIR} CUDA_HOME=/usr/local/cuda
 module unload mpi/hpcx

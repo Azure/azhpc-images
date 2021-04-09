ee1e8461c681b89910abd965e8155d01db5ad2ac
Souvik De <souvikde@microsoft.com>
2021-04-09 10:17:56 -0700

    Updated IMPI to 2021.2.0; DCGM to 2.1.7; Updated Tests

--- common/install_dcgm.sh
+++ common/install_dcgm.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # Install DCGM
-DCGM_VERSION=2.1.4
-DCGM_GPUMNGR_URL=https://developer.download.nvidia.com/compute/redist/dcgm/${DCGM_VERSION}/DEBS/datacenter-gpu-manager_${DCGM_VERSION}_amd64.deb
-$COMMON_DIR/download_and_verify.sh $DCGM_GPUMNGR_URL "eac676359ce6ba6682b480fca00e74c6526c2c01421c345f2452857d84de2daa"
+DCGM_VERSION=2.1.7
+DCGM_GPUMNGR_URL=https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/datacenter-gpu-manager_${DCGM_VERSION}_amd64.deb
+$COMMON_DIR/download_and_verify.sh $DCGM_GPUMNGR_URL "c55591b3f8ce66dc6215f1f40c6e7debdd557469ad911532642fd8622124e08f"
 sudo dpkg -i datacenter-gpu-manager_*.deb && \
 sudo rm -f datacenter-gpu-manager_*.deb
 

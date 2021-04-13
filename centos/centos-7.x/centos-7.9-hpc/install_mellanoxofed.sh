919abb4324e0cac3314ab1f6e006394854e706b4
Anshul jain <ansja@microsoft.com>
2021-04-13 13:42:50 -0700

    Updating MLNX OFED and HPCX Drivers for CentOS 7.9

--- centos/centos-7.x/centos-7.9-hpc/install_mellanoxofed.sh
+++ centos/centos-7.x/centos-7.9-hpc/install_mellanoxofed.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 set -ex
 
-MLNX_OFED_DOWNLOAD_URL=https://azhpcstor.blob.core.windows.net/azhpc-images-store/MLNX_OFED_LINUX-5.2-1.0.4.0-rhel7.9-x86_64.tgz
+MLNX_OFED_DOWNLOAD_URL=https://azhpcstor.blob.core.windows.net/azhpc-images-store/MLNX_OFED_LINUX-5.2-2.2.3.0-rhel7.9-x86_64.tgz
 TARBALL=$(basename ${MLNX_OFED_DOWNLOAD_URL})
 MOFED_FOLDER=$(basename ${MLNX_OFED_DOWNLOAD_URL} .tgz)
 
-$COMMON_DIR/download_and_verify.sh $MLNX_OFED_DOWNLOAD_URL "eaefb19d8820153715436fb8df003ea9e1675c75165b515fcdbbed60d0551034"
+$COMMON_DIR/download_and_verify.sh $MLNX_OFED_DOWNLOAD_URL "988ecf8b8f537cf6d9e78c94aa30142b477000938b1ddfb412aac20b53d1b236"
 tar zxvf ${TARBALL}
 
 KERNEL=( $(rpm -q kernel | sed 's/kernel\-//g') )

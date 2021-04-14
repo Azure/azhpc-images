7aed4a0cfd3891c9a44a80ecd45730a7dfc175d6
Anshul Jain <63129380+anshuljainansja@users.noreply.github.com>
2021-04-14 16:23:28 -0700

    Removing sudo and fixing file permissions (#76)

--- common/install_nvidiagpudriver.sh
+++ common/install_nvidiagpudriver.sh
@@ -5,7 +5,7 @@ set -ex
 CUDA_URL=https://developer.download.nvidia.com/compute/cuda/11.2.2/local_installers/cuda_11.2.2_460.32.03_linux.run
 $COMMON_DIR/download_and_verify.sh $CUDA_URL "0a2e477224af7f6003b49edfd2bfee07667a8148fe3627cfd2765f6ad72fa19d"
 chmod +x cuda_11.2.2_460.32.03_linux.run
-sudo ./cuda_11.2.2_460.32.03_linux.run --silent
+sh cuda_11.2.2_460.32.03_linux.run --silent
 echo 'export PATH=$PATH:/usr/local/cuda/bin' | sudo tee -a /etc/bash.bashrc
 echo 'export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/cuda/lib64' | sudo tee -a /etc/bash.bashrc
 
--- ubuntu/ubuntu-18.x/ubuntu-18.04-hpc/install_nvidiagpudriver.sh
+++ ubuntu/ubuntu-18.x/ubuntu-18.04-hpc/install_nvidiagpudriver.sh
@@ -6,6 +6,6 @@ $UBUNTU_COMMON_DIR/install_nvidiagpudriver.sh
 # Install nvidia fabric manager (required for ND96asr_v4)
 NVIDIA_FABRIC_MNGR_URL=http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/nvidia-fabricmanager-460_460.32.03-1_amd64.deb
 $COMMON_DIR/download_and_verify.sh $NVIDIA_FABRIC_MNGR_URL "157da63c4f7823bb5ae4428891978345a079830629e23579e0c3126f42a4411c"
-sudo apt install -y ./nvidia-fabricmanager-460_460.32.03-1_amd64.deb
-sudo systemctl enable nvidia-fabricmanager
-sudo systemctl start nvidia-fabricmanager
+apt install -y ./nvidia-fabricmanager-460_460.32.03-1_amd64.deb
+systemctl enable nvidia-fabricmanager
+systemctl start nvidia-fabricmanager

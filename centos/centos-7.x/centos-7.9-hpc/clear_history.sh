c0a47740d21a70a0f939dec913578f5c36241dd0
Christine Tai <christinetai@microsoft.com>
2021-07-29 12:16:35 -0700

    zero out unused space

--- centos/centos-7.x/centos-7.9-hpc/clear_history.sh
+++ centos/centos-7.x/centos-7.9-hpc/clear_history.sh
@@ -4,8 +4,7 @@ set -ex
 # Clear History
 sudo rm -rf /var/log/*
 sudo rm -f /etc/resolv.conf /var/lib/NetworkManager/* /etc/ssh/ssh_host_*
-sudo rm -rf /tmp/tmp*
-sudo rm -f /tmp/*.log* /tmp/ssh-* /tmp/yum*
+sudo rm -rf /tmp/ssh-* /tmp/yum* /tmp/tmp* /tmp/*.log* /tmp/*tenant*
 sudo rm -f /var/spool/plymouth/boot.log 
 sudo rm -f /var/lib/dhclient/* /var/lib/systemd/random-seed
 sudo rm -rf /var/cache/*
@@ -14,4 +13,12 @@ unset HISTFILE
 history -c
 sudo rm -rf /run/cloud-init /var/lib/cloud/instances/*
 yum clean all
+
+# Zero out unused space to minimize actual disk usage
+for part in $(awk '$3 == "xfs" {print $2}' /proc/mounts)
+do
+    dd if=/dev/zero of=${part}/EMPTY bs=1M || true;
+    rm -f ${part}/EMPTY
+done
+sync;
 

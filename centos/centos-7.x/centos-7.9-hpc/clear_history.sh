b1f039ecaf6ac90130eea08a5ba05c5821b0cae7
Aditya Bhamidipati <87097035+abhamidipati0614@users.noreply.github.com>
2021-08-19 11:38:00 -0700

    Fix for DNS resolution for VMs (#98)

--- centos/centos-7.x/centos-7.9-hpc/clear_history.sh
+++ centos/centos-7.x/centos-7.9-hpc/clear_history.sh
@@ -2,16 +2,16 @@
 set -ex
 
 # Clear History
-sudo rm -rf /var/log/*
-sudo rm -f /etc/resolv.conf /var/lib/NetworkManager/* /etc/ssh/ssh_host_*
-sudo rm -rf /tmp/ssh-* /tmp/yum* /tmp/tmp* /tmp/*.log* /tmp/*tenant*
-sudo rm -f /var/spool/plymouth/boot.log 
-sudo rm -f /var/lib/dhclient/* /var/lib/systemd/random-seed
-sudo rm -rf /var/cache/*
+rm -rf /var/log/*
+rm -f /etc/ssh/ssh_host_*
+rm -rf /tmp/ssh-* /tmp/yum* /tmp/tmp* /tmp/*.log* /tmp/*tenant*
+rm -rf /tmp/nvidia* /tmp/MLNX* /tmp/ofed.conf /tmp/dkms* /tmp/*mlnx*
+rm -f /var/lib/systemd/random-seed
+rm -rf /var/cache/*
 unset HISTFILE
 #rm -f /root/.bash_history
 history -c
-sudo rm -rf /run/cloud-init /var/lib/cloud/instances/*
+rm -rf /run/cloud-init /var/lib/cloud/instances/*
 yum clean all
 
 # Zero out unused space to minimize actual disk usage

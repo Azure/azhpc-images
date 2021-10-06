1044c550a0711cb53df4c9e4a0998841470e102e
Aditya Bhamidipati <abhamidipati@microsoft.com>
2021-10-05 21:52:29 -0700

    MOFED-LTS; MLNX_OFED_LINUX-4.9-3.1.5.0; HPC-x v2.7.0

---
+++ ubuntu/ubuntu-18.x/ubuntu-18.04-LTS-hpc/install_nvidiagpudriver.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+set -ex
+
+$COMMON_DIR/install_nvidiagpudriver.sh
---
+++ ubuntu/ubuntu-18.x/ubuntu-18.04-LTS-hpc/set_properties.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+export TOP_DIR=../../..
+export COMMON_DIR=../../../common
+export UBUNTU_COMMON_DIR=../../common
+export TEST_DIR=../../../tests

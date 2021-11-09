8adc26f6c4ab1a7a5ddea09430767a7dd22d638d
Aditya Bhamidipati <abhamidipati@microsoft.com>
2021-11-09 15:59:28 -0800

    nvidia driver 495.29.05, CUDA 11.5.0, NCCL v2.11.4-1, DCGM 2.3.1 update for Ubuntu 18.04, 20.04 and CentOS 7.9

--- ubuntu/common/install_dcgm.sh
+++ ubuntu/common/install_dcgm.sh
@@ -6,12 +6,12 @@ set -ex
 VERSION=$1
 
 # Install DCGM
-DCGM_VERSION=2.1.7
+DCGM_VERSION=2.3.1
 $COMMON_DIR/write_component_version.sh "DCGM" ${DCGM_VERSION}
 DCGM_GPUMNGR_URL=https://developer.download.nvidia.com/compute/cuda/repos/ubuntu${VERSION}/x86_64/datacenter-gpu-manager_${DCGM_VERSION}_amd64.deb
-$COMMON_DIR/download_and_verify.sh $DCGM_GPUMNGR_URL "c55591b3f8ce66dc6215f1f40c6e7debdd557469ad911532642fd8622124e08f"
-dpkg -i datacenter-gpu-manager_*.deb && \
-rm -f datacenter-gpu-manager_*.deb
+$COMMON_DIR/download_and_verify.sh $DCGM_GPUMNGR_URL "0431dc987d3e67e6193b47c40ce71be443069c49adaa91ea0b904629b594a12c"
+dpkg -i datacenter-gpu-manager_${DCGM_VERSION}_amd64.deb && \
+rm -f datacenter-gpu-manager_${DCGM_VERSION}_amd64.deb
 
 # Create service for dcgm to launch on bootup
 bash -c "cat > /etc/systemd/system/dcgm.service" <<'EOF'

3a9a5fc62d89e0f1e4968444f4064deb76c1dd10
Aditya Bhamidipati <87097035+abhamidipati0614@users.noreply.github.com>
2021-11-12 18:04:25 -0500

    fix for version mismatch between kernel-devel, kernel-modules-extra (#113)

--- centos/centos-8.x/centos-8.1-hpc/install_mellanoxofed.sh
+++ centos/centos-8.x/centos-8.1-hpc/install_mellanoxofed.sh
@@ -10,13 +10,18 @@ MOFED_FOLDER=$(basename ${MLNX_OFED_DOWNLOAD_URL} .tgz)
 $COMMON_DIR/download_and_verify.sh $MLNX_OFED_DOWNLOAD_URL "171b304d76f2e88ec62138b01691a5580c5fa3674209f262592669c637891c13"
 tar zxvf ${TARBALL}
 
-KERNEL=( $(rpm -q kernel | sed 's/kernel\-//g') )
-KERNEL=${KERNEL[-1]}
 # Uncomment the lines below if you are running this on a VM
+#KERNEL=( $(rpm -q kernel | sed 's/kernel\-//g') )
+#KERNEL=${KERNEL[-1]}
 #RELEASE=( $(cat /etc/centos-release | awk '{print $4}') )
 #yum install -y http://olcentwus.cloudapp.net/centos/${RELEASE}/BaseOS/x86_64/os/kernel-devel-${KERNEL}.rpm
-yum install -y kernel-devel-${KERNEL}
+#yum install -y http://olcentwus.cloudapp.net/centos/${RELEASE}/BaseOS/x86_64/os/kernel-modules-extra-${KERNEL}.rpm
+
+yum install -y kernel-devel
 yum install -y kernel-modules-extra
+KERNEL=( $(rpm -q kernel-devel | sed 's/kernel-devel\-//g') )
+KERNEL=${KERNEL[-1]}
+
 ./${MOFED_FOLDER}/mlnxofedinstall --kernel $KERNEL --kernel-sources /usr/src/kernels/${KERNEL} --add-kernel-support --skip-repo
 
 # Restarting openibd

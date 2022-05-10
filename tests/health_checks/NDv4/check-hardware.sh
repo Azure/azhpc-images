6d2af649918096d6d6d7362d481e284f62efa049
Scott Moe <moes1@noreply.com>
2022-05-10 15:53:11 -0700

    Added error checking and time limits for external commands called from dcgmi-check.sh and ecc-errors.sh.

--- tests/health_checks/NDv4/check-hardware.sh
+++ tests/health_checks/NDv4/check-hardware.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+##This is a test to verify an NDv4 vm is seeing the expected number of GPUs
+##and NICs.
+
+#Catch error codes that may be thrown by the executable passed as the first
+#input, and if an error code is tripped throw the second input as a message
 catch_error() {
 	output=$($1)
 	err_code=$?
@@ -12,17 +17,17 @@ catch_error() {
 #Store output from calls passed to catch_error
 output=""
 
-#Count the number of gpu-names nvidia-smi outputs.
-executable="sudo timeout 3m nvidia-smi --query-gpu=name --format=csv,noheader"
-error="Fail: nvidia-smi failed counting GPUs with error code"
-catch_error "$executable" "$error"
+#Count the number of gpu-name nvidia-smi outputs.
+gpu_names="sudo timeout 3m nvidia-smi --query-gpu=name --format=csv,noheader"
+error_smi="Fail: nvidia-smi failed with error code"
+catch_error "$gpu_names" "$error_smi"
 ngpus=$(echo "$output" | wc -l)
 
 
 #Count the number of nics lshw detects.
-executable="sudo timeout 3m lshw -C network"
-error="Fail: lshw failed with error code"
-catch_error "$executable" "$error"
+find_nics="sudo timeout 3m lshw -C network"
+error_nics="Fail: lshw failed with error code"
+catch_error "$find_nics" "$error_nics"
 nnics=$(echo "$output" | grep -i ConnectX-6 | wc -l)
 
 #Did either test fail?
@@ -40,6 +45,7 @@ fi
 
 if [ $passed -ne 1 ]; then
 	echo "Fail: Error when detecting GPUs and NICs."
+	exit 1;
 else
 	echo "Correct number of GPUs and NICs detected."
 fi

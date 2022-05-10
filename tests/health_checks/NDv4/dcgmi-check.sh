3c626c756651fce78dc39de9846b3352f3f09f7c
Scott Moe <moes1@noreply.com>
2022-05-09 23:43:41 -0700

    Updating the dcgmi-check script to check if a GPU has persistence mode set, if it does not enable it, and then disable it after testing.

--- tests/health_checks/NDv4/dcgmi-check.sh
+++ tests/health_checks/NDv4/dcgmi-check.sh
@@ -1,10 +1,31 @@
 #!/bin/bash
 
+#get the number of GPUs
+ngpus=$(sudo nvidia-smi --query-gpu=name --format=csv,noheader | wc -l)
+#array to hold status of persistence mode for each gpu
+declare -a pmvals=()
+
+#Find which GPUs have persistence mode off, record that information in pmvals.
+#Enable persistence mode on GPUs that have it disabled. 
+for i in $(seq 0 $((ngpus-1))); do
+	pmvals[$i]=$(nvidia-smi -i $i -q | grep "Persistence Mode" \
+		| awk '{print $4}')
+	if [ ${pmvals[$i]} == "Disabled" ]; then
+		sudo nvidia-smi -i $i -pm 1 > /dev/null
+	fi	       
+done
+
 #Turn on persistence mode.
-sudo nvidia-smi -pm 1 > /dev/null
 
 #Run the basic (r=1) dcgmi diag tests.
 fails=$(dcgmi diag -r 1 | grep "Fail" | wc -l)
+
+#Disable persistence mode on GPUs that started with it disabled.
+for i in $(seq 0 $((ngpus-1))); do
+	if [ ${pmvals[$i]} == "Disabled" ]; then
+	       sudo nvidia-smi -i $i -pm 0 > /dev/null
+	fi	       
+done
 
 if [ $fails -gt 0 ]; then
 	echo "Fail: dcgmi tests have hit errors. Execute the command"\

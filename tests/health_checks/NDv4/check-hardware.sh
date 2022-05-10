0807f9006221ac50b9b3e2771455a2eb0bb0a498
Scott Moe <moes1@noreply.com>
2022-05-10 13:39:04 -0700

    Add error checking and add a time limit to external commands called by check-hardware.sh

--- tests/health_checks/NDv4/check-hardware.sh
+++ tests/health_checks/NDv4/check-hardware.sh
@@ -1,10 +1,29 @@
 #!/bin/bash
 
+catch_error() {
+	output=$($1)
+	err_code=$?
+	if [ $err_code -ne 0 ]; then
+		echo "$2 $err_code" >&2
+		exit $err_code;
+	fi
+}
+
+#Store output from calls passed to catch_error
+output=""
+
 #Count the number of gpu-names nvidia-smi outputs.
-ngpus=$(sudo nvidia-smi --query-gpu=name --format=csv,noheader | wc -l)
+executable="sudo timeout 3m nvidia-smi --query-gpu=name --format=csv,noheader"
+error="Fail: nvidia-smi failed counting GPUs with error code"
+catch_error "$executable" "$error"
+ngpus=$(echo "$output" | wc -l)
+
 
 #Count the number of nics lshw detects.
-nnics=$(sudo lshw -C network | grep -i ConnectX-6 | wc -l)
+executable="sudo timeout 3m lshw -C network"
+error="Fail: lshw failed with error code"
+catch_error "$executable" "$error"
+nnics=$(echo "$output" | grep -i ConnectX-6 | wc -l)
 
 #Did either test fail?
 passed=1

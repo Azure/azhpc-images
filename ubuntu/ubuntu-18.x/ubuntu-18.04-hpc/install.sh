4cc29cd0f994649cd7b6b9799b0470c6c65e28ec
rafsalas19 <70273488+rafsalas19@users.noreply.github.com>
2022-06-01 11:07:50 -0400

    Moneo (#128)
    * adding moneo submodule and scripts
    
    * updating way to pull submodule Moneo so that the contents show up in Moneo directory
    
    * Adding install script for moneo
    
    * removed monitor tools install from centos

--- ubuntu/ubuntu-18.x/ubuntu-18.04-hpc/install.sh
+++ ubuntu/ubuntu-18.x/ubuntu-18.04-hpc/install.sh
@@ -47,6 +47,9 @@ $COMMON_DIR/network-tuning.sh
 # copy test file
 $COMMON_DIR/copy_test_file.sh
 
+# install monitor tools
+$COMMON_DIR/install_monitoring_tools.sh
+
 # diable auto kernel updates
 $UBUNTU_COMMON_DIR/disable_auto_upgrade.sh
 

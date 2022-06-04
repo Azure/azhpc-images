d3b0361cc1728795fd6da022701d60ac5b80777c
jamesongithub@users.noreply.github.com <jamesongithub@users.noreply.github.com>
2022-06-03 18:46:46 -0700

    rm unused lib

--- suse/sle-15-spx-hpc/install_utils.sh
+++ suse/sle-15-spx-hpc/install_utils.sh
@@ -1,40 +1,10 @@
 #!/bin/bash
 set -ex
 
-zypper update --no-confirm
-
 # Install pre-reqs and development tools
 
 zypper install --no-confirm \
     bzip2
-#     numactl-devel \
-#     libxml2-devel \
-#     byacc \
-#     environment-modules \
-#     gtk2 \
-#     atk \
-#     cairo \
-#     tcl \
-#     tk \
-#     m4 \
-#     tcsh \
-#     gcc-gfortran \
-#     python36-devel \
-#     elfutils-libelf-devel \
-#     kernel-rpm-macros \
-#     glibc-devel \
-#     libudev-devel \
-#     binutils \
-#     binutils-devel \
-#     selinux-policy-devel \
-#     kernel-headers \
-#     nfs-utils \
-#     fuse-libs \
-#     libpciaccess \
-#     cmake \
-#     libnl3-devel \
-#     libarchive \
-#     libsecret
 
 # Install azcopy tool 
 # To copy blobs or files to or from a storage account.

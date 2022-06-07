2dc2d6875ade51929680d5139a4905f9a96d9a5c
Scott Moe <moes1@noreply.com>
2022-06-07 16:50:59 -0700

    rolling back use of 5.1.3 until I can do more testing

--- ubuntu/ubuntu-20.x/ubuntu-20.04-hpc/install_rocm.sh
+++ ubuntu/ubuntu-20.x/ubuntu-20.04-hpc/install_rocm.sh
@@ -11,11 +11,11 @@ sudo apt install -y $pack
 wget -q -O - https://repo.radeon.com/rocm/rocm.gpg.key | sudo apt-key add -
 
 
-sudo apt-get update
-amddeb="https://repo.radeon.com/amdgpu-install/22.10.3/ubuntu/focal/"
-amddeb+="amdgpu-install_22.10.3.50103-1_all.deb"
+wget -q -O - https://repo.radeon.com/rocm/rocm.gpg.key | sudo apt-key add -
+amddeb="https://repo.radeon.com/amdgpu-install/22.10.1/ubuntu/focal/"
+amddeb+="amdgpu-install_22.10.1.50101-1_all.deb"
 wget $amddeb
-sudo apt-get install -y ./amdgpu-install_22.10.3.50103-1_all.deb
+sudo apt-get install -y ./amdgpu-install_22.10.1.50101-1_all.deb
 sudo amdgpu-install -y --usecase=rocm
 
 
@@ -41,19 +41,3 @@ cat /etc/default/grub.d/50-cloudimg-settings.cfg | sed -e "s/$line/$string/" > t
 sudo mv temp_file.txt /etc/default/grub.d/50-cloudimg-settings.cfg
 sudo update-grub
 
-#echo -e '#!/usr/bin/bash\n\nsleep 120s\nsudo modprobe amdgpu' | sudo tee rocmstartup.sh
-#
-#sudo mv rocmstartup.sh /usr/local/bin/rocmstartup.sh
-#sudo chown :render /usr/local/bin/rocmstartup.sh
-#sudo chmod g+x /usr/local/bin/rocmstartup.sh
-#
-#echo -e '[Unit]\n\nDescription=Runs /usr/local/bin/rocmstartup.sh\n\n' \
-#        | sudo tee rocmstartup.service
-#echo -e '[Service]\n\nExecStart=/usr/local/bin/rocmstartup.sh\n\n' \
-#        | sudo tee -a rocmstartup.service
-#echo -e '[Install]\n\nWantedBy=multi-user.target' \
-#        | sudo tee -a rocmstartup.service
-#
-#sudo mv rocmstartup.service /etc/systemd/system/rocmstartup.service
-#sudo systemctl start rocmstartup
-#sudo systemctl enable rocmstartup

5bb90502ec82adb9841fa7f41c80ed2a04ad4a55
Aditya Bhamidipati <87097035+abhamidipati0614@users.noreply.github.com>
2022-06-14 13:42:23 -0700

    dcgm update v2.4.4; NCCL update v2.12.12 (#138)

--- ubuntu/common/install_dcgm.sh
+++ ubuntu/common/install_dcgm.sh
@@ -6,9 +6,9 @@ set -ex
 VERSION=$1
 
 # Install DCGM
-DCGM_VERSION=2.3.6
-DCGM_GPUMNGR_URL=https://developer.download.nvidia.com/compute/cuda/repos/ubuntu${VERSION}/x86_64/datacenter-gpu-manager_${DCGM_VERSION}_amd64.deb
-$COMMON_DIR/download_and_verify.sh $DCGM_GPUMNGR_URL "d43955818b37fa80744eff75b84b71cc4c43c22a024cecfe9cbc3dc279705a6e"
+DCGM_VERSION=2.4.4
+DCGM_GPUMNGR_URL=https://azhpcstor.blob.core.windows.net/azhpc-images-store/datacenter-gpu-manager_${DCGM_VERSION}_amd64.deb
+$COMMON_DIR/download_and_verify.sh $DCGM_GPUMNGR_URL "69ba98bbc4f657f6a15a2922aee0ea6b495fad49147d056a8f442c531b885e0e"
 dpkg -i datacenter-gpu-manager_${DCGM_VERSION}_amd64.deb && \
 rm -f datacenter-gpu-manager_${DCGM_VERSION}_amd64.deb
 $COMMON_DIR/write_component_version.sh "DCGM" ${DCGM_VERSION}

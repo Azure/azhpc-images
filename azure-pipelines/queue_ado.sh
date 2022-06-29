e36e09994fefd23999b16de1562c4cb9817aae32
Yakov Dyadkin <yakovdyad@gmail.com>
2022-06-29 13:45:46 -0500

    Check for succesful post & use 1ES agent pool

--- azure-pipelines/queue_ado.sh
+++ azure-pipelines/queue_ado.sh
@@ -13,15 +13,22 @@ get_builds_res () {
 
 
 # Queue a build
-post_build_res=$(curl -s -X POST "https://dev.azure.com/hpc-platform-team/hpc-image-val/_apis/build/builds?api-version=6.0" \
+res_code=$(curl -s -w "%{response_code}\n" -o post.json -X POST "https://dev.azure.com/hpc-platform-team/hpc-image-val/_apis/build/builds?api-version=6.0" \
 -H "Authorization: Bearer $SYSTEM_ACCESSTOKEN" -H "Content-Type: application/json" \
 --data-raw "{\"definition\": {\"id\": 3}, \"sourceBranch\": \"github_pr\", \"parameters\": \"{\\\"PR_NUM\\\": \\\"$PR_NUM\\\"}\"}")
+
+if [ $res_code -ne 200 ]
+then
+    echo "Error! Could not queue build. Response code: $res_code"
+    exit 1
+fi
 
 echo "Queued build!"
 
 # Get URL for queued AzDo build
+post_build_res=$(cat ./post.json)
 build_url=$(echo $post_build_res | jq -r "._links.self.href")
-echo "Build url: $build_url"
+echo "Build url: $(echo $post_build_res | jq -r "._links.web.href")"
 
 # Wait until build finishes
 build_status=$(get_builds_res | jq -r ".status")

5075b19ab5312140d3ce9b8ab82622fc3fb07357
Scott Moe <moes1@noreply.com>
2022-08-29 18:20:12 -0700

    Updated nvidia gpu driver to 470.141.03 to fix a infoROM corruption issue

--- ubuntu/ubuntu-18.x/ubuntu-18.04-LTS-hpc/install_nvidiagpudriver.sh
+++ ubuntu/ubuntu-18.x/ubuntu-18.04-LTS-hpc/install_nvidiagpudriver.sh
@@ -7,7 +7,7 @@ CHECKSUM="749183821ffc051e123f12ebdeb171b263d55b86f0dd7c8f23611db1802d6c37"
 
 # Reference - https://docs.nvidia.com/cuda/cuda-installation-guide-linux/index.html#ubuntu-installation
 # Install Cuda
-NVIDIA_VERSION="470.82.01"
+NVIDIA_VERSION="470.141.03"
 CUDA_VERSION="11.4.3"
 $COMMON_DIR/write_component_version.sh "CUDA" ${CUDA_VERSION}
 CUDA_URL=https://developer.download.nvidia.com/compute/cuda/${CUDA_VERSION}/local_installers/cuda_${CUDA_VERSION}_${NVIDIA_VERSION}_linux.run
@@ -30,6 +30,6 @@ popd
 
 # Nvidia driver
 NVIDIA_DRIVER_URL=https://download.nvidia.com/XFree86/Linux-x86_64/${NVIDIA_VERSION}/NVIDIA-Linux-x86_64-${NVIDIA_VERSION}.run
-$COMMON_DIR/download_and_verify.sh $NVIDIA_DRIVER_URL "ad78fc2b29b8f498015277e30ae42530b61fecc298706bc0e9c193ee5e9c0660"
+$COMMON_DIR/download_and_verify.sh $NVIDIA_DRIVER_URL "be98d247a43d7491a65bfdc997fb6531e1594346eb12a0faaa044672cdb5709f"
 bash NVIDIA-Linux-x86_64-${NVIDIA_VERSION}.run --silent --dkms
 $COMMON_DIR/write_component_version.sh "NVIDIA" ${NVIDIA_VERSION}

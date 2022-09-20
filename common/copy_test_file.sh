d4502ca7bfc6797dc35d964340c4134f929be116
Scott Moe <moes1@noreply.com>
2022-09-19 17:18:11 -0700

    Adding back compilation of gpu-copy executable at vm setup just so the health checks are completely setup when intallation completes. Also fixing a small bug in the ecc-errors script and making an error message created by the test-bandwidth more clear.

--- common/copy_test_file.sh
+++ common/copy_test_file.sh
@@ -7,4 +7,23 @@ mkdir -p $DEST_TEST_DIR
 
 cp $TEST_DIR/run-tests.sh $DEST_TEST_DIR
 
+#Test if nvcc is installed and if so install gpu-copy test.
+if test -f "/usr/local/cuda/bin/nvcc"; then
+	#Compile the gpu-copy benchmark.
+	NVCC=/usr/local/cuda/bin/nvcc
+	cufile="$TEST_DIR/health_checks/NDv4/gpu-copy.cu"
+	outfile="$TEST_DIR/health_checks/NDv4/gpu-copy"
+
+	#Test if the default gcc compiler is new enough to compile gpu-copy.
+	#If it is not then use the 9.2 compiler, that should be installed in
+	#/opt.
+	if [ $(gcc -dumpversion | cut -d. -f1) -gt 6 ]; then
+		$NVCC -lnuma $cufile -o $outfile
+	else
+		$NVCC --compiler-bindir /opt/gcc-9.2.0/bin \
+			-lnuma $cufile -o $outfile
+	fi
+fi
+cp -r $TEST_DIR/health_checks $DEST_TEST_DIR
+
 exit 0

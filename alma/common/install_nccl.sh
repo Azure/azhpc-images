68c07ed60851d698390cfe825c8188b574598201
Aditya Bhamidipati <abhamidipati@microsoft.com>
2022-09-28 20:22:16 -0400

    NVIDIA and NCCL installation for AlmaLinux 8.6

---
+++ alma/common/install_nccl.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+set -ex
+
+# Install NCCL
+yum install -y rpm-build rpmdevtools
+NCCL_VERSION="2.14.3-1"
+TARBALL="v${NCCL_VERSION}.tar.gz"
+NCCL_DOWNLOAD_URL=https://github.com/NVIDIA/nccl/archive/refs/tags/${TARBALL}
+pushd /tmp
+wget ${NCCL_DOWNLOAD_URL}
+tar -xvf ${TARBALL}
+
+pushd nccl-${NCCL_VERSION}
+make -j src.build
+make pkg.redhat.build
+rpm -i ./build/pkg/rpm/x86_64/libnccl-${NCCL_VERSION}+cuda11.6.x86_64.rpm
+echo "exclude=libnccl" | tee -a /etc/yum.conf
+rpm -i ./build/pkg/rpm/x86_64/libnccl-devel-${NCCL_VERSION}+cuda11.6.x86_64.rpm
+echo "exclude=libnccl-devel" | tee -a /etc/yum.conf
+rpm -i ./build/pkg/rpm/x86_64/libnccl-static-${NCCL_VERSION}+cuda11.6.x86_64.rpm
+echo "exclude=libnccl-static" | tee -a /etc/yum.conf
+popd
+
+# Install the nccl rdma sharp plugin
+mkdir -p /usr/local/nccl-rdma-sharp-plugins
+git clone https://github.com/Mellanox/nccl-rdma-sharp-plugins.git
+pushd nccl-rdma-sharp-plugins
+./autogen.sh
+./configure --prefix=/usr/local/nccl-rdma-sharp-plugins --with-cuda=/usr/local/cuda
+make
+make install
+popd
+popd
+
+# Build the nccl tests
+source /etc/profile.d/modules.sh
+module load mpi/hpcx
+git clone https://github.com/NVIDIA/nccl-tests.git
+pushd nccl-tests
+make MPI=1 MPI_HOME=${HPCX_MPI_DIR} CUDA_HOME=/usr/local/cuda
+popd
+mv nccl-tests /opt/.
+module unload mpi/hpcx
+$COMMON_DIR/write_component_version.sh "NCCL" ${NCCL_VERSION}
+
+# Remove installation files
+rm -rf /tmp/${TARBALL}
+rm -rf /tmp/nccl-${NCCL_VERSION}

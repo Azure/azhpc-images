d893614acf324e6c369b60f1aadfd185e214222b
Jithin Jose <jijos@microsoft.com>
2022-10-14 09:56:12 -0500

    Ubuntu updates (#173)
    Co-authored-by: Ubuntu <jijos@h100hpc.pxrbrzfpuxmepfgrsrhz2vsf2b.ax.internal.cloudapp.net>

--- ubuntu/common/install_nvidia_fabric_manager.sh
+++ ubuntu/common/install_nvidia_fabric_manager.sh
@@ -6,9 +6,9 @@ set -ex
 VERSION=$1
 
 # Install nvidia fabric manager
-NVIDIA_FABRIC_MANAGER_VERSION="510_510.85.02-1"
+NVIDIA_FABRIC_MANAGER_VERSION="520_520.61.05-1"
 NVIDIA_FABRIC_MNGR_URL=http://developer.download.nvidia.com/compute/cuda/repos/ubuntu${VERSION}/x86_64/nvidia-fabricmanager-${NVIDIA_FABRIC_MANAGER_VERSION}_amd64.deb
-$COMMON_DIR/download_and_verify.sh $NVIDIA_FABRIC_MNGR_URL "64634872a8ae79e12a8eb4ae476fda8e4c32b57279475b8e299589eeaae5b1a2"
+$COMMON_DIR/download_and_verify.sh $NVIDIA_FABRIC_MNGR_URL "8beeb76ade836327298337b4f65eb3906498cebe71ab35bca5bb1a638c1bfd0a"
 apt install -y ./nvidia-fabricmanager-${NVIDIA_FABRIC_MANAGER_VERSION}_amd64.deb
-sudo apt-mark hold nvidia-fabricmanager-510
+sudo apt-mark hold nvidia-fabricmanager-520
 $COMMON_DIR/write_component_version.sh "NVIDIA_FABRIC_MANAGER" ${NVIDIA_FABRIC_MANAGER_VERSION}

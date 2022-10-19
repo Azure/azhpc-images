056aee12abc6fc16833bb1c44f25d417f78c71e0
Aditya Bhamidipati <87097035+abhamidipati0614@users.noreply.github.com>
2022-10-19 12:40:31 -0700

    azcopy installation fix for centos and ubuntu scripts; Status badge fix (#175)

--- centos/centos-8.x/common/install_utils.sh
+++ centos/centos-8.x/common/install_utils.sh
@@ -35,7 +35,7 @@ yum install -y numactl \
 
 # Install azcopy tool 
 # To copy blobs or files to or from a storage account.
-wget https://azcopyvnextrelease.blob.core.windows.net/release20210920/azcopy_linux_se_amd64_10.12.2.tar.gz
+wget https://azhpcstor.blob.core.windows.net/azhpc-images-store/azcopy_linux_se_amd64_10.12.2.tar.gz
 tar -xvf azcopy_linux_se_amd64_10.12.2.tar.gz
 
 # copy the azcopy to the bin path
@@ -45,3 +45,6 @@ popd
 
 # Allow execute permissions
 chmod +x /usr/bin/azcopy
+
+# remove tarball from azcopy
+rm -rf *.tar.gz

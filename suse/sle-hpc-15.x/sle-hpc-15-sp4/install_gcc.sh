a157d16e96194bb1246fda6a93d5e6f241e7b46a
Peter <18663039+petersatsuse@users.noreply.github.com>
2022-10-21 17:14:41 +0200

    GCC and MOFED installation for SLE HPC 15 SP4

---
+++ suse/sle-hpc-15.x/sle-hpc-15-sp4/install_gcc.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# With SLE HPC 15 SP4 we have gcc7 and gcc11 provided as the base compiler toolchain
+# and requires lua-lmod to supply environment module support.
+# the default is gnu/7 within package gnu-compilers-hpc, gcc11 is in gnu11-compilers-hpc
+# MODULE_FILES_DIRECTORY=/usr/share/lmod/modulefiles
+
+zypper in gnu-compilers-hpc
+module load gnu/7

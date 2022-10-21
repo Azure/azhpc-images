df8e3b389caae51ab9bb7d99058619b00640b391
Peter <18663039+petersatsuse@users.noreply.github.com>
2022-10-21 17:53:20 +0200

    README and single node tests for SLE HPC 15 SP4

---
+++ suse/sle-hpc-15.x/sle-hpc-15-sp4/README.md
@@ -0,0 +1,57 @@
+# SUSE Linux Enterprise HPC 15 SP4 Image
+
+The SLE HPC 15 SP4 includes optimizations and recommended configurations to deliver optimal performance,
+consistency, and reliability.
+As an enterprise distribution, we provide stable long term support and many certifications with vendors.
+
+See documentation at https://documentation.suse.com/sle-hpc/15-SP4/
+
+The environment module Lmod is installed for all roles.
+It is required at build time and runtime of the system.
+For more information, Section 7.1, “Lmod - Lua-based environment modules” within the documentation.
+
+Many components come per default with the distribution,
+but some are added like in the CentOS and Ubuntu images.
+
+All libraries specifically built for HPC are installed under /usr/lib/hpc.
+They are not part of the standard search path, so the Lmod environment module system is required.
+
+This image consists of the following HPC tools and libraries:
+
+- Mellanox OFED (inbox drivers)
+- Pre-configured IPoIB (IP-over-InfiniBand)
+- Popular InfiniBand based MPI Libraries
+
+  - HPC-X v.2.12
+  - IntelMPI (via Intel oneAPI) v2021.7.0
+  - MVAPICH2
+  - MPICH 4
+  - OpenMPI (v3 and v4)
+
+<!--
+- Communication Runtimes
+  - Libfabric
+  - OpenUCX
+-->
+- Optimized librares
+
+  - AMD (via tarball AOCL 3.1.0)
+      Blis
+      FFTW
+      Flame
+
+  - Intel MKL (Intel oneAPI via intel-oneapi-repo)
+    v2022.2.0
+
+  - Nvida drivers 520.61
+  - CUDA 11.8
+
+- Data Center GPU Manager 3.0.4
+- Azure HPC Diagnostics Tool
+
+- Docker
+- NVIDIA Docker 20.10.17_ce
+
+Software packages are configured as environment modules (lmod). Users can select preferred MPI or software packages as follows:
+`module load <package-name>`
+
--- tests/run-tests.sh
+++ tests/run-tests.sh
@@ -44,27 +44,35 @@ then
 else
     HPCX_VERSION_CENTOS="v2.9.0"
     HPCX_VERSION_UBUNTU="v2.11"
+    HPCX_VERSION_SUSE="2.12"  # remove the v for version so we can use the variable better
     MOFED_VERSION_UBUNTU="MLNX_OFED_LINUX-5.6-1.0.3.3"
     HPCX_MOFED_INTEGRATION_VERSION="MLNX_OFED_LINUX-5.4-1.0.3.0"
     HPCX_OMB_PATH_UBUNTU_1804="/opt/hpcx-${HPCX_VERSION_UBUNTU}-gcc-MLNX_OFED_LINUX-5-ubuntu18.04-cuda11-gdrcopy2-nccl2.11-x86_64/ompi/tests/osu-micro-benchmarks-5.8"
 fi
 
 MVAPICH2_VERSION_CENTOS="2.3.6"
-MVAPICH2_VERSION_ALMA="2.3.7"
 MVAPICH2_VERSION_UBUNTU="2.3.7"
+MVAPICH2_VERSION_SUSE="2.3.6"
+
 OMPI_VERSION_CENTOS="4.1.1"
-OMPI_VERSION_ALMA="4.1.3"
 OMPI_VERSION_UBUNTU="4.1.3"
+OMPI_VERSION_SUSE="4.1.1"
+
 IMPI_2021_VERSION_CENTOS="2021.4.0"
-IMPI_2021_VERSION_ALMA="2021.7.0"
-IMPI_2021_VERSION_UBUNTU="2021.7.0"
+IMPI_2021_VERSION_UBUNTU="2021.6.0"
+IMPI_2021_VERSION_SUSE="2021.7.0"
+
+
 MVAPICH2X_INSTALLATION_DIRECTORY="/opt/mvapich2-x"
 IMPI2018_PATH="/opt/intel/compilers_and_libraries_2018.5.274"
 
 MOFED_VERSION_CENTOS="MLNX_OFED_LINUX-5.4-1.0.3.0"
 MOFED_VERSION_CENTOS_79="MLNX_OFED_LINUX-5.4-3.0.0.0"
 MOFED_VERSION_CENTOS_83="MLNX_OFED_LINUX-5.2-1.0.4.0"
-MOFED_VERSION_ALMA_86="MLNX_OFED_LINUX-5.6-2.0.9.0"
+
+MOFED_VERSION_SUSE="MLNX_OFED_INBOX_5.14.21-4.0.0"
+#MOFED_VERSION_SUSE="MLNX_OFED-5.7-1.0.2.0"
+
 
 HPCX_OMB_PATH_CENTOS_76="/opt/hpcx-${HPCX_VERSION_CENTOS}-gcc${GCC_VERSION}-${MOFED_VERSION_CENTOS}-redhat7.6-x86_64/ompi/tests/osu-micro-benchmarks-5.6.2"
 HPCX_OMB_PATH_CENTOS_77="/opt/hpcx-${HPCX_VERSION_CENTOS}-gcc${GCC_VERSION}-${MOFED_VERSION_CENTOS}-redhat7.7-x86_64/ompi/tests/osu-micro-benchmarks-5.6.2"
@@ -72,24 +80,31 @@ HPCX_OMB_PATH_CENTOS_78="/opt/hpcx-${HPCX_VERSION_CENTOS}-gcc${GCC_VERSION}-${MO
 HPCX_OMB_PATH_CENTOS_79="/opt/hpcx-${HPCX_VERSION_CENTOS}-gcc${GCC_VERSION}-${HPCX_MOFED_INTEGRATION_VERSION}-redhat7.9-x86_64/ompi/tests/osu-micro-benchmarks-5.6.2"
 HPCX_OMB_PATH_CENTOS_81="/opt/hpcx-${HPCX_VERSION_CENTOS}-gcc${GCC_VERSION}-${MOFED_VERSION_CENTOS}-redhat8.1-x86_64/ompi/tests/osu-micro-benchmarks-5.6.2"
 HPCX_OMB_PATH_CENTOS_83="/opt/hpcx-v2.8.0-gcc-${MOFED_VERSION_CENTOS_83}-redhat8.3-x86_64/ompi/tests/osu-micro-benchmarks-5.6.2"
+
+#
+## Globals for the different versions, to simplify settings
+#
 MODULE_FILES_ROOT_CENTOS="/usr/share/Modules/modulefiles"
 IMPI2021_PATH_CENTOS="/opt/intel/oneapi/mpi/${IMPI_2021_VERSION_CENTOS}"
-MVAPICH2_PATH_CENTOS="/opt/mvapich2-${MVAPICH2_VERSION_CENTOS}"
+# added "libexec" to the path, as centos and ubuntu use "libexec", but SUSE only "lib"
+MVAPICH2_PATH_CENTOS="/opt/mvapich2-${MVAPICH2_VERSION_CENTOS}/libexec"
 MVAPICH2X_PATH_CENTOS="${MVAPICH2X_INSTALLATION_DIRECTORY}/gnu9.2.0/mofed5.1/azure-xpmem/mpirun"
 OPENMPI_PATH_CENTOS="/opt/openmpi-${OMPI_VERSION_CENTOS}"
-
-HPCX_OMB_PATH_ALMA_86="/opt/hpcx-v2.11-gcc-MLNX_OFED_LINUX-5-redhat8-cuda11-gdrcopy2-nccl2.11-x86_64/ompi/tests/osu-micro-benchmarks-5.8"
-MODULE_FILES_ROOT_ALMA="/usr/share/Modules/modulefiles"
-IMPI2021_PATH_ALMA="/opt/intel/oneapi/mpi/${IMPI_2021_VERSION_ALMA}"
-MVAPICH2_PATH_ALMA="/opt/mvapich2-${MVAPICH2_VERSION_ALMA}"
-OPENMPI_PATH_ALMA="/opt/openmpi-${OMPI_VERSION_ALMA}"
 
 MODULE_FILES_ROOT_UBUNTU="/usr/share/modules/modulefiles"
 HPCX_OMB_PATH_UBUNTU_2004="/opt/hpcx-${HPCX_VERSION_UBUNTU}-gcc-MLNX_OFED_LINUX-5-ubuntu20.04-cuda11-gdrcopy2-nccl2.11-x86_64/ompi/tests/osu-micro-benchmarks-5.8"
 IMPI2021_PATH_UBUNTU="/opt/intel/oneapi/mpi/${IMPI_2021_VERSION_UBUNTU}"
-MVAPICH2_PATH_UBUNTU="/opt/mvapich2-${MVAPICH2_VERSION_UBUNTU}"
+# added "libexec" to the path, as centos and ubuntu use "libexec", but SUSE only "lib"
+MVAPICH2_PATH_UBUNTU="/opt/mvapich2-${MVAPICH2_VERSION_UBUNTU}/libexec"
 MVAPICH2X_PATH_UBUNTU="${MVAPICH2X_INSTALLATION_DIRECTORY}/gnu9.2.0/mofed5.0/advanced-xpmem/mpirun"
 OPENMPI_PATH_UBUNTU="/opt/openmpi-${OMPI_VERSION_UBUNTU}"
+
+MODULE_FILES_ROOT_SUSE="/usr/share/lmod/modulefiles"
+HPCX_OMB_PATH_SUSE="/opt/hpcx-v${HPCX_VERSION_SUSE}-gcc-inbox-suse15.4-cuda11-gdrcopy2-nccl${HPCX_VERSION_SUSE}-x86_64/ompi/tests/osu-micro-benchmarks-5.8"
+IMPI2021_PATH_SUSE="/opt/intel/oneapi/mpi/${IMPI_2021_VERSION_SUSE}"
+MVAPICH2_PATH_SUSE="/usr/lib/hpc/gnu7/mpi/mvapich2-psm2/2.3.6/lib"
+MVAPICH2X_PATH_SUSE=""
+OPENMPI_PATH_SUSE="/usr/lib/hpc/gnu7/mpi/openmpi/4.1.1"
 
 CHECK_HPCX=0
 CHECK_IMPI_2021=0
@@ -99,24 +114,34 @@ CHECK_MVAPICH2=0
 CHECK_MVAPICH2X=0
 CHECK_CUDA=0
 CHECK_AOCL=1
+CHECK_NV_PMEM=0
 CHECK_NCCL=0
 CHECK_GCC=1
 
 # Find distro
 find_distro() {
+#TODO - the function and usage is quite strange as it does only work for special setups and not in general, so a rewrite would be a good suggestion
+# a) use always os-release and only as fallback *-release
+# b) use ID and VERSION_ID instead of NAME
+# like
+# awk '/^ID=/' $1 | awk -F'=' '{ print tolower($2) }'
+# awk '/^VERSION_ID=/' $1 | awk -F'=' '{ print tolower($2) }'
+# c) no second funtion is needed
+
+    # /etc/os-release is the standart for newer distros
     local os=`cat /etc/os-release | awk 'match($0, /^NAME="(.*)"/, result) { print result[1] }'`
     if [[ $os == "CentOS Linux" ]]
     then
         local centos_distro=`find_centos_distro`
         echo "${os} ${centos_distro}"
-    elif [[ $os == "AlmaLinux" ]]
-    then
-        local alma_distro=`find_alma_distro`
-        echo "${os} ${alma_distro}"
     elif [[ $os == "Ubuntu" ]]
     then
         local ubuntu_distro=`find_ubuntu_distro`
         echo "${os} ${ubuntu_distro}"
+    elif [[ $os == "SLE_HPC" ]]
+    then
+        local sle_hpc_distro=`find_sle_hpc_distro`
+        echo ${sle_hpc_distro}
     else
         echo "*** Error - invalid distro!"
         exit -1
@@ -126,16 +151,18 @@ find_distro() {
 # Find CentOS distro
 find_centos_distro() {
     echo `cat /etc/redhat-release | awk '{print $4}'`
-}
-
-# Find Alma distro
-find_alma_distro() {
-    echo `cat /etc/redhat-release | awk '{print $3}'`
+    # TODO - centos should have os-release too
 }
 
 # Find Ubuntu distro
 find_ubuntu_distro() {
     echo `cat /etc/os-release | awk 'match($0, /^PRETTY_NAME="(.*)"/, result) { print result[1] }' | awk '{print $2}' | cut -d. -f1,2`
+    #TODO - check if this is right, as the code below needs name *and* version
+}
+
+# Find SUSE Linux Enterprise HPC distro
+find_sle_hpc_distro() {
+    echo $(cat /etc/os-release | awk 'match($0, /^PRETTY_NAME="(.*)"/, result) { print result[1] }')
 }
 
 distro=`find_distro`
@@ -204,6 +231,7 @@ then
     MVAPICH2X_PATH=${MVAPICH2X_PATH_CENTOS}
     OPENMPI_PATH=${OPENMPI_PATH_CENTOS}
     CHECK_AOCL=1
+    CHECK_NV_PMEM=1
     CHECK_NCCL=1
 elif [[ $distro == "CentOS Linux 8.1.1911" ]]
 then
@@ -233,24 +261,9 @@ then
     MVAPICH2_PATH=${MVAPICH2_PATH_CENTOS}
     MVAPICH2X_PATH=${MVAPICH2X_PATH_CENTOS}
     OPENMPI_PATH=${OPENMPI_PATH_CENTOS}
-elif [[ $distro == "AlmaLinux 8.6" ]]
-then
-    HPCX_OMB_PATH=${HPCX_OMB_PATH_ALMA_86}
-    CHECK_HPCX=1
-    CHECK_IMPI_2021=1
-    CHECK_IMPI_2018=0
-    CHECK_OMPI=1
-    CHECK_MVAPICH2=1
-    CHECK_MVAPICH2X=0
-    MODULE_FILES_ROOT=${MODULE_FILES_ROOT_ALMA}
-    MOFED_VERSION=${MOFED_VERSION_ALMA_86}
-    IMPI2021_PATH=${IMPI2021_PATH_ALMA}
-    MVAPICH2_PATH=${MVAPICH2_PATH_ALMA}
-    OPENMPI_PATH=${OPENMPI_PATH_ALMA}
-    CHECK_AOCL=1
-    CHECK_NCCL=1
 elif [[ $distro == "Ubuntu 18.04" ]]
 then
+#TODO - check if this path get reached as the function above does only return the version
     HPCX_OMB_PATH=${HPCX_OMB_PATH_UBUNTU_1804}
     CHECK_HPCX=1
     CHECK_IMPI_2021=1
@@ -266,8 +279,15 @@ then
     CHECK_AOCL=0
     CHECK_GCC=0
     CHECK_NCCL=1
+    if [ "${MOFED_LTS}" = true ]
+    then
+        CHECK_NV_PMEM=0
+    else
+        CHECK_NV_PMEM=1
+    fi
 elif [[ $distro == "Ubuntu 20.04" ]]
 then
+#TODO - check if this path get reached as the function above does only return the version
     HPCX_OMB_PATH=${HPCX_OMB_PATH_UBUNTU_2004}
     CHECK_HPCX=1
     CHECK_IMPI_2021=1
@@ -281,8 +301,35 @@ then
     MVAPICH2X_PATH=${MVAPICH2X_PATH_UBUNTU}
     OPENMPI_PATH=${OPENMPI_PATH_UBUNTU}
     CHECK_AOCL=0
+    CHECK_NV_PMEM=1
     CHECK_NCCL=1
     CHECK_GCC=0
+elif [[ $distro == "SUSE Linux Enterprise High Performance Computing 15 SP4" ]]
+then
+    MKL_VERSION="2022.2.0"
+    #
+    CHECK_GCC=0
+    CHECK_ONEAPI=1
+    CHECK_AZURE_HPC_DIAG=1
+
+    HPCX_OMB_PATH=${HPCX_OMB_PATH_SUSE}
+    CHECK_HPCX=1
+    CHECK_IMPI_2021=1
+    CHECK_MVAPICH2=1
+    CHECK_MVAPICH2X=0
+    CHECK_CUDA=1
+    CHECK_OMPI=1
+    CHECK_BLIS_MT=1
+
+    MODULE_FILES_ROOT=${MODULE_FILES_ROOT_SUSE}
+    MOFED_VERSION=${MOFED_VERSION_SUSE}
+    IMPI2021_PATH=${IMPI2021_PATH_SUSE}
+    MVAPICH2_PATH=${MVAPICH2_PATH_SUSE}
+    OPENMPI_PATH=${OPENMPI_PATH_SUSE}
+
+    CHECK_AOCL=1
+    CHECK_NV_PMEM=0
+    CHECK_NCCL=0
 else
     echo "*** Error - invalid distro!"
     exit -1
@@ -314,8 +361,13 @@ check_exit_code() {
 }
 
 # verify MOFED installation
-ofed_info | grep ${MOFED_VERSION}
-check_exit_code "MOFED installed" "MOFED not installed"
+if [[ $distro == "SUSE Linux Enterprise High Performance Computing 15 SP4" ]]
+then
+  echo "ofed inbox driver does miss the ofed_info tool"
+else
+  ofed_info | grep ${MOFED_VERSION}
+  check_exit_code "MOFED installed" "MOFED not installed"
+fi
 
 # verify IB device is listed
 lspci | grep "Infiniband controller\|Network controller"
@@ -339,7 +391,10 @@ then
     check_exists "/opt/gcc-${GCC_VERSION}/"
 fi
 
-check_exists "/opt/intel/oneapi/mkl/${MKL_VERSION}/"
+if [ $CHECK_ONEAPI -eq 1 ]
+then
+    check_exists "/opt/intel/oneapi/mkl/${MKL_VERSION}/"
+fi
 
 # verify hpcdiag installation
 check_exists '/opt/azurehpc/diagnostics/gather_azhpc_vm_diagnostics.sh'
@@ -356,7 +411,9 @@ fi
 # verify mpi installations and their modulefiles
 module avail
 
-# hpcx
+# hpcxEdit source
+
+
 if [ $CHECK_HPCX -eq 1 ]
 then
     check_exists "${MODULE_FILES_ROOT}/mpi/hpcx"
@@ -390,13 +447,19 @@ then
 fi
 
 # mvapich2
-if [ $CHECK_MVAPICH2 -eq 1 ]
+if [ $CHECK_MVAPICH2 -eq 1
 then
     check_exists "${MODULE_FILES_ROOT}/mpi/mvapich2"
+    # SUSE module load gnu/7 mvapich2
+    if [[ $distro == "SUSE Linux Enterprise High Performance Computing 15 SP4" ]]
+    then
+        module load gnu/7 mvapich2
+    else
+        module load mpi/mvapich2
+    fi
 
-    module load mpi/mvapich2
     # Env MV2_FORCE_HCA_TYPE=22 explicitly selects EDR
-    mpiexec -np 2 -ppn 2 -env MV2_USE_SHARED_MEM=0  -env MV2_FORCE_HCA_TYPE=22  ${MVAPICH2_PATH}/libexec/osu-micro-benchmarks/mpi/pt2pt/osu_latency
+    mpiexec -np 2 -ppn 2 -env MV2_USE_SHARED_MEM=0  -env MV2_FORCE_HCA_TYPE=22  ${MVAPICH2_PATH}/osu-micro-benchmarks/mpi/pt2pt/osu_latency
     check_exit_code "MVAPICH2" "Failed to run MVAPICH2"
     module unload mpi/mvapich2
 fi
@@ -427,6 +490,13 @@ then
     check_exit_code "Nvidia SMI - Cuda Drivers" "Failed to run Nvidia SMI - Cuda Drivers"
 fi
 
+# Check NV_Peer_Memory
+if [ $CHECK_NV_PMEM -eq 1 ]
+then
+    lsmod | grep nv
+    check_exit_code "NV Peer Memory Module" "Failed to locate Module"
+fi
+
 # Perform Single Node NCCL Test
 if [ $CHECK_NCCL -eq 1 ]
 then
@@ -450,11 +520,14 @@ then
         --map-by ppr:8:node \
         -x LD_LIBRARY_PATH=/usr/local/nccl-rdma-sharp-plugins/lib:$LD_LIBRARY_PATH \
         -mca coll_hcoll_enable 0 \
+        -x NCCL_IB_PCI_RELAXED_ORDERING=1 \
+        -x UCX_IB_PCI_RELAXED_ORDERING=on \
         -x UCX_TLS=tcp \
         -x CUDA_DEVICE_ORDER=PCI_BUS_ID \
         -x NCCL_SOCKET_IFNAME=eth0 \
         -x NCCL_DEBUG=WARN \
         -x NCCL_NET_GDR_LEVEL=5 \
+        -x NCCL_TOPO_FILE=/opt/microsoft/ndv4-topo.xml \
         /opt/nccl-tests/build/all_reduce_perf -b1K -f2 -g1 -e 4G
     fi
 

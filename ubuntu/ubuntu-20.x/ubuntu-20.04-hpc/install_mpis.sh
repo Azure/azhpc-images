770c982961d415e2c250bd4fc1d822d50692f1ff
Aditya Bhamidipati <abhamidipati@microsoft.com>
2022-11-02 12:51:00 -0400

    HPC-x update to v2.13

--- ubuntu/ubuntu-20.x/ubuntu-20.04-hpc/install_mpis.sh
+++ ubuntu/ubuntu-20.x/ubuntu-20.04-hpc/install_mpis.sh
@@ -2,4 +2,4 @@
 set -ex
 
 # Install common MPIs for Ubuntu
-$UBUNTU_COMMON_DIR/install_mpis.sh 20.04 "ba9905d39bffb7691cce67c0f7d6c0dd0558fd5b11a085e798012b13ed66614d"
+$UBUNTU_COMMON_DIR/install_mpis.sh 20.04 "0e1d30c5e5275b88d4ca3a8272770f429ec7022e4778ea8e6203e2e6eaef9b74"

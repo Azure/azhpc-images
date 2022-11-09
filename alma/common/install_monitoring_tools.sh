808e3ce5d6ba670c4d8db30cd49e58a9084c3ea9
rafsalas19 <rafsalas1987@hotmail.com>
2022-11-09 16:04:41 +0000

    adding Moneo and dependencies

--- alma/common/install_monitoring_tools.sh
+++ alma/common/install_monitoring_tools.sh
@@ -6,6 +6,9 @@ set -e
 python3 -m pip install --upgrade pip
 python3 -m pip install ansible
 
+# Adding path to sudo user
+sed -i 's/.*secure_path.*/Defaults    secure_path = \/usr\/local\/sbin:\/usr\/local\/bin:\/sbin:\/bin:\/usr\/sbin:\/usr\/bin\//' /etc/sudoers
+
 MONITOR_DIR=/opt/azurehpc/tools
 
 mkdir -p $MONITOR_DIR

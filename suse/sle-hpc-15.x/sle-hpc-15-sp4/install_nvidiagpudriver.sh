0dcc33d5281f602d4ce2e3c255fd3ab896dd63d9
Peter <18663039+petersatsuse@users.noreply.github.com>
2022-11-21 13:36:07 +0100

    add comment for more cuda install possibilities

--- suse/sle-hpc-15.x/sle-hpc-15-sp4/install_nvidiagpudriver.sh
+++ suse/sle-hpc-15.x/sle-hpc-15-sp4/install_nvidiagpudriver.sh
@@ -14,6 +14,16 @@ set -ex
 
 # install latest cuda package
 # the repo contains more versions, so for example we want cuda 11.3 the package name is "cuda-11-3"
+#
+# cuda 	                    Installs all CUDA Toolkit and Driver packages. Handles upgrading to the next version of the cuda package when it's released.
+# cuda-11-8 	            Installs all CUDA Toolkit and Driver packages. Remains at version 11.8 until an additional version of CUDA is installed.
+# cuda-toolkit-11-8 	    Installs all CUDA Toolkit packages required to develop CUDA applications. Does not include the driver.
+# cuda-tools-11-8 	        Installs all CUDA command line and visual tools.
+# cuda-runtime-11-8      	Installs all CUDA Toolkit packages required to run CUDA applications, as well as the Driver packages.
+# cuda-compiler-11-8    	Installs all CUDA compiler packages.
+# cuda-libraries-11-8 	    Installs all runtime CUDA Library packages.
+# cuda-libraries-dev-11-8 	Installs all development CUDA Library packages.
+# cuda-drivers 	            Installs all Driver packages. Handles upgrading to the next version of the Driver packages when they're released.
 zypper install -y -l cuda cuda-drivers
 
 CUDA_VERSION=$(rpm -q --qf="%{VERSION}" cuda)

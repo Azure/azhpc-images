1a377beba89253441c1bad53c144c53b02c41b73
joe-atzinger <67014403+joe-atzinger@users.noreply.github.com>
2022-12-14 15:48:35 -0500

    Add Lustre clients to the images.
    Stop removing MS repo during docker setup.

--- alma/alma-8.x/alma-8.6-hpc/install.sh
+++ alma/alma-8.x/alma-8.6-hpc/install.sh
@@ -7,6 +7,10 @@ source ./set_properties.sh
 # install utils
 ./install_utils.sh
 
+# install Lustre client
+# Disable until testing is performed.
+#./install_lustre_client.sh
+
 # install compilers
 ./install_gcc.sh
 

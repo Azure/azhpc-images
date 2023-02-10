d3fe4f4ff3e7fcd74ca627ed48a292f387b77437
Aditya Bhamidipati <abhamidipati@microsoft.com>
2023-02-10 22:38:15 +0530

    NVIDIA driver updates for Ubuntu 20.04 HPC

--- ubuntu/common/install_nvidia_fabric_manager.sh
+++ ubuntu/common/install_nvidia_fabric_manager.sh
@@ -6,9 +6,9 @@ set -ex
 VERSION=$1
 
 # Install nvidia fabric manager
-NVIDIA_FABRIC_MANAGER_VERSION="520_520.61.05-1"
+NVIDIA_FABRIC_MANAGER_VERSION="525_525.85.12-1"
 NVIDIA_FABRIC_MNGR_URL=http://developer.download.nvidia.com/compute/cuda/repos/ubuntu${VERSION}/x86_64/nvidia-fabricmanager-${NVIDIA_FABRIC_MANAGER_VERSION}_amd64.deb
-$COMMON_DIR/download_and_verify.sh $NVIDIA_FABRIC_MNGR_URL "8beeb76ade836327298337b4f65eb3906498cebe71ab35bca5bb1a638c1bfd0a"
+$COMMON_DIR/download_and_verify.sh $NVIDIA_FABRIC_MNGR_URL "77e2f8768e4901114c35582b530b10fe6bd3b924862a929f96fc83aee078b12c"
 apt install -y ./nvidia-fabricmanager-${NVIDIA_FABRIC_MANAGER_VERSION}_amd64.deb
-sudo apt-mark hold nvidia-fabricmanager-520
+sudo apt-mark hold nvidia-fabricmanager-525
 $COMMON_DIR/write_component_version.sh "NVIDIA_FABRIC_MANAGER" ${NVIDIA_FABRIC_MANAGER_VERSION}

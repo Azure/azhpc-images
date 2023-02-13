9c52bdfc232c7500cfb44c41565541f5b1df1bb7
Aditya Bhamidipati <abhamidipati@microsoft.com>
2023-02-14 02:26:04 +0530

    MOFED update v5.9-0.5.6.0 for Ubuntu 20.04 HPC

--- ubuntu/ubuntu-20.x/ubuntu-20.04-hpc/install_mellanoxofed.sh
+++ ubuntu/ubuntu-20.x/ubuntu-20.04-hpc/install_mellanoxofed.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 set -ex
 
-VERSION="5.8-1.0.1.1"
+VERSION="5.9-0.5.6.0"
 TARBALL="MLNX_OFED_LINUX-$VERSION-ubuntu20.04-x86_64.tgz"
 MLNX_OFED_DOWNLOAD_URL=https://content.mellanox.com/ofed/MLNX_OFED-${VERSION}/$TARBALL
 MOFED_FOLDER=$(basename ${MLNX_OFED_DOWNLOAD_URL} .tgz)
 
-$COMMON_DIR/download_and_verify.sh $MLNX_OFED_DOWNLOAD_URL "1259c867ee1805872bb2838ab12d45685f99d4a3f355def5dd9f610801a8d4b5"
+$COMMON_DIR/download_and_verify.sh $MLNX_OFED_DOWNLOAD_URL "7a7a31deecf91d5e864f23114283c77115e363c0577faf6e4b8bac8ad965d96a"
 tar zxvf ${TARBALL}
 
 ./${MOFED_FOLDER}/mlnxofedinstall --add-kernel-support --skip-unsupported-devices-check --without-fw-update

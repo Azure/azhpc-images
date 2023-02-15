0211624e476050b7039c93fd1adad47124f849e0
Aditya Bhamidipati <abhamidipati@microsoft.com>
2023-02-15 15:35:48 +0530

    de-parameterizing intel oneapi mkl installation

--- ubuntu/ubuntu-18.x/ubuntu-18.04-LTS-hpc/install.sh
+++ ubuntu/ubuntu-18.x/ubuntu-18.04-LTS-hpc/install.sh
@@ -27,7 +27,7 @@ rm -Rf -- */
 $UBUNTU_COMMON_DIR/install_dcgm.sh
 
 # install Intel libraries
-$UBUNTU_COMMON_DIR/install_intel_libs.sh 1804
+$UBUNTU_COMMON_DIR/install_intel_libs.sh
 
 # install diagnostic script
 $COMMON_DIR/install_hpcdiag.sh
--- ubuntu/ubuntu-22.x/ubuntu-22.04-hpc/install.sh
+++ ubuntu/ubuntu-22.x/ubuntu-22.04-hpc/install.sh
@@ -30,7 +30,7 @@ rm -Rf -- */
 $UBUNTU_COMMON_DIR/install_dcgm.sh
 
 # install Intel libraries
-$UBUNTU_COMMON_DIR/install_intel_libs.sh 2204
+$UBUNTU_COMMON_DIR/install_intel_libs.sh
 
 # install diagnostic script
 $COMMON_DIR/install_hpcdiag.sh

78e1a1c7e3ed738a5dbf6be767f17d34acadcff1
Peter S <18663039+petersatsuse@users.noreply.github.com>
2023-02-22 15:32:16 +0100

    pin mkl version to 2022.2.0

--- suse/sle-hpc-15.x/sle-hpc-15-sp4/install_intel_libs.sh
+++ suse/sle-hpc-15.x/sle-hpc-15-sp4/install_intel_libs.sh
@@ -6,8 +6,7 @@ set -ex
 # Intel provides oneapi RPM packages for SUSE
 # and the repository is set up in install_utils.sh
 # so we can simply install the needed package
-zypper install -y -l intel-oneapi-mkl
+VERSION="2022.2.0"
+zypper install -y -l intel-oneapi-mkl = $VERSION
 
-#VERSION="2022.1.0.223"
-VERSION=$(rpm -q  --qf="%{VERSION}" intel-oneapi-mkl)
 $COMMON_DIR/write_component_version.sh "INTEL_ONE_MKL" $VERSION

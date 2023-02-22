51e0d3b211c52ccaaa51effe9400441c105edafa
Peter S <18663039+petersatsuse@users.noreply.github.com>
2023-02-22 15:35:28 +0100

    pin DCGM versiom ,workaround Nvidia bug

--- suse/sle-hpc-15.x/sle-hpc-15-sp4/install_dcgm.sh
+++ suse/sle-hpc-15.x/sle-hpc-15-sp4/install_dcgm.sh
@@ -3,10 +3,20 @@ set -ex
 
 # Install DCGM
 # actual version is 3.0.4, older are in the repo too
-# DCGM_VERSION=3.0.4
+DCGM_VERSION="1:3.0.4"
 
-zypper install -y -l datacenter-gpu-manager
-DCGM_VERSION=$(rpm -q  --qf="%{VERSION}" datacenter-gpu-manager)
+# workaround to create group for serviceUser=nvidia-dcgm
+# as the package expects that the variable "USERGROUPS_ENAB" in /etc/login.defs is set to yes
+# in oder that the useradd create the group too, but the SUSE default is "no", so we would change the
+# variable or simply use the parameter -U
+# wrong cmd: useradd -r -M -s /usr/sbin/nologin ${serviceUser}
+# right cmd: useradd -r -M -U -s /usr/sbin/nologin ${serviceUser}
+# see: man useradd
+# bug reported to Nvidia
+serviceUser="nvidia-dcgm"
+useradd -r -M -U -s /usr/sbin/nologin ${serviceUser}
+
+zypper install -y -l datacenter-gpu-manager = ${DCGM_VERSION}
 
 systemctl --now enable nvidia-dcgm
 

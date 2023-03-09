ce1232c33c715d30909b069e111d201d4fb330f4
Aditya Bhamidipati <abhamidipati@microsoft.com>
2023-03-09 01:05:10 -0500

    Update Intel MKL to v2023

--- ubuntu/common/install_intel_libs.sh
+++ ubuntu/common/install_intel_libs.sh
@@ -3,9 +3,9 @@ set -ex
 
 # Intel® oneAPI Math Kernel Library
 case ${DISTRIBUTION} in
-    "ubuntu18.04") INTEL_MKL_VERSION="2022.1.0.223";
-        RELEASE_VERSION="18721";
-        CHECKSUM="4b325a3c4c56e52f4ce6c8fbb55d7684adc16425000afc860464c0f29ea4563e";; 
+    "ubuntu18.04") INTEL_MKL_VERSION="2023.0.0.25398";
+        RELEASE_VERSION="19138";
+        CHECKSUM="0d61188e91a57bdb575782eb47a05ae99ea8eebefee6b2dfe20c6708e16e9927";; 
     "ubuntu20.04") INTEL_MKL_VERSION="2023.0.0.25398";
         RELEASE_VERSION="19138";
         CHECKSUM="0d61188e91a57bdb575782eb47a05ae99ea8eebefee6b2dfe20c6708e16e9927";;

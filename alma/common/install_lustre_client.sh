909b6358ee462dc7d354e7a2125e29bf58517a27
joe-atzinger <joseph.atzinger@microsoft.com>
2023-03-10 11:27:21 -0500

    Refactor to make Alma as maintainable as Ubuntu. Enable Alma builds.

---
+++ alma/common/install_lustre_client.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -ex
+
+LUSTRE_VERSION=2.15.1_24_gbaa21ca
+
+# Expected params:
+# $1 = the major version of the distro. "8" for RHEL/Alma8, "9" for RHEL/Alma9.
+
+source $ALMA_COMMON_DIR/setup_lustre_repo.sh "$1"
+
+dnf install --refresh amlfs-lustre-client-${LUSTRE_VERSION}-$(uname -r | sed -e "s/\.$(uname -p)$//" | sed -re 's/[-_]/\./g')-1
+
+$COMMON_DIR/write_component_version.sh "LUSTRE" ${LUSTRE_VERSION}

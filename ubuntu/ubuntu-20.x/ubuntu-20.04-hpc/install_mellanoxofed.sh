41cb3fd2d1d5aaabb6b4b2439d96e62fcef42df5
Aditya Bhamidipati <abhamidipati@microsoft.com>
2023-03-27 14:56:51 -0400

    MOFED driver v5.8 for H100 systems using Ubuntu HPC 20.04/ 22.04

--- ubuntu/ubuntu-20.x/ubuntu-20.04-hpc/install_mellanoxofed.sh
+++ ubuntu/ubuntu-20.x/ubuntu-20.04-hpc/install_mellanoxofed.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 set -ex
 
-VERSION="5.9-0.5.6.0.107"
+VERSION="5.8-2.0.3.0"
 TARBALL="MLNX_OFED_LINUX-$VERSION-ubuntu20.04-x86_64.tgz"
 MLNX_OFED_DOWNLOAD_URL=https://content.mellanox.com/ofed/MLNX_OFED-${VERSION}/$TARBALL
 MOFED_FOLDER=$(basename ${MLNX_OFED_DOWNLOAD_URL} .tgz)
 
-$COMMON_DIR/download_and_verify.sh $MLNX_OFED_DOWNLOAD_URL "3c79370a8ef9548aa8078c1faa9ed832a4bd2cd0c83ebbb0fcc4ac7902ca7f65"
+$COMMON_DIR/download_and_verify.sh $MLNX_OFED_DOWNLOAD_URL "99aa2966ce260f3ca282e24a26c6f5302692f9072117626107aa599868208d8f"
 tar zxvf ${TARBALL}
 
 ./${MOFED_FOLDER}/mlnxofedinstall --add-kernel-support --skip-unsupported-devices-check --without-fw-update
--- ubuntu/ubuntu-22.x/ubuntu-22.04-hpc/install_mellanoxofed.sh
+++ ubuntu/ubuntu-22.x/ubuntu-22.04-hpc/install_mellanoxofed.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 set -ex
 
-VERSION="5.9-0.5.6.0.107"
+VERSION="5.8-2.0.3.0"
 TARBALL="MLNX_OFED_LINUX-$VERSION-ubuntu22.04-x86_64.tgz"
 MLNX_OFED_DOWNLOAD_URL=https://content.mellanox.com/ofed/MLNX_OFED-${VERSION}/$TARBALL
 MOFED_FOLDER=$(basename ${MLNX_OFED_DOWNLOAD_URL} .tgz)
 
-$COMMON_DIR/download_and_verify.sh $MLNX_OFED_DOWNLOAD_URL "064af835071a332e0394ba35267c5aa444613bfdde8f0ea8806e70031dc57381"
+$COMMON_DIR/download_and_verify.sh $MLNX_OFED_DOWNLOAD_URL "6ec0e1b6a8de6fbcc6d6bceaedf853f801003b0ee854f3b660e53bd2f73ef79a"
 tar zxvf ${TARBALL}
 
 ./${MOFED_FOLDER}/mlnxofedinstall --add-kernel-support --skip-unsupported-devices-check --without-fw-update

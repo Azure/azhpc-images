a78ab1f6a2412035985b0a966ef35075ef7e7397
Aditya Bhamidipati <87097035+abhamidipati0614@users.noreply.github.com>
2023-03-27 15:10:58 -0400

    SKU specific nvme bring-up configuration (#212)
    * Mount NVME devices in the A100s
    
    * Set NFS read ahead limits for Alma, Ubuntu
    
    * setup sku customization in the end to get around issue with dpkg frontend lock
    
    * SKU customizations for HBv4

---
+++ customizations/hbv4.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+set -ex
+
+## Setup NVME devices
+/opt/azurehpc/customizations/setup_nvme.sh
--- ubuntu/ubuntu-20.x/ubuntu-20.04-hpc/install.sh
+++ ubuntu/ubuntu-20.x/ubuntu-20.04-hpc/install.sh
@@ -46,9 +46,6 @@ $COMMON_DIR/install_azure_persistent_rdma_naming.sh
 # optimizations
 $UBUNTU_COMMON_DIR/hpc-tuning.sh
 
-# SKU Customization
-$COMMON_DIR/setup_sku_customizations.sh
-
 # copy test file
 $COMMON_DIR/copy_test_file.sh
 
@@ -61,6 +58,9 @@ $UBUNTU_COMMON_DIR/disable_auto_upgrade.sh
 
 # Disable Predictive Network interface renaming
 $UBUNTU_COMMON_DIR/disable_predictive_interface_renaming.sh
+
+# SKU Customization
+$COMMON_DIR/setup_sku_customizations.sh
 
 # clear history
 # Uncomment the line below if you are running this on a VM

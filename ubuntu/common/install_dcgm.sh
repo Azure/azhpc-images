4e55ec1ef6d944a0b19890a58ea0688cfd5768b1
Aditya Bhamidipati <abhamidipati@microsoft.com>
2023-03-31 02:39:10 -0400

    conforming dcgm script with other component scripts

--- ubuntu/common/install_dcgm.sh
+++ ubuntu/common/install_dcgm.sh
@@ -5,8 +5,8 @@ set -ex
 # Reference: https://developer.nvidia.com/dcgm#Downloads
 # the repo is already added during nvidia/ cuda installations
 apt-get install -y datacenter-gpu-manager
-DCGM_VERSION=$(apt list --installed | grep datacenter-gpu-manager | awk '{print $2}' | awk -F ':' '{print $2}')
-$COMMON_DIR/write_component_version.sh "DCGM" ${DCGM_VERSION}
+dcgm_version=$(apt list --installed | grep datacenter-gpu-manager | awk '{print $2}' | awk -F ':' '{print $2}')
+$COMMON_DIR/write_component_version.sh "dcgm" $dcgm_version
 
 # Enable the dcgm service
 systemctl --now enable nvidia-dcgm
@@ -14,8 +14,8 @@ systemctl start nvidia-dcgm
 # Check if the service is active
 systemctl is-active --quiet nvidia-dcgm
 error_code=$?
-if [ ${error_code} -ne 0 ]
+if [ $error_code -ne 0 ]
 then
     echo "DCGM is inactive!"
-    exit ${error_code}
+    exit $error_code
 fi

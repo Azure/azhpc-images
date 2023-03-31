d4016e82faf7ca61fdae04991a8b5a66bf42695d
Aditya Bhamidipati <abhamidipati@microsoft.com>
2023-03-31 02:45:35 -0400

    install intel oneapi mkl using spack for ubuntu

--- ubuntu/common/install_intel_libs.sh
+++ ubuntu/common/install_intel_libs.sh
@@ -1,21 +1,10 @@
 #!/bin/bash
 set -ex
 
-# Intel® oneAPI Math Kernel Library
-case ${DISTRIBUTION} in
-    "ubuntu18.04") INTEL_MKL_VERSION="2023.0.0.25398";
-        RELEASE_VERSION="19138";
-        CHECKSUM="0d61188e91a57bdb575782eb47a05ae99ea8eebefee6b2dfe20c6708e16e9927";; 
-    "ubuntu20.04") INTEL_MKL_VERSION="2023.0.0.25398";
-        RELEASE_VERSION="19138";
-        CHECKSUM="0d61188e91a57bdb575782eb47a05ae99ea8eebefee6b2dfe20c6708e16e9927";;
-    "ubuntu22.04") INTEL_MKL_VERSION="2023.0.0.25398";
-        RELEASE_VERSION="19138";
-        CHECKSUM="0d61188e91a57bdb575782eb47a05ae99ea8eebefee6b2dfe20c6708e16e9927";;
-    *) ;;
-esac
+# Set Intel® oneAPI Math Kernel Library version
+intel_one_mkl_version=$(jq -r '.intel_one_mkl."'"$DISTRIBUTION"'".version' $TOP_DIR/requirements.json)
 
-ONE_MKL_DOWNLOAD_URL=https://registrationcenter-download.intel.com/akdlm/irc_nas/${RELEASE_VERSION}/l_onemkl_p_${INTEL_MKL_VERSION}_offline.sh
-$COMMON_DIR/write_component_version.sh "INTEL_ONE_MKL" ${INTEL_MKL_VERSION}
-$COMMON_DIR/download_and_verify.sh ${ONE_MKL_DOWNLOAD_URL} ${CHECKSUM}
-sh ./l_onemkl_p_${INTEL_MKL_VERSION}_offline.sh -s -a -s --eula accept
+spack add intel-oneapi-mkl@$intel_one_mkl_version
+# If there is a space crunch use spack gc (this performs garbage collection)
+spack install
+$COMMON_DIR/write_component_version.sh "intel_one_mkl" $intel_one_mkl_version

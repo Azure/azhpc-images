5bcca0c99ad2c24a08241d774560ec5a5403bef6
Aditya Bhamidipati <abhamidipati@microsoft.com>
2023-04-10 23:34:42 -0400

    nvme as a service in sku customization

--- common/setup_sku_customizations.sh
+++ common/setup_sku_customizations.sh
@@ -2,18 +2,16 @@
 set -ex
 
 ## Copy customization scripts to /opt/azurehpc/customizations
-mkdir -p $HPC_ENV/customizations
-cp $COMMON_DIR/../customizations/* $HPC_ENV/customizations
+mkdir -p /opt/azurehpc/customizations
+cp $COMMON_DIR/../customizations/* /opt/azurehpc/customizations
 
 
 ## Systemd service for setting up appropriate customizations based on SKU
 cat <<EOF >/usr/sbin/setup_sku_customizations.sh
 #!/bin/bash
-
 metadata_endpoint="http://169.254.169.254/metadata/instance?api-version=2019-06-04"
 vmSize=\$(curl -H Metadata:true \$metadata_endpoint | jq -r ".compute.vmSize")
 vmSize=\$(echo "\$vmSize" | awk '{print tolower(\$0)}')
-
 ## Topo file setup based on SKU
 case \$vmSize in
     standard_nc*ads_a100_v4)
@@ -24,13 +22,10 @@ case \$vmSize in
         
     standard_nd40rs_v2)
         /opt/azurehpc/customizations/ndv2.sh;;
-
     standard_hb176*v4)
         /opt/azurehpc/customizations/hbv4.sh;;
-
     *) echo "No SKU customization for \$vmSize";;
 esac
-
 EOF
 chmod 755 /usr/sbin/setup_sku_customizations.sh
 
@@ -38,13 +33,11 @@ cat <<EOF >/etc/systemd/system/sku-customizations.service
 [Unit]
 Description=Customizations based on SKU
 After=network.target
-
 [Service]
 Type=oneshot
 ExecStart=/usr/sbin/setup_sku_customizations.sh
 RemainAfterExit=true
 StandardOutput=journal
-
 [Install]
 WantedBy=multi-user.target
 EOF
--- customizations/setup_nvme.sh
+++ customizations/setup_nvme.sh
@@ -28,14 +28,26 @@ mdadm --create /dev/md128 --level=0 --raid-devices=\$nvme_disks \$nvme_disks_nam
 # Create the filesystem if it doesn't exist
 if [ -z "\$(blkid -o value -s UUID /dev/md128)" ]; then
     echo "Creating filesystem on /dev/md128..."
+
     # Create a file system
     mkfs.xfs /dev/md128
+
     # Assign a unique ID for the generated file system
     xfs_admin -U generate /dev/md128
-    uuid_md128=\$(mdadm --detail /dev/md128 | grep UUID | awk '{print \$3}')
-    echo "UUID=\$uuid_md128 /dev/md128" | tee -a /etc/mdadm/mdadm.conf
-    update-initramfs -u
 fi
+
+uuid_md128=\$(mdadm --detail /dev/md128 | grep UUID | awk '{print \$3}')
+
+# Check if mdadm config already has /dev/md128 UUID info
+# replace if it does and append if not
+if grep -q '^UUID=' /etc/mdadm/mdadm.conf
+then
+    sed -i '/^UUID=[0-9a-f:]* \/dev\/md128/s/.*/UUID='"\$uuid_md128"' \/dev\/md128/' /etc/mdadm/mdadm.conf
+else
+    echo "UUID=\$uuid_md128 /dev/md128" | tee -a /etc/mdadm/mdadm.conf
+fi
+
+update-initramfs -u
 
 mkdir -p /mnt/resource_nvme
 
@@ -46,7 +58,6 @@ fi
 
 # Mount the file system
 mount /dev/md128 /mnt/resource_nvme
-
 chmod 777 /mnt/resource_nvme
 
 EOF
@@ -68,6 +79,9 @@ if [ -e /dev/md128 ]; then
         mdadm --zero-superblock \$dev
     done
 fi
+
+# Clear the UUID from /etc/mdadm/mdadm.conf
+sed -i '/^UUID=/d' /etc/mdadm/mdadm.conf
 
 EOF
 chmod 755 /usr/sbin/nvme_raid_stop.sh

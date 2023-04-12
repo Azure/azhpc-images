1a52b219e7ca9456e41e905f90e63e5202235b0f
Aditya Bhamidipati <abhamidipati@microsoft.com>
2023-04-12 13:00:03 -0400

    MOFED update v5.9-0.5.9.0

--- ubuntu/ubuntu-22.x/ubuntu-22.04-hpc/install_mellanoxofed.sh
+++ ubuntu/ubuntu-22.x/ubuntu-22.04-hpc/install_mellanoxofed.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 set -ex
 
-VERSION="5.8-2.0.3.0"
+VERSION="5.9-0.5.9.0"
 TARBALL="MLNX_OFED_LINUX-$VERSION-ubuntu22.04-x86_64.tgz"
 MLNX_OFED_DOWNLOAD_URL=https://content.mellanox.com/ofed/MLNX_OFED-${VERSION}/$TARBALL
 MOFED_FOLDER=$(basename ${MLNX_OFED_DOWNLOAD_URL} .tgz)
 
-$COMMON_DIR/download_and_verify.sh $MLNX_OFED_DOWNLOAD_URL "6ec0e1b6a8de6fbcc6d6bceaedf853f801003b0ee854f3b660e53bd2f73ef79a"
+$COMMON_DIR/download_and_verify.sh $MLNX_OFED_DOWNLOAD_URL "70d62106230446ebf47b325077b2c2494c1f3ced2e03ac97fce08469da7d8224"
 tar zxvf ${TARBALL}
 
 ./${MOFED_FOLDER}/mlnxofedinstall --add-kernel-support --skip-unsupported-devices-check --without-fw-update

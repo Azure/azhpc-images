cfda440ea5b24fa528cd2d3e51fcbe67b80bccb2
Ubuntu <rafsalas@ub22h3e8f000003.dqjt2vfkzmou3mpiz4ibgqqi1c.ax.internal.cloudapp.net>
2023-05-01 14:14:31 +0000

    adding nhc health checks for ndv4

---
+++ ubuntu/common/install_health_checks.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+
+set -e
+
+AZHC_VERSION=v0.1.0
+
+
+pushd /opt/azurehpc/test/
+
+git clone https://github.com/Azure/azurehpc-health-checks.git --branch $AZHC_VERSION
+
+pushd azurehpc-health-checks
+
+NHC_VERSION=1.4.3
+echo "Installed NHC verison $NHC_VERSION"
+
+wget -O nhc-$NHC_VERSION.tar.xz https://github.com/mej/nhc/releases/download/1.4.3/lbnl-nhc-1.4.3.tar.xz
+tar -xf nhc-$NHC_VERSION.tar.xz
+
+pushd lbnl-nhc-$NHC_VERSION
+./configure --prefix=/usr --sysconfdir=/etc --libexecdir=/usr/libexec
+
+sudo make test
+echo -e "\n"
+sudo make install
+popd
+
+echo -e "\nRunning set up script for custom tests"
+pushd customTests/
+./custom-test-setup.sh
+popd
+popd
+
+$COMMON_DIR/write_component_version.sh "MONEO" ${AZHC_VERSION}

a3ebbd6969f3bc8141c9cdcd1ed08b734198c7cb
Peter <18663039+petersatsuse@users.noreply.github.com>
2023-05-04 16:37:58 +0200

    move install scripts to common

--- suse/sle-hpc-15.x/common/hpc-tuning.sh
+++ suse/sle-hpc-15.x/common/hpc-tuning.sh
@@ -1,38 +1,3 @@
 #!/bin/bash
 
-# Disable some unneeded services by default (administrators can re-enable if desired)
-systemctl disable firewalld
-
-# Update memory limits
-cat << EOF >> /etc/security/limits.conf
-*               hard    memlock         unlimited
-*               soft    memlock         unlimited
-*               hard    nofile          65535
-*               soft    nofile          65535
-*               hard    stack           unlimited
-*               soft    stack           unlimited
-EOF
-
-# Enable reclaim mode
-echo "vm.zone_reclaim_mode = 1" >> /etc/sysctl.conf
-sysctl -p
-
-# Remove auoms if exists - Prevent CPU utilization by auoms
-if zypper se --installed-only azsec-monitor >/dev/null 2>&1; then zypper remove -y azsec-monitor; fi
-
-# Update WALinuxAgent - for IPoIB
-zypper update -y python-azure-agent
-
-# Configure WALinuxAgent
-# this should be already set by default within the SLE HPC image
-sed -i -e 's/# OS.EnableRDMA=y/OS.EnableRDMA=y/g' /etc/waagent.conf
-
-echo "Extensions.GoalStatePeriod=300" | sudo tee -a /etc/waagent.conf
-echo "Extensions.InitialGoalStatePeriod=6" | sudo tee -a /etc/waagent.conf
-echo "OS.EnableFirewallPeriod=300" | sudo tee -a /etc/waagent.conf
-echo "OS.RemovePersistentNetRulesPeriod=300" | sudo tee -a /etc/waagent.conf
-echo "OS.RootDeviceScsiTimeoutPeriod=300" | sudo tee -a /etc/waagent.conf
-echo "OS.MonitorDhcpClientRestartPeriod=60" | sudo tee -a /etc/waagent.conf
-echo "Provisioning.MonitorHostNamePeriod=60" | sudo tee -a /etc/waagent.conf
-systemctl restart waagent
-$COMMON_DIR/write_component_version.sh "WAAGENT" $(waagent --version | head -n 1 | awk -F' ' '{print $1}' | awk -F- '{print $2}')
+../common/hpc-tuning.sh

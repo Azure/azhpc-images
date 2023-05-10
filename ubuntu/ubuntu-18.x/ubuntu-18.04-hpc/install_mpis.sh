c94bd174aac91315a11e0859e4981beda5228eeb
Aditya Bhamidipati <abhamidipati@microsoft.com>
2023-05-10 00:50:36 -0400

    Component updates for Ubuntu systems

--- ubuntu/ubuntu-18.x/ubuntu-18.04-hpc/install_mpis.sh
+++ ubuntu/ubuntu-18.x/ubuntu-18.04-hpc/install_mpis.sh
@@ -2,4 +2,4 @@
 set -ex
 
 # Install common MPIs for Ubuntu
-$UBUNTU_COMMON_DIR/install_mpis.sh "d19dfde071b102f888c29a9a69f50cca444c0c3c8d3fac5a553761084588d8b0"
+$UBUNTU_COMMON_DIR/install_mpis.sh "c8868382a7b93286cd70ac46c63489f176bbfe0d76b3e5b2b91ecc0f93272414"

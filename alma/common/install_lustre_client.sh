e874669db7e0dddb1649bfa97f1ecf2e1df55cb9
Aditya Bhamidipati <87097035+abhamidipati0614@users.noreply.github.com>
2023-05-23 12:36:31 -0400

    support for kvp_client on azhpc-images (#242)
    * primer for copying kvp_client
    
    * copy kvp client file in supported distros
    
    * Misc. exclude amlfs packages from udates
    
    * primer for copying kvp_client
    
    * copy kvp client file in supported distros
    
    * Misc. exclude amlfs packages from udates
    
    * move kvp-client file to /opt/azurehpc/tools

--- alma/common/install_lustre_client.sh
+++ alma/common/install_lustre_client.sh
@@ -9,5 +9,6 @@ LUSTRE_VERSION=2.15.1_24_gbaa21ca
 source $ALMA_COMMON_DIR/setup_lustre_repo.sh "$1"
 
 dnf install -y --disableexcludes=main --refresh amlfs-lustre-client-${LUSTRE_VERSION}-$(uname -r | sed -e "s/\.$(uname -p)$//" | sed -re 's/[-_]/\./g')-1
+sed -i "$ s/$/ amlfs*/" /etc/dnf/dnf.conf
 
 $COMMON_DIR/write_component_version.sh "LUSTRE" ${LUSTRE_VERSION}

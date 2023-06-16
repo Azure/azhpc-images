071bb12161608d187d4df0c3a45c57bbca9c079a
Aditya Bhamidipati <87097035+abhamidipati0614@users.noreply.github.com>
2023-06-16 13:15:14 -0700

    topo files resistant to sku service failures (#260)
    * make topo files resistant to sku service failures
    
    * [Disabled]: Fix setting up nvme in compliant SKUs

--- customizations/ndv4.sh
+++ customizations/ndv4.sh
@@ -4,46 +4,7 @@ set -ex
 # Place NDv4 topo file under /opt/microsoft/ndv4
 mkdir -p /opt/microsoft/ndv4
 
-# Place the topology file in /opt/microsoft
-bash -c "cat > /opt/microsoft/ndv4-topo.xml" <<'EOF'
-<system version="1">
-  <cpu numaid="0" affinity="0000ffff,0000ffff" arch="x86_64" vendor="AuthenticAMD" familyid="23" modelid="49">
-    <pci busid="ffff:ff:01.0" class="0x060400" link_speed="16 GT/s" link_width="16">
-      <pci busid="0001:00:00.0" class="0x030200" link_speed="16 GT/s" link_width="16"/>
-      <pci busid="0101:00:00.0" class="0x020700" link_speed="16 GT/s" link_width="16"/>
-      <pci busid="0002:00:00.0" class="0x030200" link_speed="16 GT/s" link_width="16"/>
-      <pci busid="0102:00:00.0" class="0x020700" link_speed="16 GT/s" link_width="16"/>
-    </pci>
-  </cpu>
-  <cpu numaid="1" affinity="0000ffff,0000ffff" arch="x86_64" vendor="AuthenticAMD" familyid="23" modelid="49">
-    <pci busid="ffff:ff:02.0" class="0x060400" link_speed="16 GT/s" link_width="16">
-      <pci busid="0003:00:00.0" class="0x030200" link_speed="16 GT/s" link_width="16"/>
-      <pci busid="0103:00:00.0" class="0x020700" link_speed="16 GT/s" link_width="16"/>
-      <pci busid="0004:00:00.0" class="0x030200" link_speed="16 GT/s" link_width="16"/>
-      <pci busid="0104:00:00.0" class="0x020700" link_speed="16 GT/s" link_width="16"/>
-    </pci>
-  </cpu>
-  <cpu numaid="2" affinity="0000ffff,0000ffff" arch="x86_64" vendor="AuthenticAMD" familyid="23" modelid="49">
-      <pci busid="ffff:ff:03.0" class="0x060400" link_speed="16 GT/s" link_width="16">
-      <pci busid="000b:00:00.0" class="0x030200" link_speed="16 GT/s" link_width="16"/>
-      <pci busid="0105:00:00.0" class="0x020700" link_speed="16 GT/s" link_width="16"/>
-      <pci busid="000c:00:00.0" class="0x030200" link_speed="16 GT/s" link_width="16"/>
-      <pci busid="0106:00:00.0" class="0x020700" link_speed="16 GT/s" link_width="16"/>
-    </pci>
-  </cpu>
-  <cpu numaid="3" affinity="0000ffff,0000ffff" arch="x86_64" vendor="AuthenticAMD" familyid="23" modelid="49">
-    <pci busid="ffff:ff:04.0" class="0x060400" link_speed="16 GT/s" link_width="16">
-      <pci busid="000d:00:00.0" class="0x030200" link_speed="16 GT/s" link_width="16"/>
-      <pci busid="0107:00:00.0" class="0x020700" link_speed="16 GT/s" link_width="16"/>
-      <pci busid="000e:00:00.0" class="0x030200" link_speed="16 GT/s" link_width="16"/>
-      <pci busid="0108:00:00.0" class="0x020700" link_speed="16 GT/s" link_width="16"/>
-    </pci>
-  </cpu>
-</system>
-EOF
-
 # Link the NDv4 topology file into /opt/microsoft/ndv4/
-# Topology file in /opt/microsoft/ndv4-topo.xml will eventually be deleted
 ln -sf /opt/microsoft/ndv4-topo.xml /opt/microsoft/ndv4/topo.xml
 
 ## Set NCCL configuration file for NDv4
--- customizations/ndv5.sh
+++ customizations/ndv5.sh
@@ -4,50 +4,7 @@ set -ex
 # Place NDv5 customizations under /opt/microsoft/ndv5
 mkdir -p /opt/microsoft/ndv5
 
-# Place the topology file in /opt/microsoft
-bash -c "cat > /opt/microsoft/ndv5-topo.xml" <<'EOF'
-<system version="1">
-  <cpu numaid="0" affinity="ffffffff,ffff0000,00000000" arch="x86_64" vendor="GenuineIntel" familyid="6" modelid="143">
-    <pci busid="ffff:ff:01.0" class="0x060400" link_speed="32.0 GT/s PCIe" link_width="16" vendor="0x0000" device="0x0000" subsystem_vendor="0x0000" subsystem_device="0x0000">
-      <pci busid="0001:00:00.0" class="0x030200" link_speed="32.0 GT/s PCIe" link_width="16"/>
-      <pci busid="0101:00:00.0" class="0x020700" link_speed="32.0 GT/s PCIe" link_width="16"/>
-    </pci>
-    <pci busid="ffff:ff:02.0" class="0x060400" link_speed="32.0 GT/s PCIe" link_width="16" vendor="0x0000" device="0x0000" subsystem_vendor="0x0000" subsystem_device="0x0000">
-      <pci busid="0002:00:00.0" class="0x030200" link_speed="32.0 GT/s PCIe" link_width="16"/>
-      <pci busid="0102:00:00.0" class="0x020700" link_speed="32.0 GT/s PCIe" link_width="16"/>
-    </pci>
-    <pci busid="ffff:ff:03.0" class="0x060400" link_speed="32.0 GT/s PCIe" link_width="16" vendor="0x0000" device="0x0000" subsystem_vendor="0x0000" subsystem_device="0x0000">
-      <pci busid="0003:00:00.0" class="0x030200" link_speed="32.0 GT/s PCIe" link_width="16"/>
-      <pci busid="0103:00:00.0" class="0x020700" link_speed="32.0 GT/s PCIe" link_width="16"/>
-    </pci>
-    <pci busid="ffff:ff:04.0" class="0x060400" link_speed="32.0 GT/s PCIe" link_width="16" vendor="0x0000" device="0x0000" subsystem_vendor="0x0000" subsystem_device="0x0000">
-      <pci busid="0008:00:00.0" class="0x030200" link_speed="32.0 GT/s PCIe" link_width="16"/>
-      <pci busid="0104:00:00.0" class="0x020700" link_speed="32.0 GT/s PCIe" link_width="16"/>
-    </pci>
-  </cpu>
-  <cpu numaid="1" affinity="00000000,0000ffff,ffffffff" arch="x86_64" vendor="GenuineIntel" familyid="6" modelid="143">
-    <pci busid="ffff:ff:05.0" class="0x060400" link_speed="32.0 GT/s PCIe" link_width="16" vendor="0x0000" device="0x0000" subsystem_vendor="0x0000" subsystem_device="0x0000">
-      <pci busid="0009:00:00.0" class="0x030200" link_speed="32.0 GT/s PCIe" link_width="16"/>
-      <pci busid="0105:00:00.0" class="0x020700" link_speed="32.0 GT/s PCIe" link_width="16"/>
-    </pci>
-    <pci busid="ffff:ff:06.0" class="0x060400" link_speed="32.0 GT/s PCIe" link_width="16" vendor="0x0000" device="0x0000" subsystem_vendor="0x0000" subsystem_device="0x0000">
-      <pci busid="000a:00:00.0" class="0x030200" link_speed="32.0 GT/s PCIe" link_width="16"/>
-      <pci busid="0106:00:00.0" class="0x020700" link_speed="32.0 GT/s PCIe" link_width="16"/>
-    </pci>
-    <pci busid="ffff:ff:07.0" class="0x060400" link_speed="32.0 GT/s PCIe" link_width="16" vendor="0x0000" device="0x0000" subsystem_vendor="0x0000" subsystem_device="0x0000">
-      <pci busid="000b:00:00.0" class="0x030200" link_speed="32.0 GT/s PCIe" link_width="16"/>
-      <pci busid="0107:00:00.0" class="0x020700" link_speed="32.0 GT/s PCIe" link_width="16"/>
-    </pci>
-    <pci busid="ffff:ff:08.0" class="0x060400" link_speed="32.0 GT/s PCIe" link_width="16" vendor="0x0000" device="0x0000" subsystem_vendor="0x0000" subsystem_device="0x0000">
-      <pci busid="000c:00:00.0" class="0x030200" link_speed="32.0 GT/s PCIe" link_width="16"/>
-      <pci busid="0108:00:00.0" class="0x020700" link_speed="32.0 GT/s PCIe" link_width="16"/>
-    </pci>
-  </cpu>
-</system>
-EOF
-
 # Link the NDv5 topology file into /opt/microsoft/ndv5/
-# Topology file in /opt/microsoft/ndv5-topo.xml will eventually be deleted
 ln -sf /opt/microsoft/ndv5-topo.xml /opt/microsoft/ndv5/topo.xml
 
 ## Set NCCL configuration file for NDv5
@@ -70,4 +27,3 @@ fi
 
 ## load nvidia-peermem module
 modprobe nvidia-peermem
-

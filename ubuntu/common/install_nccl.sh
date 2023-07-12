6c7ce2b03bce00acb28aac36f0b1b9c84c5bd990
Aditya Bhamidipati <87097035+abhamidipati0614@users.noreply.github.com>
2023-07-12 14:41:15 -0400

    NVIDIA GPU Driver update v535.54.03 (#264)

--- ubuntu/common/install_nccl.sh
+++ ubuntu/common/install_nccl.sh
@@ -8,9 +8,9 @@ case ${DISTRIBUTION} in
     "ubuntu18.04") NCCL_VERSION="2.18.3-1"; 
         CUDA_VERSION="12.1";;
     "ubuntu20.04") NCCL_VERSION="2.18.3-1"; 
-        CUDA_VERSION="12.1";;
+        CUDA_VERSION="12.2";;
     "ubuntu22.04") NCCL_VERSION="2.18.3-1"; 
-        CUDA_VERSION="12.1";;
+        CUDA_VERSION="12.2";;
     *) ;;
 esac
 

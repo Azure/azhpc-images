0a901756b3f2cd70418949d24354c3d46ec89090
Aditya Bhamidipati <abhamidipati@microsoft.com>
2023-07-31 13:05:55 -0400

    Fix for nvidia_fabric_manager_version variable naming

--- ubuntu/common/install_nvidia_fabric_manager.sh
+++ ubuntu/common/install_nvidia_fabric_manager.sh
@@ -8,7 +8,7 @@ nvidia_fabricmanager_distribution=$(jq -r '.distribution' <<< $nvidia_fabricmana
 nvidia_fabricmanager_version=$(jq -r '.version' <<< $nvidia_fabricmanager_metadata)
 nvidia_fabricmanager_sha256=$(jq -r '.sha256' <<< $nvidia_fabricmanager_metadata)
 
-nvidia_fabricmanager_download_url=http://developer.download.nvidia.com/compute/cuda/repos/$nvidia_fabricmanager_distribution/x86_64/nvidia-fabricmanager-$nvidia_fabricmanager_version_amd64.deb
+nvidia_fabricmanager_download_url=http://developer.download.nvidia.com/compute/cuda/repos/$nvidia_fabricmanager_distribution/x86_64/nvidia-fabricmanager-${nvidia_fabricmanager_version}_amd64.deb
 $COMMON_DIR/download_and_verify.sh $nvidia_fabricmanager_download_url $nvidia_fabricmanager_sha256
 apt install -y ./nvidia-fabricmanager-${nvidia_fabricmanager_version}_amd64.deb
 apt-mark hold nvidia-fabricmanager-$nvidia_fabricmanager_prefix

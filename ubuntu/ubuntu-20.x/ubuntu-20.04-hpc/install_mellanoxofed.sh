91cb7f37b216b0248eb8539407030e21cd7417e0
Matt Fraser <mattfras@microsoft.com>
2023-09-06 11:25:06 -0700

    update mofed and nccl for Ubuntu 20.04 and 22.04

--- ubuntu/ubuntu-20.x/ubuntu-20.04-hpc/install_mellanoxofed.sh
+++ ubuntu/ubuntu-20.x/ubuntu-20.04-hpc/install_mellanoxofed.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 set -ex
 
-VERSION="23.07-0.5.0.0"
+VERSION="23.07-0.5.1.2"
 TARBALL="MLNX_OFED_LINUX-$VERSION-ubuntu20.04-x86_64.tgz"
 MLNX_OFED_DOWNLOAD_URL=https://content.mellanox.com/ofed/MLNX_OFED-${VERSION}/$TARBALL
 MOFED_FOLDER=$(basename ${MLNX_OFED_DOWNLOAD_URL} .tgz)
 
-$COMMON_DIR/download_and_verify.sh $MLNX_OFED_DOWNLOAD_URL "600c98b6d02850214d33b55e3c6a8ccd2d8f440c2ae3f8cd1b4dc004db7e99fd"
+$COMMON_DIR/download_and_verify.sh $MLNX_OFED_DOWNLOAD_URL "923ddbd48d250b25ba50098ad8852ad6a591df3e975f3e0b9922b752181bdd12"
 tar zxvf ${TARBALL}
 
 ./${MOFED_FOLDER}/mlnxofedinstall --add-kernel-support --skip-unsupported-devices-check --without-fw-update

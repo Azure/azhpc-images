be68c17afbfee3f8de54eaf69d1b0f9b6bf4ea7f
Shivani Patel <87090780+shivanispatel@users.noreply.github.com>
2023-09-12 13:33:13 -0700

    add monitoring service to installation script

--- common/install_azure_persistent_rdma_naming.sh
+++ common/install_azure_persistent_rdma_naming.sh
@@ -3,6 +3,7 @@ set -ex
 
 #
 # install rdma_rename with NAME_FIXED option
+# install rdma_rename monitor
 #
 
 pushd /tmp
@@ -68,3 +69,53 @@ EOF
 
 systemctl enable azure_persistent_rdma_naming.service
 systemctl start azure_persistent_rdma_naming.service
+
+
+#
+# setup systemd service
+#
+
+cat <<EOF >/usr/sbin/azure_persistent_rdma_naming_monitor.sh
+#!/bin/bash
+
+# monitoring service to check that hca_id's are named correctly
+# if incorrect, restart azure_persistent_rdma_naming.service
+
+while true; do 
+
+    for device in \$(ibdev2netdev -v | sort -n | cut -f2 -d' '); do
+        
+        link_layer=\$(ibv_devinfo -d \$device | sed -n 's/^[\ \t]*link_layer:[\ \t]*\([a-zA-Z]*\)\$/\1/p')
+
+        if [[ \$device != *"an"* && \$device != *"ib"* ]]; then 
+            sudo systemctl enable azure_persistent_rdma_naming.service
+            sudo systemctl restart azure_persistent_rdma_naming.service
+            sleep 60
+            break
+        fi
+        
+    done
+
+    sleep 60 
+
+done
+EOF
+chmod 755 /usr/sbin/azure_persistent_rdma_naming_monitor.sh
+
+cat <<EOF >/etc/systemd/system/azure_persistent_rdma_naming_monitor.service
+[Unit]
+Description=Azure persistent RDMA naming Monitor
+After=network.target
+
+[Service]
+Type=simple
+ExecStart=/usr/sbin/azure_persistent_rdma_naming_monitor.sh
+RemainAfterExit=true
+StandardOutput=journal
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+systemctl enable azure_persistent_rdma_naming_monitor.service
+systemctl start azure_persistent_rdma_naming_monitor.service

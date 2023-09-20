f41c610cc24a19984196ec8aa3ccf9ef412099a9
hugo meiland <hugo.meiland@microsoft.com>
2023-09-20 12:24:26 +0200

    start rhel

--- rhel/rhel-8.x/common/install_utils.sh
+++ rhel/rhel-8.x/common/install_utils.sh
@@ -3,9 +3,12 @@ set -ex
 
 # Install Kernel dependencies
 KERNEL=( $(rpm -q kernel | sed 's/kernel\-//g') )
-yum install -y https://repo.almalinux.org/almalinux/8.7/BaseOS/x86_64/os/Packages/kernel-devel-${KERNEL}.rpm \
-    https://repo.almalinux.org/almalinux/8.7/BaseOS/x86_64/os/Packages/kernel-headers-${KERNEL}.rpm \
-    https://repo.almalinux.org/almalinux/8.7/BaseOS/x86_64/os/Packages/kernel-modules-extra-${KERNEL}.rpm
+#yum install -y https://repo.almalinux.org/almalinux/8.7/BaseOS/x86_64/os/Packages/kernel-devel-${KERNEL}.rpm \
+#    https://repo.almalinux.org/almalinux/8.7/BaseOS/x86_64/os/Packages/kernel-headers-${KERNEL}.rpm \
+#    https://repo.almalinux.org/almalinux/8.7/BaseOS/x86_64/os/Packages/kernel-modules-extra-${KERNEL}.rpm
+yum install -y kernel-devel-${KERNEL}.rpm \
+    kernel-headers-${KERNEL}.rpm \
+    kernel-modules-extra-${KERNEL}.rpm
 
 # Install Python 3.8
 yum install -y python3.8

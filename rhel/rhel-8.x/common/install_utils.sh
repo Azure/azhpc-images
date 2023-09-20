69f8cd2c55fb0e6623c47aa52c0ad57fa1dad3af
hugo meiland <hugo.meiland@microsoft.com>
2023-09-20 12:58:59 +0200

    rhel utils

--- rhel/rhel-8.x/common/install_utils.sh
+++ rhel/rhel-8.x/common/install_utils.sh
@@ -5,9 +5,6 @@ yum install -y rhui*
 
 # Install Kernel dependencies
 KERNEL=( $(rpm -q kernel | sed 's/kernel\-//g') )
-#yum install -y https://repo.almalinux.org/almalinux/8.7/BaseOS/x86_64/os/Packages/kernel-devel-${KERNEL}.rpm \
-#    https://repo.almalinux.org/almalinux/8.7/BaseOS/x86_64/os/Packages/kernel-headers-${KERNEL}.rpm \
-#    https://repo.almalinux.org/almalinux/8.7/BaseOS/x86_64/os/Packages/kernel-modules-extra-${KERNEL}.rpm
 yum install -y kernel-devel-${KERNEL} \
     kernel-headers-${KERNEL} \
     kernel-modules-extra-${KERNEL}
@@ -21,7 +18,6 @@ ln -fs /usr/bin/python3.8 /usr/bin/python3
 #wget https://dl.fedoraproject.org/pub/epel/8/Everything/aarch64/Packages/p/pssh-$PSSH_VER.el8.noarch.rpm
 #yum install -y  pssh-$PSSH_VER.el8.noarch.rpm
 #rm -f pssh-$PSSH_VER.el8.noarch.rpm
-subscription-manager repos --enable codeready-builder-for-rhel-8-x86_64-rpms
 yum install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm
 yum install -y pssh
 
@@ -69,21 +65,24 @@ sed -i "$ s/$/ shim*/" /etc/dnf/dnf.conf
 sed -i "$ s/$/ grub2*/" /etc/dnf/dnf.conf
 
 ## Install dkms from the EPEL repository
-wget -r --no-parent -A "dkms-*.el8.noarch.rpm" https://dl.fedoraproject.org/pub/epel/8/Everything/x86_64/Packages/d/
-yum localinstall ./dl.fedoraproject.org/pub/epel/8/Everything/x86_64/Packages/d/dkms-*.el8.noarch.rpm -y
+#wget -r --no-parent -A "dkms-*.el8.noarch.rpm" https://dl.fedoraproject.org/pub/epel/8/Everything/x86_64/Packages/d/
+#yum localinstall ./dl.fedoraproject.org/pub/epel/8/Everything/x86_64/Packages/d/dkms-*.el8.noarch.rpm -y
+yum install -y dkms
 
 ## Install subunit and subunit-devel from EPEL repository
-wget -r --no-parent -A "subunit-*.el8.x86_64.rpm" https://dl.fedoraproject.org/pub/epel/8/Everything/x86_64/Packages/s/
-yum localinstall ./dl.fedoraproject.org/pub/epel/8/Everything/x86_64/Packages/s/subunit-[0-9].*.el8.x86_64.rpm -y
-yum localinstall ./dl.fedoraproject.org/pub/epel/8/Everything/x86_64/Packages/s/subunit-devel-[0-9].*.el8.x86_64.rpm -y
+#wget -r --no-parent -A "subunit-*.el8.x86_64.rpm" https://dl.fedoraproject.org/pub/epel/8/Everything/x86_64/Packages/s/
+#yum localinstall ./dl.fedoraproject.org/pub/epel/8/Everything/x86_64/Packages/s/subunit-[0-9].*.el8.x86_64.rpm -y
+#yum localinstall ./dl.fedoraproject.org/pub/epel/8/Everything/x86_64/Packages/s/subunit-devel-[0-9].*.el8.x86_64.rpm -y
+yum install -y subunit subunit-devel
 
 # Download jq utility
-wget -r --no-parent -A "jq-*.el8.x86_64.rpm" https://repo.almalinux.org/almalinux/8/AppStream/x86_64/os/Packages/
-yum localinstall ./repo.almalinux.org/almalinux/8/AppStream/x86_64/os/Packages/jq-*.el8.x86_64.rpm -y
+#wget -r --no-parent -A "jq-*.el8.x86_64.rpm" https://repo.almalinux.org/almalinux/8/AppStream/x86_64/os/Packages/
+#yum localinstall ./repo.almalinux.org/almalinux/8/AppStream/x86_64/os/Packages/jq-*.el8.x86_64.rpm -y
+yum install -y jq
 
 # Remove rpm files
-rm -rf ./dl.fedoraproject.org/
-rm -rf ./repo.almalinux.org/
+#rm -rf ./dl.fedoraproject.org/
+#rm -rf ./repo.almalinux.org/
 
 # Install azcopy tool 
 # To copy blobs or files to or from a storage account.

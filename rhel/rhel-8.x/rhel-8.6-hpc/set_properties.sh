e82a001685de130f8b146b249ff9aec35447bdfa
hugo meiland <hugo.meiland@microsoft.com>
2023-10-24 13:27:45 +0200

    rhel properties

--- rhel/rhel-8.x/rhel-8.6-hpc/set_properties.sh
+++ rhel/rhel-8.x/rhel-8.6-hpc/set_properties.sh
@@ -2,6 +2,6 @@
 
 export TOP_DIR=../../..
 export COMMON_DIR=../../../common
-export ALMA_COMMON_DIR=../../common
+export RHEL_COMMON_DIR=../../common
 export TEST_DIR=../../../tests
 export DISTRIBUTION=$(. /etc/os-release;echo $ID$VERSION_ID)

52cf00e6de27a19e312cfb86a96fc3df1cfad17c
hugo meiland <hugo.meiland@microsoft.com>
2023-10-24 13:29:19 +0200

    rhel lustre

--- rhel/common/install_lustre_client.sh
+++ rhel/common/install_lustre_client.sh
@@ -6,7 +6,7 @@ LUSTRE_VERSION=2.15.1_24_gbaa21ca
 # Expected params:
 # $1 = the major version of the distro. "8" for RHEL/Alma8, "9" for RHEL/Alma9.
 
-source $COMMON_DIR/setup_lustre_repo.sh "$1"
+source $COMMON_DIR/../rhel/common/setup_lustre_repo.sh "$1"
 
 dnf install -y --disableexcludes=main --refresh amlfs-lustre-client-${LUSTRE_VERSION}-$(uname -r | sed -e "s/\.$(uname -p)$//" | sed -re 's/[-_]/\./g')-1
 sed -i "$ s/$/ amlfs*/" /etc/dnf/dnf.conf

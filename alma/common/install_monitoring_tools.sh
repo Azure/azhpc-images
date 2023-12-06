ffc035706ef51486eb982a050fe90bad407b29c0
Matt Fraser <mattfras@microsoft.com>
2023-12-05 18:42:29 -0800

    fix alma install

--- alma/common/install_monitoring_tools.sh
+++ alma/common/install_monitoring_tools.sh
@@ -3,7 +3,7 @@ set -ex
 
 # Dependencies
 ln -fs /usr/bin/python3.8 /usr/bin/python3
-python3 -m pip install --upgrade pip
+/usr/bin/python3 -m pip install --upgrade pip
 # Adding path to sudo user
 sed -i 's/.*secure_path.*/Defaults    secure_path = "\/usr\/local\/sbin:\/usr\/local\/bin:\/sbin:\/bin:\/usr\/sbin:\/usr\/bin\/"/' /etc/sudoers
 

c9b3d55a909cbb234371fdf36c1cfbdef2b6cc5a
KimPhillips128 <v-kiphillips@microsoft.com>
2024-01-23 10:14:23 -0800

    Updating Ubuntu2004 install scripts

--- ubuntu/ubuntu-20.x/ubuntu-20.04-hpc/install_mpis.sh
+++ ubuntu/ubuntu-20.x/ubuntu-20.04-hpc/install_mpis.sh
@@ -2,4 +2,4 @@
 set -ex
 
 # Install common MPIs for Ubuntu
-$UBUNTU_COMMON_DIR/install_mpis.sh "d1a1b73597c96b66b20e16bce880770d659c51f21214cfe1695805c785ed7f18"
+$UBUNTU_COMMON_DIR/install_mpis.sh

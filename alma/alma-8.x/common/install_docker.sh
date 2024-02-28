7152d5c1d7bb07b312d2aa00c5a655b554b43e17
KimPhillips128 <v-kiphillips@microsoft.com>
2024-02-27 16:09:39 -0800

    Updating changes from Master prior to merge

--- alma/alma-8.x/common/install_docker.sh
+++ alma/alma-8.x/common/install_docker.sh
@@ -8,13 +8,9 @@ yum install -y moby-cli
 # Install NVIDIA Docker
 # Reference: https://docs.nvidia.com/datacenter/cloud-native/container-toolkit/install-guide.html
 # Setting up NVIDIA Container Toolkit
-case ${DISTRIBUTION} in
-    "almalinux8.7") distribution="rhel8.7";
-        ;;
-    *) ;;
-esac
+curl -s -L https://nvidia.github.io/libnvidia-container/stable/rpm/nvidia-container-toolkit.repo | \
+  sudo tee /etc/yum.repos.d/nvidia-container-toolkit.repo
 
-curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.repo | tee /etc/yum.repos.d/nvidia-docker.repo
 # MIG Capability on A100
 # curl -s -L https://nvidia.github.io/nvidia-container-runtime/experimental/$distribution/nvidia-container-runtime.list | tee /etc/yum.repos.d/nvidia-container-runtime.list
 
@@ -28,20 +24,21 @@ yum install -y nvidia-container-runtime
 # Mark the installed packages on hold to disable updates
 sed -i "$ s/$/ *nvidia-container*/" /etc/dnf/dnf.conf
 
-wget https://raw.githubusercontent.com/NVIDIA/nvidia-docker/master/nvidia-docker
-cp nvidia-docker /bin/
-chmod +x /bin/nvidia-docker
-wget https://raw.githubusercontent.com/NVIDIA/nvidia-docker/master/daemon.json
-cp daemon.json /etc/docker/
+nvidia-ctk runtime configure --runtime=docker
+
+# enable and restart the docker daemon to complete the installation
+systemctl enable docker
+systemctl restart docker
 
 # Working setup can be tested by running a base CUDA container
 # nvidia-docker run -e NVIDIA_VISIBLE_DEVICES=all nvidia/cuda:11.0-base nvidia-smi
 
 # disabling aufs, btrfs, zfs and devmapper snapshotter plugins
 mkdir -p /etc/containerd
-cat << EOF | tee -a /etc/containerd/config.toml
-disabled_plugins = ["cri", "zfs", "aufs", "btrfs", "devmapper"]
-EOF
+containerd config default | sudo tee /etc/containerd/config.toml
+sed -i 's/runtime = "runc"/runtime = "nvidia-container-runtime"/g' /etc/containerd/config.toml
+# disabled_plugins = ["cri", "zfs", "aufs", "btrfs", "devmapper"]
+
 
 # restart containerd service
 systemctl restart containerd
@@ -49,13 +46,10 @@ systemctl restart containerd
 # status of containerd snapshotter plugins
 ctr plugin ls
 
-# enable and restart the docker daemon to complete the installation
-systemctl enable docker
-systemctl restart docker
 
 # Write the docker version to components file
-docker_version=$(nvidia-docker --version | awk -F' ' '{print $3}')
-$COMMON_DIR/write_component_version.sh "nvidia_docker" ${docker_version::-1}
+docker_version=$(docker --version | awk -F' ' '{print $3}')
+$COMMON_DIR/write_component_version.sh "docker" ${docker_version::-1}
 
 # Clean repos
 rm -rf /etc/yum.repos.d/nvidia-*

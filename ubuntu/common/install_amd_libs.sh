243ade0a49197a5b0c322c2fa4ad072867382a3b
Matt Fraser <mattfras@microsoft.com>
2024-03-05 19:16:04 -0800

    Revert "Update AMD lib install"
    This reverts commit e2af376fa079ad78451ff2bae7b062e74f84aeb7.

--- ubuntu/common/install_amd_libs.sh
+++ ubuntu/common/install_amd_libs.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 set -ex
 
-# Set the GCC version
-export GCC_VERSION=$(gcc --version | grep gcc | awk '{print $4}')
+AOCC_VERSION=4.0.0_1
 
-$COMMON_DIR/install_amd_libs.sh
+# install dependency
+wget https://download.amd.com/developer/eula/aocc-compiler/aocc-compiler-${AOCC_VERSION}_amd64.deb
+apt install -y ./aocc-compiler-${AOCC_VERSION}_amd64.deb
+
+rm aocc-compiler-${AOCC_VERSION}_amd64.deb 
+
+$COMMON_DIR/write_component_version.sh "AOCC" ${AOCC_VERSION}

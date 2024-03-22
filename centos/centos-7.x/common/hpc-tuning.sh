77c0cdfa733c7941f445e42c37a39184123ec98f
Matt Fraser <mattfras@microsoft.com>
2024-03-22 14:13:28 -0700

    delete old distros

--- centos/centos-7.x/common/hpc-tuning.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Disable some unneeded services by default (administrators can re-enable if desired)
-systemctl disable wpa_supplicant
-systemctl disable abrtd
-
-../../common/hpc-tuning.sh
--- ubuntu/ubuntu-18.x/ubuntu-18.04-hpc/set_properties.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-export TOP_DIR=../../..
-export COMMON_DIR=../../../common
-export UBUNTU_COMMON_DIR=../../common
-export TEST_DIR=../../../tests
-export DISTRIBUTION=$(. /etc/os-release;echo $ID$VERSION_ID)

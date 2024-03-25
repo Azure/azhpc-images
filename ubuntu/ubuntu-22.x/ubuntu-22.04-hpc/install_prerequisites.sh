22f793dcf8ba89de7071c178bb00f8aee9489c5f
Matt Fraser <mattfras@microsoft.com>
2024-03-25 14:07:01 -0700

    test no apt upgrade

--- ubuntu/ubuntu-22.x/ubuntu-22.04-hpc/install_prerequisites.sh
+++ ubuntu/ubuntu-22.x/ubuntu-22.04-hpc/install_prerequisites.sh
@@ -2,11 +2,11 @@
 set -ex
 
 # Don't allow the kernel to be updated
-apt-mark hold linux-azure
+# apt-mark hold linux-azure
 
 # upgrade pre-installed components
 apt update
-apt upgrade -y
+# apt upgrade -y
 
 # jq is needed to parse the component versions from the requirements.json file
 apt install -y jq

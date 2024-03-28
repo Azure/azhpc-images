466aef5b445eae707e4d871f3d426da67999b851
Matt Fraser <mattfras@microsoft.com>
2024-03-28 16:14:33 -0700

    update ubuntu to use the LTS kernel

--- ubuntu/ubuntu-22.x/ubuntu-22.04-hpc/install_prerequisites.sh
+++ ubuntu/ubuntu-22.x/ubuntu-22.04-hpc/install_prerequisites.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 set -ex
 
-# Don't allow the kernel to be updated
-apt-mark hold linux-azure
-
 # upgrade pre-installed components
 apt update
-apt upgrade -y
+
+# install LTS kernel
+apt install -y linux-azure-lts
 
 # jq is needed to parse the component versions from the requirements.json file
 apt install -y jq

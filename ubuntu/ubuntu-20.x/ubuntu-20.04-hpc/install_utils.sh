b4370aea9c73a6a079fe5d45dde26e4963eee4be
Matt Fraser <mattfras@microsoft.com>
2024-03-28 16:55:20 -0700

    move PMC repo to prerequisites

--- ubuntu/ubuntu-20.x/ubuntu-20.04-hpc/install_utils.sh
+++ ubuntu/ubuntu-20.x/ubuntu-20.04-hpc/install_utils.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 set -ex
 
-# Setup microsoft packages repository for moby
-# Download the repository configuration package
-curl https://packages.microsoft.com/config/ubuntu/20.04/prod.list > ./microsoft-prod.list
-# Copy the generated list to the sources.list.d directory
-cp ./microsoft-prod.list /etc/apt/sources.list.d/
-# Install the Microsoft GPG public key
-curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
-cp ./microsoft.gpg /etc/apt/trusted.gpg.d/
+# # Setup microsoft packages repository for moby
+# # Download the repository configuration package
+# curl https://packages.microsoft.com/config/ubuntu/20.04/prod.list > ./microsoft-prod.list
+# # Copy the generated list to the sources.list.d directory
+# cp ./microsoft-prod.list /etc/apt/sources.list.d/
+# # Install the Microsoft GPG public key
+# curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
+# cp ./microsoft.gpg /etc/apt/trusted.gpg.d/
 
 #apt-get install packages
 

533c6ab9457fd3911e1d9e2e1ba142e02080ba51
Matt Fraser <mattfras@microsoft.com>
2024-03-28 17:12:51 -0700

    update LTS kernel package name

--- ubuntu/ubuntu-22.x/ubuntu-22.04-hpc/install_prerequisites.sh
+++ ubuntu/ubuntu-22.x/ubuntu-22.04-hpc/install_prerequisites.sh
@@ -1,20 +1,11 @@
 #!/bin/bash
 set -ex
 
-# Setup microsoft packages repository for moby
-# Download the repository configuration package
-curl https://packages.microsoft.com/config/ubuntu/22.04/prod.list > ./microsoft-prod.list
-# Copy the generated list to the sources.list.d directory
-cp ./microsoft-prod.list /etc/apt/sources.list.d/
-# Install the Microsoft GPG public key
-curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
-cp ./microsoft.gpg /etc/apt/trusted.gpg.d/
-
 # upgrade pre-installed components
 apt update
 
 # install LTS kernel
-apt install -y linux-azure-lts
+apt install -y linux-azure-lts-22.04
 
 # jq is needed to parse the component versions from the requirements.json file
 apt install -y jq

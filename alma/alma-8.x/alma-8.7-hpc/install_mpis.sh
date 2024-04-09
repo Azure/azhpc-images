0795da501f6befe1263f1c4484397b805cda60d4
Matt Fraser <mattfras@microsoft.com>
2024-04-09 08:27:18 -0700

    copy tests to rebuild

--- alma/alma-8.x/alma-8.7-hpc/install_mpis.sh
+++ alma/alma-8.x/alma-8.7-hpc/install_mpis.sh
@@ -32,6 +32,7 @@ $COMMON_DIR/write_component_version.sh "HPCX" $HPCX_VERSION
 
 # rebuild HPCX with PMIx
 ${HPCX_PATH}/utils/hpcx_rebuild.sh --with-hcoll --ompi-extra-config "--with-pmix=${PMIX_PATH} --enable-orterun-prefix-by-default"
+cp -r ${HPCX_PATH}/ompi/tests ${HPCX_PATH}/hpcx-rebuild
 
 # exclude ucx from updates
 sed -i "$ s/$/ ucx*/" /etc/dnf/dnf.conf

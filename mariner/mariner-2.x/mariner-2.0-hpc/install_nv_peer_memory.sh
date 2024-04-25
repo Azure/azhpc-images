78ce27bfbc88dc209f0f77e6d9ca2d12f567e364
yakovdyadkin <yakovdyad@gmail.com>
2024-04-25 18:00:01 -0500

    Install nv_peer_mem kmod with dkms

--- mariner/mariner-2.x/mariner-2.0-hpc/install_nv_peer_memory.sh
+++ mariner/mariner-2.x/mariner-2.0-hpc/install_nv_peer_memory.sh
@@ -7,26 +7,34 @@ NV_PEER_MEMORY_VERSION_PREFIX=$(echo ${NV_PEER_MEMORY_VERSION} | awk -F- '{print
 TARBALL="${NV_PEER_MEMORY_VERSION}.tar.gz"
 NV_PEER_MEM_DOWNLOAD_URL="https://github.com/Mellanox/nv_peer_memory/archive/refs/tags/${TARBALL}"
 wget ${NV_PEER_MEM_DOWNLOAD_URL}
-tar -xvf $TARBALL
 
-# Build and install NV Peer Memory
-pushd nv_peer_memory-${NV_PEER_MEMORY_VERSION}
+# Install NV Peer Memory
+dkms ldtarball $TARBALL
+dkms status
+dkms install nv_peer_mem -v $NV_PEER_MEMORY_VERSION_PREFIX
 
-# ./build_module.sh's rpmbuild command does not work, following "simialr" steps below
-tmpdir=$(mktemp -d /tmp/nv.XXXXXX)
-cp -r . $tmpdir/nvidia_peer_memory-$NV_PEER_MEMORY_VERSION_PREFIX
+modprobe nv_peer_mem
 
-pushd $tmpdir > /dev/null
-tar czf nvidia_peer_memory-$NV_PEER_MEMORY_VERSION_PREFIX.tar.gz  --exclude='.*' --exclude=build_release.sh nvidia_peer_memory-$NV_PEER_MEMORY_VERSION_PREFIX
-popd > /dev/null
+# tar -xvf $TARBALL
 
-echo "Building source rpm for nvidia_peer_memory..."
-mkdir -p $tmpdir/topdir/{SRPMS,RPMS,SPECS,BUILD,SOURCES}
-cp $tmpdir/nvidia_peer_memory-$NV_PEER_MEMORY_VERSION_PREFIX.tar.gz $tmpdir/topdir/SOURCES/
-rpmbuild -bb --nodeps --define "_topdir $tmpdir/topdir" --define 'dist %{nil}' --define '_source_filedigest_algorithm md5' --define '_binary_filedigest_algorithm md5' $tmpdir/nvidia_peer_memory-$NV_PEER_MEMORY_VERSION_PREFIX/nvidia_peer_memory.spec
-rpm -ivh $tmpdir/topdir/RPMS/x86_64/nvidia_peer_memory-$NV_PEER_MEMORY_VERSION.x86_64.rpm
+# # Build and install NV Peer Memory
+# pushd nv_peer_memory-${NV_PEER_MEMORY_VERSION}
 
-popd
+# # ./build_module.sh's rpmbuild command does not work, following "simialr" steps below
+# tmpdir=$(mktemp -d /tmp/nv.XXXXXX)
+# cp -r . $tmpdir/nvidia_peer_memory-$NV_PEER_MEMORY_VERSION_PREFIX
+
+# pushd $tmpdir > /dev/null
+# tar czf nvidia_peer_memory-$NV_PEER_MEMORY_VERSION_PREFIX.tar.gz  --exclude='.*' --exclude=build_release.sh nvidia_peer_memory-$NV_PEER_MEMORY_VERSION_PREFIX
+# popd > /dev/null
+
+# echo "Building source rpm for nvidia_peer_memory..."
+# mkdir -p $tmpdir/topdir/{SRPMS,RPMS,SPECS,BUILD,SOURCES}
+# cp $tmpdir/nvidia_peer_memory-$NV_PEER_MEMORY_VERSION_PREFIX.tar.gz $tmpdir/topdir/SOURCES/
+# rpmbuild -bb --nodeps --define "_topdir $tmpdir/topdir" --define 'dist %{nil}' --define '_source_filedigest_algorithm md5' --define '_binary_filedigest_algorithm md5' $tmpdir/nvidia_peer_memory-$NV_PEER_MEMORY_VERSION_PREFIX/nvidia_peer_memory.spec
+# rpm -ivh $tmpdir/topdir/RPMS/x86_64/nvidia_peer_memory-$NV_PEER_MEMORY_VERSION.x86_64.rpm
+
+# popd
 
 lsmod | grep nv_peer_mem
 

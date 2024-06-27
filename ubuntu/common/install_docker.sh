0ffbac9a9b41853de5db582e43250c79bb9a5366
Matt Fraser <mattfras@microsoft.com>
2024-06-26 21:16:05 -0700

    fix moby var

--- ubuntu/common/install_docker.sh
+++ ubuntu/common/install_docker.sh
@@ -52,7 +52,7 @@ systemctl restart docker
 # Write the docker version to components file
 moby_version=$(apt list --installed | grep moby-engine | awk -F' ' '{print $2}')
 docker_version=$(nvidia-docker --version | awk -F' ' '{print $3}')
-$COMMON_DIR/write_component_version.sh "MOBY-ENGINE" ${moby_version}
+$COMMON_DIR/write_component_version.sh "MOBY_ENGINE" ${moby_version}
 $COMMON_DIR/write_component_version.sh "DOCKER" ${docker_version::-1}
 
 # Remove unwanted repos

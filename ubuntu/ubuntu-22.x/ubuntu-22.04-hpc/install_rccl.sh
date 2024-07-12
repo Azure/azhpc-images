080e79a1ce1a318f0dab83e86c494d2823de1559
Scott Moe <moes1@noreply.com>
2024-07-12 12:37:33 -0500

    addressing several comments

--- ubuntu/ubuntu-22.x/ubuntu-22.04-hpc/install_rccl.sh
+++ ubuntu/ubuntu-22.x/ubuntu-22.04-hpc/install_rccl.sh
@@ -1,28 +1,28 @@
 #!/bin/bash
 set -ex
 
-#sudo apt-get install -y rocblas rccl-dev rccl-rdma-sharp-plugins
 sudo apt install libstdc++-12-dev
 sudo apt remove -y rccl
-cd ~
+pushd ~
 git clone https://github.com/rocm/rccl
-cd rccl
-mkdir build
-cd build
+popd
+mkdir ~/rccl/build
+pushd ~/rccl/build
 CXX=/opt/rocm/bin/hipcc cmake -DCMAKE_PREFIX_PATH=/opt/rocm/ -DCMAKE_INSTALL_PREFIX=/opt/rccl ..
 make -j 32
 sudo make install
-cd ~
+popd
 
+pushd ~
 
 sudo sysctl kernel.numa_balancing=0
 echo "kernel.numa_balancing=0" | sudo tee -a /etc/sysctl.conf
 
 
 git clone https://github.com/ROCmSoftwarePlatform/rccl-tests
-cd rccl-tests
+pushd ~/rccl-tests
 
-source /opt/hpcx-v2.16-gcc-mlnx_ofed-ubuntu22.04-cuda12-gdrcopy2-nccl2.18-x86_64/hpcx-init.sh
+source /opt/hpcx*/hpcx-init.sh
 hpcx_load
 
 HPCX="/opt/hpcx-v2.16-gcc-mlnx_ofed-ubuntu22.04-cuda12-gdrcopy2"
@@ -37,7 +37,7 @@ echo "gfx90a" >> target.lst
 ROCM_TARGET_LST=$(pwd)/target.lst make MPI=1 \
         NCCL_HOME=$RCCLDIR CUSTOM_RCCL_LIB=$RCCLLIB
 
-cd ~
+popd
 
 DEST_TEST_DIR=/opt/rccl-tests
 sudo mkdir -p $DEST_TEST_DIR
@@ -45,15 +45,12 @@ sudo mkdir -p $DEST_TEST_DIR
 sudo cp -r ~/rccl-tests/build/* $DEST_TEST_DIR
 rm -rf rccl-tests
 
-cd ~
 git clone https://github.com/ROCm/rdma-perftest
 sudo mkdir /opt/rocm-perftest
-cd rdma-perftest
+pushd ~/rdma-perftest
 ./autogen.sh
 ./configure --enable-rocm --with-rocm=/opt/rocm --prefix=/opt/rocm-perftest/
 make -j 32
 sudo make install
 
-cd ~
-sudo ./install_rdc.sh
-
+popd

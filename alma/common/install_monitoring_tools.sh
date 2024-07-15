a5300adcdddef7af7af4d23884dddbd3aadf4ccc
Matt Fraser <Matt.Fraser@microsoft.com>
2024-07-15 15:22:20 -0700

    update moneo bashrc

--- alma/common/install_monitoring_tools.sh
+++ alma/common/install_monitoring_tools.sh
@@ -26,8 +26,8 @@ pushd $MONITOR_DIR
 popd
 
 # add an alias for Moneo
-if ! grep -qxF "alias moneo='python3 /opt/azurehpc/tools/Moneo/moneo.py'" /etc/bashrc; then
-    echo "alias moneo='python3 /opt/azurehpc/tools/Moneo/moneo.py'" >> /etc/bashrc
+if ! grep -qxF "alias moneo='python3 /opt/azurehpc/tools/Moneo/moneo.py'" /etc//etc/bash.bashrc; then
+    echo "alias moneo='python3 /opt/azurehpc/tools/Moneo/moneo.py'" >> /etc//etc/bash.bashrc
 fi
 
 $COMMON_DIR/write_component_version.sh "MONEO" ${MONEO_VERSION}

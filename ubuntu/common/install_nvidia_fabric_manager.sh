a4b1a38406dcb4eace77957029aaaaf35253c19a
Matt Fraser <Matt.Fraser@microsoft.com>
2024-07-15 17:11:07 -0700

    remove fabric manager prefix config

--- ubuntu/common/install_nvidia_fabric_manager.sh
+++ ubuntu/common/install_nvidia_fabric_manager.sh
@@ -3,11 +3,11 @@ set -ex
 
 # Set NVIDIA fabricmanager version
 nvidia_fabricmanager_metadata=$(jq -r '.nvidia."'"$DISTRIBUTION"'".fabricmanager' <<< $COMPONENT_VERSIONS)
-NVIDIA_FABRICMANAGER_PREFIX=$(jq -r '.prefix' <<< $nvidia_fabricmanager_metadata)
 NVIDIA_FABRICMANAGER_DISTRIBUTION=$(jq -r '.distribution' <<< $nvidia_fabricmanager_metadata)
 NVIDIA_FABRICMANAGER_VERSION=$(jq -r '.version' <<< $nvidia_fabricmanager_metadata)
 NVIDIA_FABRICMANAGER_SHA256=$(jq -r '.sha256' <<< $nvidia_fabricmanager_metadata)
 
+NVIDIA_FABRICMANAGER_PREFIX=$(echo $NVIDIA_FABRICMANAGER_VERSION | cut -d '_' -f1)
 NVIDIA_FABRIC_MNGR_URL=http://developer.download.nvidia.com/compute/cuda/repos/${NVIDIA_FABRICMANAGER_DISTRIBUTION}/x86_64/nvidia-fabricmanager-${NVIDIA_FABRICMANAGER_VERSION}_amd64.deb
 $COMMON_DIR/download_and_verify.sh ${NVIDIA_FABRIC_MNGR_URL} ${NVIDIA_FABRICMANAGER_SHA256}
 apt install -y ./nvidia-fabricmanager-${NVIDIA_FABRICMANAGER_VERSION}_amd64.deb

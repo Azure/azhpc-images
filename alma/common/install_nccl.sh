98b3a67079e9752e09e46c862f8dc0ef7f9f4b00
Matt Fraser <Matt.Fraser@microsoft.com>
2024-07-15 19:44:39 -0700

    fix nccl paths

--- alma/common/install_nccl.sh
+++ alma/common/install_nccl.sh
@@ -15,7 +15,6 @@ NCCL_DOWNLOAD_URL=https://github.com/NVIDIA/nccl/archive/refs/tags/${TARBALL}
 # Install NCCL
 yum install -y rpm-build rpmdevtools
 
-pushd /tmp
 $COMMON_DIR/download_and_verify.sh ${NCCL_DOWNLOAD_URL} ${NCCL_SHA256}
 tar -xvf ${TARBALL}
 

4b784e8f780efd8c53b303fa0b4a6267c96316c9
Matt Fraser <Matt.Fraser@microsoft.com>
2024-07-19 11:08:28 -0700

    Only enable RDMA in WAAgent in Ubuntu

--- ubuntu/common/install_waagent.sh
+++ ubuntu/common/install_waagent.sh
@@ -18,6 +18,7 @@ python3 setup.py install --register-service
 popd
 
 # Configure WALinuxAgent
+sed -i -e 's/# OS.EnableRDMA=y/OS.EnableRDMA=y/g' /etc/waagent.conf
 sed -i -e 's/Provisioning.MonitorHostName=n/Provisioning.MonitorHostName=y/g' /etc/waagent.conf
 
 $COMMON_DIR/install_waagent.sh

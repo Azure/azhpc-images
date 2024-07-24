5cdd5a75e2e34f6b33e7df86d8d90d50f83903a0
Matt Fraser <Matt.Fraser@microsoft.com>
2024-07-24 11:54:32 -0700

    update azcopy URL

--- common/install_azcopy.sh
+++ common/install_azcopy.sh
@@ -7,7 +7,7 @@ azcopy_version=$(jq -r '.version' <<< $azcopy_metadata)
 azcopy_release=$(jq -r '.release' <<< $azcopy_metadata)
 azcopy_sha256=$(jq -r '.sha256' <<< $azcopy_metadata)
 TARBALL="azcopy_linux_amd64_$azcopy_version.tar.gz"
-AZCOPY_DOWNLOAD_URL="https://azcopyvnext.azureedge.net/$azcopy_release/$TARBALL"
+AZCOPY_DOWNLOAD_URL="https://azcopyvnext.azureedge.net/releases/$azcopy_release/$TARBALL"
 
 ${COMMON_DIR}/download_and_verify.sh ${AZCOPY_DOWNLOAD_URL} ${azcopy_sha256}
 tar -xvf ${TARBALL}

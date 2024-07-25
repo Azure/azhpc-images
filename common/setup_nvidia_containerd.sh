e2a3e01ac7b023506a7bbd4b04ad3f45d5a2ed52
Matt Fraser <Matt.Fraser@microsoft.com>
2024-07-25 11:53:59 -0700

    Revert "break out containerd setup"
    This reverts commit 0dd94c24405937891cb6a4cf28307fa2afd0b2fd.

--- common/setup_nvidia_containerd.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -ex
-
-# Configure containerd to use NVIDIA runtime
-mkdir -p /etc/containerd
-containerd config default | sudo tee /etc/containerd/config.toml
-sed -i 's/runtime = "runc"/runtime = "nvidia-container-runtime"/g' /etc/containerd/config.toml
-sed -i 's/disabled_plugins = \[\]/disabled_plugins = \["cri", "zfs", "aufs", "btrfs", "devmapper"\]/g' /etc/containerd/config.toml

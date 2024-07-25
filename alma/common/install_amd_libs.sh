72bb93769e0ee078774adffafbe0ef3d0f267634
Matt Fraser <Matt.Fraser@microsoft.com>
2024-07-24 20:36:50 -0700

    Fix aocc folder for alma

--- alma/common/install_amd_libs.sh
+++ alma/common/install_amd_libs.sh
@@ -6,9 +6,10 @@ $COMMON_DIR/install_amd_libs.sh
 # Set AOCC version
 amd_metadata=$(jq -r '.amd."'"$DISTRIBUTION"'"' <<< $COMPONENT_VERSIONS)
 AOCC_VERSION=$(jq -r '.aocc.version' <<< $amd_metadata)
+AOCC_FOLDER_VERSION=$(echo $AOCC_VERSION | cut -d'.' -f1-2 --output-delimiter='-')
 
 # install dependency
-wget https://download.amd.com/developer/eula/aocc-compiler/aocc-compiler-${AOCC_VERSION}.x86_64.rpm
+wget https://download.amd.com/developer/eula/aocc/aocc-${AOCC_FOLDER_VERSION}/aocc-compiler-${AOCC_VERSION}.x86_64.rpm
 dnf install -y ./aocc-compiler-${AOCC_VERSION}.x86_64.rpm
 
 rm aocc-compiler-${AOCC_VERSION}.x86_64.rpm

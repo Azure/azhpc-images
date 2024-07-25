32796a6b6111ef8b65b268719b9f698789e94da2
Matt Fraser <Matt.Fraser@microsoft.com>
2024-07-24 18:27:39 -0700

    fix alma gdrcopy installation

--- alma/common/install_gdrcopy.sh
+++ alma/common/install_gdrcopy.sh
@@ -15,9 +15,9 @@ tar -xvf $TARBALL
 
 pushd gdrcopy-${GDRCOPY_VERSION}/packages/
 CUDA=/usr/local/cuda ./build-rpm-packages.sh
-rpm -Uvh gdrcopy-kmod-${GDRCOPY_VERSION}dkms.noarch.${GDRCOPY_DISTRIBUTION}.rpm
-rpm -Uvh gdrcopy-${GDRCOPY_VERSION}.x86_64.${GDRCOPY_DISTRIBUTION}.rpm
-rpm -Uvh gdrcopy-devel-${GDRCOPY_VERSION}.noarch.${GDRCOPY_DISTRIBUTION}.rpm
+rpm -Uvh gdrcopy-kmod-${GDRCOPY_VERSION}-1dkms.${GDRCOPY_DISTRIBUTION}.noarch.rpm
+rpm -Uvh gdrcopy-${GDRCOPY_VERSION}-1.${GDRCOPY_DISTRIBUTION}.x86_64.rpm
+rpm -Uvh gdrcopy-devel-${GDRCOPY_VERSION}-1.${GDRCOPY_DISTRIBUTION}.noarch.rpm
 sed -i "$ s/$/ gdrcopy*/" /etc/dnf/dnf.conf
 popd
 

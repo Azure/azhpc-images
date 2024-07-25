b4d4f6486b314786e3e5e953be6f1df1895deba0
Matt Fraser <Matt.Fraser@microsoft.com>
2024-07-24 20:48:59 -0700

    update docker install

--- ubuntu/common/install_nvidia_container_toolkit.sh
+++ ubuntu/common/install_nvidia_container_toolkit.sh
@@ -20,12 +20,3 @@ apt-mark hold libnvidia-container1
 
 # Configure NVIDIA Container Toolkit
 nvidia-ctk runtime configure --runtime=docker
-
-# Configure containerd to use NVIDIA runtime
-mkdir -p /etc/containerd
-containerd config default | sudo tee /etc/containerd/config.toml
-sed -i 's/runtime = "runc"/runtime = "nvidia-container-runtime"/g' /etc/containerd/config.toml
-sed -i 's/disabled_plugins = \[\]/disabled_plugins = \["cri", "zfs", "aufs", "btrfs", "devmapper"\]/g' /etc/containerd/config.toml
-
-# Remove unwanted repos
-rm -f /etc/apt/sources.list.d/nvidia*

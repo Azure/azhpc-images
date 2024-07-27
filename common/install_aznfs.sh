c92c4ad8cb2129dffe7c22d6801b8d324e08575e
Matt Fraser <Matt.Fraser@microsoft.com>
2024-07-26 18:40:23 -0700

    fix path

--- common/install_aznfs.sh
+++ common/install_aznfs.sh
@@ -5,7 +5,7 @@ set -ex
 aznfs_metadata=$(jq -r '.aznfs."'"$DISTRIBUTION"'"' <<< $COMPONENT_VERSIONS)
 AZNFS_VERSION=$(jq -r '.version' <<< $aznfs_metadata)
 AZNFS_SHA256=$(jq -r '.sha256' <<< $aznfs_metadata)
-AZNFS_DOWNLOAD_URL=https://github.com/Azure/AZNFS-mount/releases/${AZNFS_VERSION}/download/aznfs_install.sh
+AZNFS_DOWNLOAD_URL=https://github.com/Azure/AZNFS-mount/releases/download/${AZNFS_VERSION}/aznfs_install.sh
 
 ${COMMON_DIR}/download_and_verify.sh $AZNFS_DOWNLOAD_URL $AZNFS_SHA256
 

a4ebfa107803089b469fd05f56566f8d8b24c63c
Matt Fraser <Matt.Fraser@microsoft.com>
2024-09-12 10:09:57 -0700

    add ip test (needs refining)

--- tests/test-definitions.sh
+++ tests/test-definitions.sh
@@ -45,6 +45,10 @@ function verify_ib_device_status {
     # verify ifconfig
     ifconfig | grep "ib[[:digit:]]:\|ibP"
     check_exit_code "IB device is configured" "IB device not configured"
+
+    #verify hostname -i returns IP address only
+    hostname -i | grep -Eo "([0-9]{1,3}[\.]){3}[0-9]{1,3}"
+    check_exit_code "Hostname -i returns IP address" "Hostname -i does not return IP address"
 }
 
 function verify_hpcx_installation {

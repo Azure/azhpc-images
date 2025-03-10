acaf2c49c31fc45c026a82a4a96f86555746c783
hugo meiland <hugo.meiland@microsoft.com>
2025-03-10 16:38:52 +0100

    start 8.10

---
+++ rhel/rhel-8.x/rhel-8.10-hpc/install_prerequisites.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -ex
+
+# Import the newest AlmaLinux 8 GPG key
+#rpm --import https://repo.almalinux.org/almalinux/RPM-GPG-KEY-AlmaLinux
+
+# jq is needed to parse the component versions from the versions.json file
+yum install -y jq

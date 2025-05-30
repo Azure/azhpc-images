1bcde2c43cd68696041cfc759801da44567d0592
Shuyu Zhao <shuyuzhao@microsoft.com>
2025-05-29 17:22:58 -0700

    Fix

--- azurelinux/common/install_mellanoxofed.sh
+++ azurelinux/common/install_mellanoxofed.sh
@@ -40,28 +40,28 @@ tdnf install -y libibumad \
                 librdmacm-utils \
                 rdma-core \
                 rdma-core-devel \
-                https://packages.microsoft.com/azurelinux/3.0/prod/base/x86_64/Packages/m/mlnx-ofa_kernel-24.10-13.azl3.x86_64.rpm \
-                https://packages.microsoft.com/azurelinux/3.0/prod/base/x86_64/Packages/m/mlnx-ofa_kernel-modules-24.10-13.azl3.x86_64.rpm \
-                https://packages.microsoft.com/azurelinux/3.0/prod/base/x86_64/Packages/m/mlnx-ofa_kernel-devel-24.10-13.azl3.x86_64.rpm \
-                https://packages.microsoft.com/azurelinux/3.0/prod/base/x86_64/Packages/m/mlnx-ofa_kernel-source-24.10-13.azl3.x86_64.rpm \
-                https://packages.microsoft.com/azurelinux/3.0/prod/base/x86_64/Packages/m/mft_kernel-4.30.0-13.azl3.x86_64.rpm \
+                mlnx-ofa_kernel \
+                mlnx-ofa_kernel-modules \
+                mlnx-ofa_kernel-devel \
+                mlnx-ofa_kernel-source \
+                mft_kernel \
                 mstflint \
-                https://packages.microsoft.com/azurelinux/3.0/prod/base/x86_64/Packages/f/fwctl-24.10-13.azl3.x86_64.rpm  \
+                fwctl \
                 ibacm \
                 ibarr \
                 ibsim \
-                https://packages.microsoft.com/azurelinux/3.0/prod/base/x86_64/Packages/i/iser-24.10-13.azl3.x86_64.rpm \
-                https://packages.microsoft.com/azurelinux/3.0/prod/base/x86_64/Packages/i/isert-24.10-13.azl3.x86_64.rpm \
-                https://packages.microsoft.com/azurelinux/3.0/prod/base/x86_64/Packages/k/knem-1.1.4.90mlnx3-13.azl3.x86_64.rpm \
-                https://packages.microsoft.com/azurelinux/3.0/prod/base/x86_64/Packages/k/knem-modules-1.1.4.90mlnx3-13.azl3.x86_64.rpm \
+                iser \
+                isert \
+                knem \
+                knem-modules \
                 perftest \
                 libxpmem \
                 libxpmem-devel \
                 mlnx-ethtool \
                 mlnx-iproute2 \
-                https://packages.microsoft.com/azurelinux/3.0/prod/base/x86_64/Packages/m/mlnx-nfsrdma-24.10-13.azl3.x86_64.rpm \
+                mlnx-nfsrdma \
                 multiperf \
-                https://packages.microsoft.com/azurelinux/3.0/prod/base/x86_64/Packages/s/srp-24.10-13.azl3.x86_64.rpm \
+                srp \
                 srp_daemon \
                 ucx \
                 ucx-cma \
@@ -71,14 +71,14 @@ tdnf install -y libibumad \
                 ucx-rdmacm \
                 ucx-static \
                 ucx-knem \
-                https://packages.microsoft.com/azurelinux/3.0/prod/base/x86_64/Packages/x/xpmem-2.7.4-13.azl3.x86_64.rpm \
-                https://packages.microsoft.com/azurelinux/3.0/prod/base/x86_64/Packages/x/xpmem-modules-2.7.4-13.azl3.x86_64.rpm \
+                xpmem \
+                xpmem-modules \
                 ucx-xpmem \
                 libunwind \
                 libunwind-devel
 
 echo "INSTALLED MOFED!! ${MOFED_VERSION}"
-$COMMON_DIR/write_component_version.sh "MOFED" $MOFED_VERSION
+$COMMON_DIR/write_component_version.sh "OFED" $MOFED_VERSION
 
 # Restarting openibd
 # /etc/init.d/openibd restart

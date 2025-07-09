c28019c2c165ce330383f5a5d3816d49dc83500b
Jiaqi Li <ljiaqi@microsoft.com>
2025-07-09 12:31:18 -0700

    fix gdrcopy version

--- azurelinux/common/install_gdrcopy.sh
+++ azurelinux/common/install_gdrcopy.sh
@@ -9,9 +9,15 @@ GDRCOPY_VERSION=$(jq -r '.version' <<< $gdrcopy_metadata)
 GDRCOPY_SHA256=$(jq -r '.sha256' <<< $gdrcopy_metadata)
 GDRCOPY_DISTRIBUTION=$(jq -r '.distribution' <<< $gdrcopy_metadata)
 
+kernel_version=$(uname -r | sed 's/\-/./g')
+kernel_version=${kernel_version%.*}
+
+nvidia_driver_metadata=$(get_component_config "nvidia")
+NVIDIA_DRIVER_VERSION=$(jq -r '.driver.version' <<< $nvidia_driver_metadata)
+
 # Install gdrcopy kmod and devel packages from PMC
 tdnf install -y gdrcopy \
-                gdrcopy-kmod \
+                gdrcopy-kmod-2.5-7_$kernel_version.$NVIDIA_DRIVER_VERSION.azl3.x86_64 \
                 gdrcopy-devel
 
 $COMMON_DIR/write_component_version.sh "GDRCOPY" ${GDRCOPY_VERSION}

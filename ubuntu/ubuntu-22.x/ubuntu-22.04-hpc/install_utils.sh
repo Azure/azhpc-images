c46031e9cc85d7a703f30156bdcc50a5e08fae4a
Zheyu Shen <zheyushen@microsoft.com>
2025-07-30 22:34:25 +0000

    manage PMC via deb package instead

--- ubuntu/ubuntu-22.x/ubuntu-22.04-hpc/install_utils.sh
+++ ubuntu/ubuntu-22.x/ubuntu-22.04-hpc/install_utils.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -ex
 
+<<<<<<< Updated upstream
 # Setup microsoft packages repository for moby
 # Download the repository configuration package
 curl https://packages.microsoft.com/config/ubuntu/22.04/prod.list > ./microsoft-prod.list
@@ -9,6 +10,12 @@ cp ./microsoft-prod.list /etc/apt/sources.list.d/
 # Install the Microsoft GPG public key
 curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
 cp ./microsoft.gpg /etc/apt/trusted.gpg.d/
+=======
+# Setup microsoft packages repository
+curl -sSL -O https://packages.microsoft.com/config/ubuntu/22.04/packages-microsoft-prod.deb
+dpkg -i packages-microsoft-prod.deb
+rm packages-microsoft-prod.deb
+>>>>>>> Stashed changes
 
 #apt-get install packages
 

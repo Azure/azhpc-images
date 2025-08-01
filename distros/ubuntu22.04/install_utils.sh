b5fac63466d9d1eab5af5e4c3584cf0514839194
Sakana <shuyuzhao@microsoft.com>
2025-08-01 11:10:40 -0700

    fix sha256sum & apt key security

--- distros/ubuntu22.04/install_utils.sh
+++ distros/ubuntu22.04/install_utils.sh
@@ -1,15 +1,10 @@
 #!/bin/bash
 set -ex
 
-# Setup microsoft packages repository for moby
-# Download the repository configuration package
-curl https://packages.microsoft.com/config/ubuntu/22.04/prod.list > ./microsoft-prod.list
-# Copy the generated list to the sources.list.d directory
-cp ./microsoft-prod.list /etc/apt/sources.list.d/
-# Install the Microsoft GPG public key
-# Technically, per Debian Wiki guidance, this GPG isn't managed by a package, so it should go
-# into /etc/apt/keyrings, but this is what the above microsoft-prod.list expects in its signed-by
-curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > /usr/share/keyrings/microsoft-prod.gpg
+# Setup microsoft packages repository
+curl -sSL -O https://packages.microsoft.com/config/ubuntu/22.04/packages-microsoft-prod.deb
+dpkg -i packages-microsoft-prod.deb
+rm packages-microsoft-prod.deb
 
 apt-get update
 apt-get -y install build-essential

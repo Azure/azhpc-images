8fe1dfe2675ea872ed3ee1a88b87fe84d08c7d97
Jiaqi Li <ljiaqi@microsoft.com>
2025-08-18 22:36:58 -0700

    refine component version

--- azurelinux/common/install_mellanoxofed.sh
+++ azurelinux/common/install_mellanoxofed.sh
@@ -29,6 +29,9 @@ tdnf install -y bison \
 
 mofed_metadata=$(get_component_config "mofed")
 MOFED_VERSION=$(jq -r '.version' <<< $mofed_metadata)
+XPMEM_VERSION=$(jq -r '.xpmem.version' <<< $mofed_metadata)
+KNEM_VERSION=$(jq -r '.knem.version' <<< $mofed_metadata)
+MFT_KERNEL_VERSION=$(jq -r '.mft_kernel.version' <<< $mofed_metadata)
 
 kernel_version=$(uname -r | sed 's/\-/./g')
 
@@ -42,28 +45,28 @@ tdnf install -y libibumad \
                 librdmacm-utils \
                 rdma-core \
                 rdma-core-devel \
-                mlnx-ofa_kernel-24.10-20_$kernel_version.x86_64 \
-                mlnx-ofa_kernel-modules-24.10-20_$kernel_version.x86_64 \
-                mlnx-ofa_kernel-devel-24.10-20_$kernel_version.x86_64 \
-                mlnx-ofa_kernel-source-24.10-20_$kernel_version.x86_64 \
-                mft_kernel-4.30.0-20_$kernel_version.x86_64 \
+                mlnx-ofa_kernel-${MOFED_VERSION}_$kernel_version.x86_64 \
+                mlnx-ofa_kernel-modules-${MOFED_VERSION}_$kernel_version.x86_64 \
+                mlnx-ofa_kernel-devel-${MOFED_VERSION}_$kernel_version.x86_64 \
+                mlnx-ofa_kernel-source-${MOFED_VERSION}_$kernel_version.x86_64 \
+                mft_kernel-${MFT_KERNEL_VERSION}_$kernel_version.x86_64 \
                 mstflint \
-                fwctl-24.10-20_$kernel_version.x86_64 \
+                fwctl-${MOFED_VERSION}_$kernel_version.x86_64 \
                 ibacm \
                 ibarr \
                 ibsim \
-                iser-24.10-20_$kernel_version.x86_64 \
-                isert-24.10-20_$kernel_version.x86_64 \
-                knem-1.1.4.90mlnx3-20_$kernel_version.x86_64 \
-                knem-modules-1.1.4.90mlnx3-20_$kernel_version.x86_64 \
+                iser-${MOFED_VERSION}_$kernel_version.x86_64 \
+                isert-${MOFED_VERSION}_$kernel_version.x86_64 \
+                knem-${KNEM_VERSION}_$kernel_version.x86_64 \
+                knem-modules-${KNEM_VERSION}_$kernel_version.x86_64 \
                 perftest \
-                libxpmem-2.7.4-20_$kernel_version.x86_64 \
-                libxpmem-devel-2.7.4-20_$kernel_version.x86_64 \
+                libxpmem-${XPMEM_VERSION}_$kernel_version.x86_64 \
+                libxpmem-devel-${XPMEM_VERSION}_$kernel_version.x86_64 \
                 mlnx-ethtool \
                 mlnx-iproute2 \
-                mlnx-nfsrdma-24.10-20_$kernel_version.x86_64 \
+                mlnx-nfsrdma-${MOFED_VERSION}_$kernel_version.x86_64 \
                 multiperf \
-                srp-24.10-20_$kernel_version.x86_64 \
+                srp-${MOFED_VERSION}_$kernel_version.x86_64 \
                 srp_daemon \
                 ucx \
                 ucx-cma \
@@ -73,8 +76,8 @@ tdnf install -y libibumad \
                 ucx-rdmacm \
                 ucx-static \
                 ucx-knem \
-                xpmem-2.7.4-20_$kernel_version.x86_64 \
-                xpmem-modules-2.7.4-20_$kernel_version.x86_64 \
+                xpmem-${XPMEM_VERSION}_$kernel_version.x86_64 \
+                xpmem-modules-${XPMEM_VERSION}_$kernel_version.x86_64 \
                 ucx-xpmem \
                 libunwind \
                 libunwind-devel

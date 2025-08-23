995be57b7746bf052b5cce9cec7b4f7b346dfa65
Zheyu Shen <zheyushen@microsoft.com>
2025-08-23 15:09:26 -0700

    Revert "avoid re-installing waagent now that the problem has probably been fixed"
    This reverts commit db91b2a95016e63e6d6560cddc89f8a50e9375b1.

--- components/install_waagent.sh
+++ components/install_waagent.sh
@@ -20,17 +20,45 @@ function update_waagent_conf {
 if [[ $DISTRIBUTION == "azurelinux3.0" ]]; then
     # Update WALinuxAgent - for IPoIB
     tdnf update -y WALinuxAgent
+
+    update_waagent_conf "OS.EnableRDMA" "y"
+    update_waagent_conf "Extensions.GoalStatePeriod" "300"
+    update_waagent_conf "Extensions.GoalStatePeriod" "300"
+    update_waagent_conf "Extensions.InitialGoalStatePeriod" "6"
+    update_waagent_conf "OS.EnableFirewallPeriod" "300"
+    update_waagent_conf "OS.RemovePersistentNetRulesPeriod" "300"
+    update_waagent_conf "OS.RootDeviceScsiTimeoutPeriod" "300"
+    update_waagent_conf "OS.MonitorDhcpClientRestartPeriod" "60"
+    update_waagent_conf "Provisioning.MonitorHostNamePeriod" "60"
+else
+    # Set waagent version and sha256
+    waagent_metadata=$(get_component_config "waagent")
+    WAAGENT_VERSION=$(jq -r '.version' <<< $waagent_metadata)
+    WAAGENT_SHA256=$(jq -r '.sha256' <<< $waagent_metadata)
+
+    # Update WALinuxAgent - for IPoIB support
+    DOWNLOAD_URL=https://github.com/Azure/WALinuxAgent/archive/refs/tags/v${WAAGENT_VERSION}.tar.gz
+    download_and_verify ${DOWNLOAD_URL} ${WAAGENT_SHA256}
+    tar -xvf $(basename ${DOWNLOAD_URL})
+    pushd WALinuxAgent-${WAAGENT_VERSION}/
+    python3 setup.py install --register-service
+    popd
+
+    # Configure WALinuxAgent
+    update_waagent_conf "Extensions.GoalStatePeriod" "300"
+    update_waagent_conf "Extensions.InitialGoalStatePeriod" "6"
+    update_waagent_conf "OS.EnableFirewallPeriod" "300"
+    update_waagent_conf "OS.EnableRDMA" "y"
+    update_waagent_conf "OS.RemovePersistentNetRulesPeriod" "300"
+    update_waagent_conf "OS.RootDeviceScsiTimeoutPeriod" "300"
+    update_waagent_conf "OS.MonitorDhcpClientRestartPeriod" "60"
+    update_waagent_conf "Provisioning.MonitorHostName" "y"
+    update_waagent_conf "Provisioning.MonitorHostNamePeriod" "60"
+    rm -rf WALinuxAgent-${WAAGENT_VERSION}
 fi
 
-update_waagent_conf "OS.EnableRDMA" "y"
-update_waagent_conf "Extensions.GoalStatePeriod" "300"
-update_waagent_conf "Extensions.InitialGoalStatePeriod" "6"
-update_waagent_conf "OS.EnableFirewallPeriod" "300"
-update_waagent_conf "OS.RemovePersistentNetRulesPeriod" "300"
-update_waagent_conf "OS.RootDeviceScsiTimeoutPeriod" "300"
-update_waagent_conf "OS.MonitorDhcpClientRestartPeriod" "60"
-update_waagent_conf "Provisioning.MonitorHostName" "y"
-update_waagent_conf "Provisioning.MonitorHostNamePeriod" "60"
+write_component_version "WAAGENT" $(waagent --version | head -n 1 | awk -F' ' '{print $1}' | awk -F- '{print $2}')
+write_component_version "WAAGENT_EXTENSIONS" $(waagent --version | sed '3q;d' | awk -F' ' '{print $4}')
 
 systemctl daemon-reload
 # Restart waagent service in distribution specific file as its name differs between distributions

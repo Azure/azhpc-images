818dc58645de480420811f1948b00a54135c3886
Zheyu Shen <zheyushen@microsoft.com>
2025-09-10 12:06:30 -0700

    temporarily skip HPC for testing purposes

--- azurelinux/azurelinux-3.x/azurelinux-3.0-hpc/install.sh
+++ azurelinux/azurelinux-3.x/azurelinux-3.0-hpc/install.sh
@@ -54,93 +54,93 @@ source ./set_properties.sh
 # install DOCA OFED
 # $AZURE_LINUX_COMMON_DIR/install_doca.sh
 
-$AZURE_LINUX_COMMON_DIR/install_mellanoxofed.sh
-
-# install PMIX
-$AZURE_LINUX_COMMON_DIR/install_pmix.sh
-
-# install mpi libraries
-$AZURE_LINUX_COMMON_DIR/install_mpis.sh
-
-if [ "$GPU" = "NVIDIA" ]; then
-    # install nvidia gpu driver
-    $AZURE_LINUX_COMMON_DIR/install_nvidiagpudriver.sh "$SKU"
-    
-    # Install NCCL
-    $AZURE_LINUX_COMMON_DIR/install_nccl.sh
-
-    # Install NVIDIA docker container
-    $AZURE_LINUX_COMMON_DIR/install_docker.sh
-
-    # Install DCGM
-    $AZURE_LINUX_COMMON_DIR/install_dcgm.sh
-fi
-
-if [ "$GPU" = "AMD" ]; then
-    # Set up docker
-    tdnf install -y moby-engine moby-cli
-    systemctl enable docker
-    systemctl restart docker
-fi
-
-
-# install AMD tuned libraries
-$COMMON_DIR/install_amd_libs.sh
-
-# install Intel libraries
-$COMMON_DIR/install_intel_libs.sh
-
-# cleanup downloaded tarballs - clear some space
-rm -rf *.tgz *.bz2 *.tbz *.tar.gz *.run *.deb *_offline.sh
-rm -rf /tmp/MLNX_OFED_LINUX* /tmp/*conf*
-rm -rf /var/intel/ /var/cache/*
-rm -Rf -- */
-
-
-# optimizations
-$AZURE_LINUX_COMMON_DIR/hpc-tuning.sh
-
-# Install AZNFS Mount Helper
-$COMMON_DIR/install_aznfs.sh
-
-# install persistent rdma naming
-$COMMON_DIR/install_azure_persistent_rdma_naming.sh
-
-# add udev rule
-$AZURE_LINUX_COMMON_DIR/add-udev-rules.sh
-
-# add interface rules
-$AZURE_LINUX_COMMON_DIR/network-config.sh
-
-# install diagnostic script
-$COMMON_DIR/install_hpcdiag.sh
-
-#install monitoring tools
-$COMMON_DIR/install_monitoring_tools.sh
-
-# install Azure/NHC Health Checks
-$COMMON_DIR/install_health_checks.sh "$GPU"
-
-# copy test file
-$COMMON_DIR/copy_test_file.sh
-
-# disable cloud-init
-# $AZURE_LINUX_COMMON_DIR/disable_cloudinit.sh
-
-# SKU Customization
-$COMMON_DIR/setup_sku_customizations.sh
-
-if [ "$GPU" = "AMD" ]; then
-    #install rocm software stack
-    $AZURE_LINUX_COMMON_DIR/install_rocm.sh
-    
-    #install rccl and rccl-tests
-    $AZURE_LINUX_COMMON_DIR/install_rccl.sh
-fi
-
-# clear history
-# Uncomment the line below if you are running this on a VM
-# $COMMON_DIR/clear_history.sh
+# $AZURE_LINUX_COMMON_DIR/install_mellanoxofed.sh
+# 
+# # install PMIX
+# $AZURE_LINUX_COMMON_DIR/install_pmix.sh
+# 
+# # install mpi libraries
+# $AZURE_LINUX_COMMON_DIR/install_mpis.sh
+# 
+# if [ "$GPU" = "NVIDIA" ]; then
+#     # install nvidia gpu driver
+#     $AZURE_LINUX_COMMON_DIR/install_nvidiagpudriver.sh "$SKU"
+#     
+#     # Install NCCL
+#     $AZURE_LINUX_COMMON_DIR/install_nccl.sh
+# 
+#     # Install NVIDIA docker container
+#     $AZURE_LINUX_COMMON_DIR/install_docker.sh
+# 
+#     # Install DCGM
+#     $AZURE_LINUX_COMMON_DIR/install_dcgm.sh
+# fi
+# 
+# if [ "$GPU" = "AMD" ]; then
+#     # Set up docker
+#     tdnf install -y moby-engine moby-cli
+#     systemctl enable docker
+#     systemctl restart docker
+# fi
+# 
+# 
+# # install AMD tuned libraries
+# $COMMON_DIR/install_amd_libs.sh
+# 
+# # install Intel libraries
+# $COMMON_DIR/install_intel_libs.sh
+# 
+# # cleanup downloaded tarballs - clear some space
+# rm -rf *.tgz *.bz2 *.tbz *.tar.gz *.run *.deb *_offline.sh
+# rm -rf /tmp/MLNX_OFED_LINUX* /tmp/*conf*
+# rm -rf /var/intel/ /var/cache/*
+# rm -Rf -- */
+# 
+# 
+# # optimizations
+# $AZURE_LINUX_COMMON_DIR/hpc-tuning.sh
+# 
+# # Install AZNFS Mount Helper
+# $COMMON_DIR/install_aznfs.sh
+# 
+# # install persistent rdma naming
+# $COMMON_DIR/install_azure_persistent_rdma_naming.sh
+# 
+# # add udev rule
+# $AZURE_LINUX_COMMON_DIR/add-udev-rules.sh
+# 
+# # add interface rules
+# $AZURE_LINUX_COMMON_DIR/network-config.sh
+# 
+# # install diagnostic script
+# $COMMON_DIR/install_hpcdiag.sh
+# 
+# #install monitoring tools
+# $COMMON_DIR/install_monitoring_tools.sh
+# 
+# # install Azure/NHC Health Checks
+# $COMMON_DIR/install_health_checks.sh "$GPU"
+# 
+# # copy test file
+# $COMMON_DIR/copy_test_file.sh
+# 
+# # disable cloud-init
+# # $AZURE_LINUX_COMMON_DIR/disable_cloudinit.sh
+# 
+# # SKU Customization
+# $COMMON_DIR/setup_sku_customizations.sh
+# 
+# if [ "$GPU" = "AMD" ]; then
+#     #install rocm software stack
+#     $AZURE_LINUX_COMMON_DIR/install_rocm.sh
+#     
+#     #install rccl and rccl-tests
+#     $AZURE_LINUX_COMMON_DIR/install_rccl.sh
+# fi
+# 
+# # clear history
+# # Uncomment the line below if you are running this on a VM
+# # $COMMON_DIR/clear_history.sh
 
 # generate SBOM using Syft
 $COMMON_DIR/syft_sbom.sh

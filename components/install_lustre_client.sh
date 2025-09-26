9b9b24117f4f9b3197ab3980835a3c98c0d5ab15
Sakana <shuyuzhao@microsoft.com>
2025-09-26 12:57:06 -0700

    fix lustre

--- components/install_lustre_client.sh
+++ components/install_lustre_client.sh
@@ -9,10 +9,12 @@ LUSTRE_VERSION=$(jq -r '.version' <<< $lustre_metadata)
 
 if [[ $DISTRIBUTION == *"ubuntu"* ]]; then
     source /etc/lsb-release
+    UBUNTU_VERSION=$(cat /etc/os-release | grep VERSION_ID | cut -d= -f2 | cut -d\" -f2)
     if [ $UBUNTU_VERSION == 24.04 ]; then
         SIGNED_BY="/usr/share/keyrings/microsoft-prod.gpg"
     elif [ $UBUNTU_VERSION == 22.04 ]; then
         SIGNED_BY="/etc/apt/trusted.gpg.d/microsoft-prod.gpg"
+    fi
     echo "deb [arch=amd64 signed-by=$SIGNED_BY] https://packages.microsoft.com/repos/amlfs-${DISTRIB_CODENAME}/ ${DISTRIB_CODENAME} main" | sudo tee /etc/apt/sources.list.d/amlfs.list
     # Enable these lines if the MS PMC repo was not already setup.
     #curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg

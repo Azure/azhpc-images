e2b8635e5f350e4b3759d4edc252726768dfe880
benjaminli-microsoft <118771976+benjaminli-microsoft@users.noreply.github.com>
2025-10-22 11:41:53 -0400

    Reload NVIDIA driver for non-V100 SKUs
    Added logic to reload NVIDIA driver for non-V100 SKUs.

--- distros/ubuntu22.04/install.sh
+++ distros/ubuntu22.04/install.sh
@@ -63,6 +63,14 @@ if [ "$GPU" = "AMD" ]; then
     $COMPONENT_DIR/install_rocm.sh    
     #install rccl and rccl-tests
     $COMPONENT_DIR/install_rccl.sh
+
+    if [ "$SKU" != "V100" ]; then
+        echo "BENJAMINLILOG reloading nvidia driver"
+        sudo systemctl stop nvidia-dcgm.service
+        sudo modprobe -r nvidia_drm nvidia_modeset gdrdrv nvidia_peermem nvidia_uvm nvidia  
+        sudo modprobe nvidia nvidia_modeset nvidia_uvm nvidia_peermem gdrdrv nvidia_drm
+        sudo systemctl start nvidia-dcgm.service
+    fi
 fi
 
 # install AMD libs

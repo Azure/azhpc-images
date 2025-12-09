b42c6ff1aedaabb71e8ae43957eeb77ee8b97d2f
Jiaqi Li <ljiaqi@microsoft.com>
2025-12-08 22:25:34 -0800

    fix lustre repo

--- components/install_lustre_client.sh
+++ components/install_lustre_client.sh
@@ -16,9 +16,9 @@ if [[ $DISTRIBUTION == *"ubuntu"* ]]; then
         SIGNED_BY="/etc/apt/trusted.gpg.d/microsoft-prod.gpg"
     fi
     if [[ "$ARCH" == "aarch64" ]]; then
-        echo "deb [arch=arm64] https://packages.microsoft.com/repos/amlfs-${DISTRIB_CODENAME}/ ${DISTRIB_CODENAME} main" | tee /etc/apt/sources.list.d/amlfs.list
+        echo "deb [arch=arm64 signed-by=$SIGNED_BY] https://packages.microsoft.com/repos/amlfs-${DISTRIB_CODENAME}/ ${DISTRIB_CODENAME} main" | tee /etc/apt/sources.list.d/amlfs.list
     else
-        echo "deb [arch=amd64] https://packages.microsoft.com/repos/amlfs-${DISTRIB_CODENAME}/ ${DISTRIB_CODENAME} main" | tee /etc/apt/sources.list.d/amlfs.list
+        echo "deb [arch=amd64 signed-by=$SIGNED_BY] https://packages.microsoft.com/repos/amlfs-${DISTRIB_CODENAME}/ ${DISTRIB_CODENAME} main" | tee /etc/apt/sources.list.d/amlfs.list
     fi
     # Enable these lines if the MS PMC repo was not already setup.
     #curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg

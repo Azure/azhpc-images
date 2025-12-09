66124db574f0dabf85dc961ef6e37915c3bb4062
Jiaqi Li <ljiaqi@microsoft.com>
2025-12-08 23:35:17 -0800

    fix versions.json

--- components/install_doca.sh
+++ components/install_doca.sh
@@ -9,7 +9,12 @@ DOCA_SHA256=$(jq -r '.sha256' <<< $doca_metadata)
 DOCA_URL=$(jq -r '.url' <<< $doca_metadata)
 DOCA_FILE=$(basename ${DOCA_URL})
 
-download_and_verify $DOCA_URL $DOCA_SHA256
+if [[ "$SKU" == "GB200" ]]; then
+    dpkg -i $TOP_DIR/internal_bits/doca-host_${DOCA_VERSION}_arm64.deb
+else
+    $COMMON_DIR/download_and_verify.sh $DOCA_URL $DOCA_SHA256
+    dpkg -i $DOCA_FILE
+fi
 
 if [[ $DISTRIBUTION == *"ubuntu"* ]]; then
     dpkg -i $DOCA_FILE

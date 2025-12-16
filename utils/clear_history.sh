1a81433b6d9da47c705e29f694b4dc7c0e0ff84d
Jiaqi Li <ljiaqi@microsoft.com>
2025-12-16 12:03:46 -0800

    purge azure-proxy-agent

--- utils/clear_history.sh
+++ utils/clear_history.sh
@@ -61,12 +61,21 @@ then
     tdnf history sync
 fi
 
-# Remove Defender
 if [[ $distro == *"Ubuntu"* ]]
 then
+    # Remove Defender
     if dpkg -l | grep -qw mdatp; then
         apt-get purge -y mdatp
     fi
+
+    # Remove Azure Proxy Agent
+    # Azure Proxy Agent is introduced in from 24.04.202512100 of Ubuntu images. It provides process-level authentication and authorization 
+    # for access to Azure IMDS and WireServer metadata endpoint, which will block core-dns from accessing metadata endpoint causing issues in some scenarios
+    # and conflicts with eBPF programs intercepted by Kubernetes CNI. 
+    if dpkg -l | grep -qw azure-proxy-agent; then
+        apt-get purge -y azure-proxy-agent
+    fi
+
 elif [[ $distro == *"AzureLinux"* ]]
 then
     if tdnf list installed | grep -qw mdatp; then

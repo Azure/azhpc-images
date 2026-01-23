f9777e1d7f68241019d1b6bc9b4bbb279e516228
Colton Paul <ColtonPaul@users.noreply.github.com>
2026-01-23 09:58:18 -0800

    Add comments / minor refactorings for install_dcgm

--- components/install_dcgm.sh
+++ components/install_dcgm.sh
@@ -9,10 +9,6 @@ CUDA_VERSION=$(nvidia-smi | sed -E -n 's/.*CUDA Version: ([0-9]+)[.].*/\1/p')
 # Check for SKU-specific CUDA version in versions.json that may be lower
 cuda_metadata=$(get_component_config "cuda")
 SKU_CUDA_VERSION=$(jq -r '.driver.version' <<< $cuda_metadata | cut -d'.' -f1)
-# Only set if it's lower than nvidia-smi version
-if [[ "${SKU_CUDA_VERSION}" -ge "${CUDA_VERSION}" ]]; then
-    SKU_CUDA_VERSION=""
-fi
 
 # Install DCGM
 # Reference: https://developer.nvidia.com/dcgm#Downloads
@@ -28,13 +24,15 @@ if [[ $DISTRIBUTION == *"ubuntu"* ]]; then
         datacenter-gpu-manager-4-core=${DCGM_VERSION} \
         datacenter-gpu-manager-4-proprietary=${DCGM_VERSION} \
         datacenter-gpu-manager-4-proprietary-cuda${CUDA_VERSION}=${DCGM_VERSION}
-    # Install DCGM packages for lower SKU-specific CUDA version if exists
-    if [[ -n "${SKU_CUDA_VERSION}" ]]; then
+
+    # Nvidia documentation says that "Generally speaking, users should install binaries targeting the major version of the CUDA user-mode driver that's installed on their system."
+    # but that v100 "is not supported by version 13.0.0 of the CUDA Toolkit. Consequently, Maxwell, Volta, and Pascal systems using driver version 580 should install DCGM packages targeting major version 12
+    # of the user-mode driver (e.g. datacenter-gpu-manager-4-cuda12) rather than DCGM packages targeting major version 13."
+    # In practice though, DCGM requires both cuda12 and cuda13 support packages (https://github.com/NVIDIA/DCGM/issues/254).
+    if [[ "${SKU_CUDA_VERSION}" -lt "${CUDA_VERSION}" ]]; then
         echo "Installing DCGM packages for SKU-specific CUDA ${SKU_CUDA_VERSION}"
         apt-get install -y \
             datacenter-gpu-manager-4-cuda${SKU_CUDA_VERSION}=${DCGM_VERSION} \
-            datacenter-gpu-manager-4-core=${DCGM_VERSION} \
-            datacenter-gpu-manager-4-proprietary=${DCGM_VERSION} \
             datacenter-gpu-manager-4-proprietary-cuda${SKU_CUDA_VERSION}=${DCGM_VERSION}
     fi
 elif [[ $DISTRIBUTION == *"almalinux"* ]]; then

696ee81754756362b513589d6beeaef9714ac468
Qixin Ye <v-mersenye@microsoft.com>
2026-02-17 14:08:26 -0500

    skip gpgcheck for nvshmem

--- components/install_nvshmem.sh
+++ components/install_nvshmem.sh
@@ -10,9 +10,9 @@ CUDA_MAJOR_VERSION=$(echo $CUDA_DRIVER_VERSION | cut -d. -f1)
 
 if [[ $DISTRIBUTION == "azurelinux3.0" ]]; then 
     path_var="$TOP_DIR/prebuilt"
-    tdnf install -y $path_var/libnvshmem3-cuda-$CUDA_MAJOR_VERSION-*.rpm
-    tdnf install -y $path_var/libnvshmem3-devel-cuda-$CUDA_MAJOR_VERSION-*.rpm
-    tdnf install -y $path_var/libnvshmem3-static-cuda-$CUDA_MAJOR_VERSION-*.rpm
+    tdnf install -y $path_var/libnvshmem3-cuda-$CUDA_MAJOR_VERSION-*.rpm --nogpgcheck
+    tdnf install -y $path_var/libnvshmem3-devel-cuda-$CUDA_MAJOR_VERSION-*.rpm --nogpgcheck
+    tdnf install -y $path_var/libnvshmem3-static-cuda-$CUDA_MAJOR_VERSION-*.rpm --nogpgcheck
     nvshmem_version=$(tdnf list installed | grep libnvshmem3-cuda-$CUDA_MAJOR_VERSION/ | cut -d' ' -f2)
 elif [[ $DISTRIBUTION == *"ubuntu"* ]]; then
     apt install libnvshmem3-cuda-$CUDA_MAJOR_VERSION libnvshmem3-dev-cuda-$CUDA_MAJOR_VERSION

86ecdf6288b530cbdc5c66802e93b2f24541a71a
Jiaqi Li <ljiaqi@microsoft.com>
2026-05-13 17:09:33 -0700

    remove sudo in gb200 kernel change

--- packer/scripts/prerequisites.sh
+++ packer/scripts/prerequisites.sh
@@ -101,14 +101,14 @@ install_ubuntu_gb200_kernel() {
     
     apt-get update
     if [[ "${KERNEL_VERSION}" == "6.14" ]]; then
-        sudo apt-get install linux-azure-nvidia -y  
-        sudo apt-mark hold linux-azure-nvidia
+        apt-get install linux-azure-nvidia -y  
+        apt-mark hold linux-azure-nvidia
     elif [[ "${KERNEL_VERSION}" == "6.17" ]]; then
-        sudo apt-get install linux-azure-nvidia-6.17 -y  
-        sudo apt-mark hold linux-azure-nvidia-6.17
+        apt-get install linux-azure-nvidia-6.17 -y  
+        apt-mark hold linux-azure-nvidia-6.17
     else #kernel 6.8
-        sudo apt-get install linux-azure-nvidia-6.8 -y
-        sudo apt-mark hold linux-azure-nvidia-6.8                
+        apt-get install linux-azure-nvidia-6.8 -y
+        apt-mark hold linux-azure-nvidia-6.8                
     fi
     
     # Purge non-nvidia kernels

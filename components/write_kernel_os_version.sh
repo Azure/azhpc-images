d8847e35bdc2d22157c88887fcb26fd35f6c9447
Qixin Ye <v-mersenye@microsoft.com>
2026-05-28 17:21:57 -0400

    use uname -r for azl kernel version

--- components/write_kernel_os_version.sh
+++ components/write_kernel_os_version.sh
@@ -5,8 +5,7 @@ set -ex
 source ${UTILS_DIR}/utilities.sh
 
 if [[ $DISTRIBUTION == "azurelinux3.0" ]]; then
-   kernel_version=$(rpm -q kernel | sed 's/kernel\-//g')
-   write_component_version "KERNEL" ${kernel_version::-12}
+   write_component_version "KERNEL" $(uname -r)
    os_version=$(rpm -qf /etc/os-release)
    write_component_version "OS" ${os_version::-12}
 else

43af41e74ca10de2839c7589490f12ff213268a5
Zheyu Shen <zheyushen@microsoft.com>
2026-06-08 17:18:53 -0700

    fix more U26 stuff

--- distros/ubuntu26.04/install.sh
+++ distros/ubuntu26.04/install.sh
@@ -139,6 +139,8 @@ if [[ "$SKU" != "GB200" ]]; then
     $COMPONENT_DIR/install_health_checks.sh "$GPU"
 fi 
 
+$COMPONENT_DIR/write_kernel_os_version.sh
+
 # add udev rule
 $COMPONENT_DIR/add-udev-rules.sh
 

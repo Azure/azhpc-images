8cc05588f6a6bbe23338102a03fdd88e7bc2aa82
Jiaqi Li <ljiaqi@microsoft.com>
2026-06-26 19:47:28 -0700

    use set -x

--- packer/scripts/prerequisites.sh
+++ packer/scripts/prerequisites.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -euo pipefail
+set -euox pipefail
 
 # =============================================================================
 # Prerequisites: LTS kernel, package updates, GB200 config

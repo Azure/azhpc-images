a47274577be706324f773e0b503ba05f244919ed
Shuyu-Zhao <shuyuzhao@microsoft.com>
2026-08-12 18:34:17 -0700

    Squashed commit of the following:
    commit 66038084a16b1338035abddbde057eb85c19d457
    Author: Zheyu Shen <arsdragonfly@gmail.com>
    Date:   Mon Mar 30 19:14:46 2026 -0700
    
        bump packer plugin version required (#507)
    
        Co-authored-by: Zheyu Shen <zheyushen@microsoft.com>
    
    commit 342785513a4510af1bda77963a5f4c0cdc57e452
    Author: Qixin Ye <54224160+Mersen130@users.noreply.github.com>
    Date:   Mon Mar 30 18:29:27 2026 -0700
    
        azl3 march refresh (#505)
    
        * delete libnvidia-nscq for aarch64 az3;  move repo config to its own file
    
        * add azl3 aarch64 entry; install docker-buildx
    
        * packer support azl3 gb200
    
        * packer azl3 gb200 artifacts
    
        * extend polling time
    
        * skip azl3 x86 package downloads for gb200
    
        * delete libnvidia-nscq for aarch64 az3;  move repo config to its own file
    
        * add azl3 aarch64 entry; install docker-buildx
    
        * packer support azl3 gb200
    
        * packer azl3 gb200 artifacts
    
        * extend polling time
    
        * skip azl3 x86 package downloads for gb200
    
        * write pmix version for az3 aarch64
    
        * add comment
    
        ---------
    
        Co-authored-by: Qixin Ye <v-mersenye@microsoft.com>
    
    commit a57762b160e53a62585f529a4750659e4487e2cb
    Author: Nguyen Vuong <nguyenshagum2008@gmail.com>
    Date:   Fri Mar 20 14:54:34 2026 -0700
    
        Ubuntu 24.04 V100 March 26 refresh (#501)
    
        * V100 March 26 refresh
    
        * Updating gdrcopy from 2.5.1 to 2.5.2
    
        ---------
    
        Co-authored-by: Nguyen Vuong (from Dev Box) <nguyenvuong@microsoft.com>

--- distros/almalinux8.10/install_utils.sh
+++ distros/almalinux8.10/install_utils.sh
@@ -7,17 +7,25 @@ source ${UTILS_DIR}/utilities.sh
 # calls to Microsoft endpoints.
 $COMPONENT_DIR/install_microsoft_tls_root_g2.sh
 
-# Setup microsoft packages repository for moby
-# Download the repository configuration package
-curl https://packages.microsoft.com/config/rhel/8/prod.repo > ./microsoft-prod.repo
+# Setup Microsoft package repositories. Alma uses the native Alma repo, while
+# Moby packages currently come from the RHEL repo under a distinct repo ID.
+curl https://packages.microsoft.com/config/alma/8/prod.repo > ./microsoft-prod.repo
+sed -i '/^\[/a priority=10' ./microsoft-prod.repo
+sed -i '/^\[/a module_hotfixes=1' ./microsoft-prod.repo
+curl https://packages.microsoft.com/config/rhel/8/prod.repo > ./microsoft-rhel-prod.repo
+sed -i 's/^\[packages-microsoft-com-prod\]/[packages-microsoft-com-rhel-prod]/' ./microsoft-rhel-prod.repo
+sed -i 's/^name=Microsoft Production/name=Microsoft RHEL Production/' ./microsoft-rhel-prod.repo
+sed -i '/^\[/a priority=20' ./microsoft-rhel-prod.repo
 # Microsoft's moby-runc rpm declares `Provides: runc`, and `runc` is a
 # `container-tools` module artifact in AppStream. dnf modular filtering
 # would therefore hide every moby-runc-*.el8 rpm and break moby-engine
-# install. Mark the MS repo as a hot-fix source to bypass modular
+# install. Mark the RHEL MS repo as a hot-fix source to bypass modular
 # filtering for its rpms only, without disturbing container-tools.
-sed -i '/^\[/a module_hotfixes=1' ./microsoft-prod.repo
+sed -i '/^\[/a module_hotfixes=1' ./microsoft-rhel-prod.repo
 # Copy the generated list to the sources.list.d directory
+grep -lE '^\[(packages-microsoft-com-prod|packages-microsoft-com-rhel-prod)\]' /etc/yum.repos.d/*.repo 2>/dev/null | xargs -r rm -f
 cp ./microsoft-prod.repo /etc/yum.repos.d/
+cp ./microsoft-rhel-prod.repo /etc/yum.repos.d/
 
 yum repolist
 
